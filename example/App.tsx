b26068e8dcff8c3b3550cd7c37f44401ac2a9fcc
Christian Falch <christian@framx.no>
2021-11-10 16:38:26 +0100

    Added example of drawing

--- example/App.tsx
+++ example/App.tsx
@@ -1,15 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useMemo} from 'react';
 import {
+  Dimensions,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,14 +9,19 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
+import type {IPath} from '@shopify/react-native-skia';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Skia,
+  usePaint,
+  useDrawCallback,
+  PaintStyle,
+  StrokeCap,
+} from '@shopify/react-native-skia';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const bgColor = Skia.Color('#7FC8A9');
+const fgColor = Skia.Color('#7F00A9');
 
 const Section: React.FC<{
   title: string;
@@ -62,31 +58,43 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const paint = usePaint();
+
+  const pathPaint = usePaint(p => {
+    p.setColor(fgColor);
+    p.setStrokeWidth(15);
+    p.setStyle(PaintStyle.Stroke);
+    p.setStrokeCap(StrokeCap.Round);
+  });
+
+  const paths = useMemo(() => [] as IPath[], []);
+
+  const onDraw = useDrawCallback(
+    canvas => {
+      paint.setColor(bgColor);
+      canvas.drawPaint(paint);
+      if (paths.length > 0) {
+        for (let i = 0; i < paths.length; i++) {
+          canvas.drawPath(paths[i]!, pathPaint);
+        }
+      }
+    },
+    [paint, pathPaint, paths],
+  );
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Simple Skia Drawing">
+            <Skia.View style={styles.skiaview} onDraw={onDraw} />
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -109,6 +117,14 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  skiaview: {
+    width: Dimensions.get('window').width * 0.85,
+    height: Dimensions.get('window').height * 0.25,
+    marginBottom: 20,
+    borderRadius: 20,
+    overflow: 'hidden',
+    backgroundColor: '#ECECEC',
   },
 });
 
