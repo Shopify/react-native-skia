2765a9de024d239a02b4e7988ac05f2e832e384b
Christian Falch <christian@framx.no>
2021-11-15 12:34:45 +0100

    Fixed smooth paths in drawing example

--- example/src/Examples/Drawing/index.tsx
+++ example/src/Examples/Drawing/index.tsx
@@ -1,5 +1,5 @@
-import React, {useMemo} from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import React, {useMemo, useRef} from 'react';
+import {Button, Dimensions, StyleSheet, View} from 'react-native';
 import type {IPath} from '@shopify/react-native-skia';
 import {
   Skia,
@@ -10,10 +10,12 @@ import {
 } from '@shopify/react-native-skia';
 
 import {Section} from '../../Section';
-import {ExampleProps} from './types';
+import {ExampleProps} from '../types';
 
 const bgColor = Skia.Color('#7FC8A9');
 const fgColor = Skia.Color('#7F00A9');
+
+type Point = {x: number; y: number};
 
 export const DrawingExample: React.FC<ExampleProps> = ({
   index,
@@ -21,6 +23,7 @@ export const DrawingExample: React.FC<ExampleProps> = ({
   onToggle,
 }) => {
   const paint = usePaint(p => p.setColor(bgColor));
+  const prevPointRef = useRef<Point>();
 
   const pathPaint = usePaint(p => {
     p.setColor(fgColor);
@@ -36,7 +39,7 @@ export const DrawingExample: React.FC<ExampleProps> = ({
       canvas.drawPaint(paint);
       if (paths.length > 0) {
         for (let i = 0; i < paths.length; i++) {
-          canvas.drawPath(paths[i]!, pathPaint);
+          canvas.drawPath(paths[i], pathPaint);
         }
       }
     },
@@ -53,16 +56,36 @@ export const DrawingExample: React.FC<ExampleProps> = ({
       <View
         onTouchStart={evt => {
           const path = Skia.Path();
-          path.moveTo(evt.nativeEvent.locationX, evt.nativeEvent.locationY);
           paths.push(path);
+          path.moveTo(evt.nativeEvent.locationX, evt.nativeEvent.locationY);
+          prevPointRef.current = {
+            x: evt.nativeEvent.locationX,
+            y: evt.nativeEvent.locationY,
+          };
         }}
         onTouchMove={evt => {
-          paths[paths.length - 1]?.lineTo(
-            evt.nativeEvent.locationX,
-            evt.nativeEvent.locationY,
+          if (prevPointRef.current === undefined) {
+            return;
+          }
+          const path = paths[paths.length - 1];
+
+          const x = evt.nativeEvent.locationX;
+          const y = evt.nativeEvent.locationY;
+
+          const x_mid = (prevPointRef.current.x + x) / 2;
+          const y_mid = (prevPointRef.current.y + y) / 2;
+
+          path.quadTo(
+            prevPointRef.current.x,
+            prevPointRef.current.y,
+            x_mid,
+            y_mid,
           );
+
+          prevPointRef.current = {x, y};
         }}>
         <Skia.View style={styles.skiaview} onDraw={onDraw} mode="continuous" />
+        <Button title="clear" onPress={() => (paths.length = 0)} />
       </View>
     </Section>
   );
--- example/src/Examples/Drawing/types.ts
+++
@@ -1,5 +0,0 @@
-export type ExampleProps = {
-  index: number;
-  isVisible: boolean;
-  onToggle: (index: number) => void;
-};
