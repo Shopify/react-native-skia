0ec5a9a194f4a00580c0b5c6cb7684379766e181
William Candillon <wcandillon@gmail.com>
2021-11-19 09:53:39 +0100

    Remove cycle

--- package/src/skia/SVG/useSvg.ts
+++ package/src/skia/SVG/useSvg.ts
@@ -1,8 +1,7 @@
 import { useMemo, useState } from "react";
 
-import { Skia } from "../Skia";
-
 import type { ISvgDom } from "./SVG";
+import { SvgObject } from './SVG';
 
 /**
  * Returns a Skia SvgDom object loaded from file using require
@@ -11,7 +10,7 @@ export const useSvg = (source: number) => {
   const [svgDom, setSvgDom] = useState<ISvgDom>();
   useMemo(
     () =>
-      Skia.Svg.fromUri(source).then((value) => {
+      SvgObject.fromUri(source).then((value) => {
         setSvgDom(value);
       }),
     [source]
