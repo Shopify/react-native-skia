57e3ae34be58512e0f90b8450fb14b55caafb67c
William Candillon <wcandillon@gmail.com>
2021-11-22 10:25:53 +0100

    :wrench:

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,12 +1,3 @@
 export * from "./renderer";
 export * from "./views";
-export type {
-  Canvas,
-  Paint,
-  Image,
-  Rect,
-  Path,
-  Shader,
-  RuntimeEffect,
-} from "./skia";
 export * from "./skia";
