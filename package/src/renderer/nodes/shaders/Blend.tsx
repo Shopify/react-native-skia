09eb1ca5c4f06ebcce117861d815c4ab70c4c5aa
William Candillon <wcandillon@gmail.com>
2021-11-27 14:59:45 +0100

    :wrench:

--- package/src/renderer/nodes/shaders/Blend.tsx
+++ package/src/renderer/nodes/shaders/Blend.tsx
@@ -15,7 +15,6 @@ export const Blend = ({ mode, ...props }: BlendProps) => {
   const onDeclare = useDeclaration(
     (_, children) => {
       const [one, two] = children.filter(isShader);
-      console.log(BlendMode[enumKey(mode)], one, two);
       return Skia.Shader.MakeBlend(BlendMode[enumKey(mode)], one, two);
     },
     [mode]
