ced5651c92bfff6214d3c4536e1c88fd56e7e5a7
William Candillon <wcandillon@gmail.com>
2021-11-29 07:01:58 +0100

    :wrench:

--- package/src/renderer/components/image/ImageShader.tsx
+++ package/src/renderer/components/image/ImageShader.tsx
@@ -2,16 +2,21 @@ import { useImage } from "../../../skia";
 import { TileMode } from "../../../skia/ImageFilter/ImageFilter";
 import { FilterMode, MipmapMode } from "../../../skia/Image/Image";
 import { useDeclaration } from "../../nodes/Declaration";
-import type { TransformProps } from "../../processors";
+import type { TransformProps, SkEnum } from "../../processors";
 import { localMatrix } from "../../processors/Transform";
 import type { AnimatedProps } from "../../processors/Animations/Animations";
 import { materialize } from "../../processors/Animations/Animations";
+import { enumKey } from "../../processors";
 
 import type { ImageProps } from "./Image";
 
 // TODO: add fit property and infer the transform matrix from src and dst
 interface ImageShaderProps extends TransformProps {
   source: ImageProps["source"];
+  tx: SkEnum<typeof TileMode>;
+  ty: SkEnum<typeof TileMode>;
+  fm: SkEnum<typeof FilterMode>;
+  mm: SkEnum<typeof MipmapMode>;
 }
 
 export const ImageShader = (
@@ -23,16 +28,23 @@ export const ImageShader = (
       if (image === null) {
         return null;
       }
-      const { transform, origin } = materialize(ctx, props);
+      const { tx, ty, fm, mm, ...transform } = materialize(ctx, props);
       return image.makeShaderOptions(
-        TileMode.Decal,
-        TileMode.Decal,
-        FilterMode.Nearest,
-        MipmapMode.None,
-        localMatrix({ transform, origin })
+        TileMode[enumKey(tx)],
+        TileMode[enumKey(ty)],
+        FilterMode[enumKey(fm)],
+        MipmapMode[enumKey(mm)],
+        localMatrix(transform)
       );
     },
     [image, props]
   );
   return <skDeclaration onDeclare={onDeclare} />;
 };
+
+ImageShader.defaultProps = {
+  tx: "decal",
+  ty: "decal",
+  fm: "nearest",
+  mm: "none",
+} as const;
