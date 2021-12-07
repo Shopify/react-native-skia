f44d34462017efc7b1763fc58362f5d7527eac84
Christian Falch <christian@framx.no>
2021-12-07 15:17:48 +0100

    Added npm template files

---
+++ npm/android/RNSkiaPackage.java
@@ -0,0 +1,18 @@
+// RNSkiaPackage.java
+
+package com.shopify.reactnative.skia;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import com.shopify.reactnative.skia.RNSkiaPackage;
+
+public class RNSkiaPackage extends RNSkiaPackage {
+    // Dummy package
+}
---
+++ npm/readme.md
@@ -0,0 +1,3 @@
+# NPM folder
+
+The NMP folder contains files need for build and distribution that should not be part of the development repository.
