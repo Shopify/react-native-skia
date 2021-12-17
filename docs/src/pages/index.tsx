4d42402535ab383edc54912cc9bcd9d97cffb9ab
William Candillon <wcandillon@gmail.com>
2021-12-17 11:24:24 +0100

    :lipstick:

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -11,6 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
+        {/* <img src="/img/skia2.png" alt="Skia" height="200" width="auto" /> */}
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
