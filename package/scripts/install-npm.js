373d381c8155810621ea1a94548e7cbb32d519e5
Nikita Dudin <nikita.dudin@gmail.com>
2021-12-19 16:00:26 +0300

    Remove the error handler, check if the link exists and unlink it before create a new symlink

--- package/scripts/install-npm.js
+++ package/scripts/install-npm.js
@@ -3,22 +3,20 @@ const path = require("path");
 
 console.log("Updating symlinks for Android build...");
 
+const isExists = (path) => {
+  return fs.existsSync(path)
+}
+
 const createSymlink = (p) => {
+  console.log(`Creating symlink to ${p}`, __dirname, process.cwd());
   const srcDir = path.resolve(`./cpp/${p}`);
   const dstDir = path.resolve(`./android/cpp/${p}`);
 
-  try {
-    console.log(`Creating symlink to ${p}`, __dirname, process.cwd());
-
-    fs.symlinkSync(srcDir, dstDir, "dir");
-  } catch (err) {
-    if (err && err.code === "EEXIST") {
-      console.log("Directory already exists:", dstDir);
-      return;
-    }
-
-    throw err;
+  if (isExists(dstDir)) {
+    fs.unlinkSync(dstDir);
   }
+
+  fs.symlinkSync(srcDir, dstDir, "dir");
 };
 
 // Copy common cpp files from the package root to the android folder
