361607ddd88a1d00eb8a3bf872bf63cc63f7fb6d
William Candillon <wcandillon@gmail.com>
2021-12-20 07:22:28 +0100

    Add support for int uniform and transform in Shader

--- package/src/renderer/components/shaders/Shader.tsx
+++ package/src/renderer/components/shaders/Shader.tsx
@@ -4,9 +4,10 @@ import type { ReactNode } from "react";
 import { isShader } from "../../../skia";
 import type { IRuntimeEffect } from "../../../skia";
 import { useDeclaration } from "../../nodes/Declaration";
-import type { AnimatedProps } from "../../processors/Animations/Animations";
+import type { AnimatedProps, TransformProps } from "../../processors";
+import { localMatrix } from "../../processors";
 
-export interface ShaderProps {
+export interface ShaderProps extends TransformProps {
   source: IRuntimeEffect;
   uniforms: number[];
   isOpaque?: boolean;
@@ -16,11 +17,12 @@ export interface ShaderProps {
 export const Shader = (props: AnimatedProps<ShaderProps>) => {
   const declaration = useDeclaration<ShaderProps>(
     props,
-    ({ uniforms, source, isOpaque }, children) => {
+    ({ uniforms, source, isOpaque, ...transform }, children) => {
       return source.makeShaderWithChildren(
         uniforms,
         isOpaque,
-        children.filter(isShader)
+        children.filter(isShader),
+        localMatrix(transform)
       );
     }
   );
