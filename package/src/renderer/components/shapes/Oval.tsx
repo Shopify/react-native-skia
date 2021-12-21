9d30d3e56747a0eda86ccb220781ebfa069d8fdf
William Candillon <wcandillon@gmail.com>
2021-12-21 09:50:28 +0100

    Minor API refactoring. fixes #42

--- package/src/renderer/components/shapes/Oval.tsx
+++ package/src/renderer/components/shapes/Oval.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import type { CustomPaintProps } from "../../processors";
-import type { RectDef, ChildrenProps } from "../../processors/Shapes";
+import type { RectDef } from "../../processors/Shapes";
 import { processRect } from "../../processors/Shapes";
 import type { AnimatedProps } from "../../processors/Animations/Animations";
 import { useDrawing } from "../../nodes/Drawing";
 
-export type OvalProps = RectDef & CustomPaintProps & ChildrenProps;
+export type OvalProps = RectDef & CustomPaintProps;
 
 export const Oval = (props: AnimatedProps<OvalProps>) => {
   const onDraw = useDrawing(props, ({ canvas, paint }, rectProps) => {
