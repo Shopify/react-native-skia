651f8916e57a69e49e7a09ee954883def9230ca1
William Candillon <wcandillon@gmail.com>
2022-01-05 10:05:52 +0100

    Fix rectangle processing

--- package/src/renderer/typeddash.ts
+++ package/src/renderer/typeddash.ts
@@ -1,8 +1,5 @@
 export const mapKeys = <T>(obj: T) => Object.keys(obj) as (keyof T)[];
 
-export const hasProperty = (obj: unknown, key: string) =>
-  !!(typeof obj === "object" && obj !== null && key in obj);
-
 export const exhaustiveCheck = (a: never): never => {
   throw new Error(`Unexhaustive handling for ${a}`);
 };
