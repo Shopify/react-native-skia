878222a4f1779761c17a512fc34a449b89b2da5a
William Candillon <wcandillon@gmail.com>
2022-01-21 12:20:37 +0100

    revert example

--- example/src/Examples/API/SVG.tsx
+++ example/src/Examples/API/SVG.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Dimensions } from "react-native";
-import { Canvas, ImageSVG, Skia, useSVG } from "@shopify/react-native-skia";
+import { Canvas, ImageSVG, useSVG } from "@shopify/react-native-skia";
 
 const { width, height } = Dimensions.get("window");
 
-const svg = Skia.SVG.MakeFromString(
-  "<svg width='290' height='500' viewBox='0 0 290 500' xmlns='http://www.w3.org/2000/svg'><circle cx='31' cy='325' r='120px' fill='#c02aaa'/></svg>"
-);
-
 export const SVG = () => {
-  // const svg = useSVG(require("./tiger.svg"));
+  const svg = useSVG(require("./tiger.svg"));
   if (svg === null) {
     return null;
   }
