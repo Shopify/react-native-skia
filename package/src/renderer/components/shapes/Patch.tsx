70a644ca05f22a2d668796136de71a6ab5257a12
William Candillon <william.candillon@shopify.com>
2022-01-27 09:32:44 +0100

    Document Coons patches (#143)

--- package/src/renderer/components/shapes/Patch.tsx
+++ package/src/renderer/components/shapes/Patch.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-import type { CustomPaintProps, SkEnum } from "../../processors";
+import type { CustomPaintProps, SkEnum, Vector } from "../../processors";
 import { enumKey, processColor } from "../../processors";
 import type { IPoint } from "../../../skia";
 import { BlendMode } from "../../../skia/Paint/BlendMode";
 import type { AnimatedProps } from "../../processors/Animations/Animations";
 import { useDrawing } from "../../nodes/Drawing";
 
+interface CubicBezier {
+  src: Vector;
+  c1: Vector;
+  c2: Vector;
+}
+
 export interface PatchProps extends CustomPaintProps {
   colors: string[];
-  cubics: IPoint[];
+  cubics: [CubicBezier, CubicBezier, CubicBezier, CubicBezier];
   texs?: IPoint[];
   blendMode?: SkEnum<typeof BlendMode>;
 }
@@ -18,9 +24,11 @@ export const Patch = (props: AnimatedProps<PatchProps>) => {
   const onDraw = useDrawing(
     props,
     ({ canvas, paint, opacity }, { colors, cubics, texs, blendMode }) => {
-      const mode = blendMode ? BlendMode[enumKey(blendMode)] : undefined;
+      // If the colors are provided, the default blendMode is set to dstOver, if not, the default is set to srcOver
+      const defaultBlendMode = colors ? BlendMode.DstOver : BlendMode.SrcOver;
+      const mode = blendMode ? BlendMode[enumKey(blendMode)] : defaultBlendMode;
       canvas.drawPatch(
-        cubics,
+        cubics.map(({ src, c1, c2 }) => [src, c1, c2]).flat(),
         colors.map((c) => processColor(c, opacity)),
         texs,
         mode,
