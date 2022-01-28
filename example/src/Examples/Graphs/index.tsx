31503822225d7f36802b57933a68a3ab89bcdbbc
chrfalch <christian.falch@gmail.com>
2022-01-28 13:13:23 +0100

    Adjusted order of examples.

--- example/src/Examples/Graphs/index.tsx
+++ example/src/Examples/Graphs/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, useWindowDimensions, View } from "react-native";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 
 import { Interpolation } from "./Interpolation";
 import { MountAnimation } from "./Mount";
@@ -11,9 +11,9 @@ export const GraphsScreen: React.FC = () => {
   const { width, height } = useWindowDimensions();
   return (
     <View style={styles.container}>
+      <MountAnimation height={height * 0.25} width={width - Padding * 2} />
       <Interpolation height={height * 0.25} width={width - Padding * 2} />
       <Slider height={height * 0.25} width={width - Padding * 2} />
-      <MountAnimation height={height * 0.25} width={width - Padding * 2} />
     </View>
   );
 };
