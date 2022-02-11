65cce79f02ff5265126cf0c5934ebc5566f2c242
chrfalch <christian.falch@gmail.com>
2022-02-11 13:57:44 +0100

    Added simple animation example using only values

---
+++ example/src/Examples/Animation/SimpleAnimation.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import {
+  Canvas,
+  Fill,
+  Rect,
+  useDerivedValue,
+  useTimestamp,
+} from "@shopify/react-native-skia";
+
+import { AnimationDemo, Padding, Size } from "./Components";
+
+export const SimpleAnimation = () => {
+  const { width } = useWindowDimensions();
+  const timestamp = useTimestamp();
+  const normalized = useDerivedValue((t) => (t / 1000) % 1.0, [timestamp]);
+  const rect = useDerivedValue(
+    (p) => ({ x: Padding, y: 30, width: p * width - Size, height: Size }),
+    [normalized]
+  );
+  return (
+    <AnimationDemo title={"Basic animation with derived values"}>
+      <Canvas style={styles.canvas}>
+        <Fill color="white" />
+        <Rect rect={rect} color="#8556E5" />
+      </Canvas>
+    </AnimationDemo>
+  );
+};
+
+const styles = StyleSheet.create({
+  canvas: {
+    height: 80,
+    width: "100%",
+    backgroundColor: "#FEFEFE",
+  },
+});
--- example/src/Examples/Animation/index.tsx
+++ example/src/Examples/Animation/index.tsx
@@ -4,10 +4,12 @@ import { View, StyleSheet } from "react-native";
 import { AnimateTextOnPath } from "./AnimateTextOnPath";
 import { AnimationWithTouchHandler } from "./AnimationWithTouchHandler";
 import { InterpolationWithEasing } from "./InterpolationWithEasing";
+import { SimpleAnimation } from "./SimpleAnimation";
 
 export const AnimationExample: React.FC = () => {
   return (
     <View style={styles.container}>
+      <SimpleAnimation />
       <AnimationWithTouchHandler />
       <InterpolationWithEasing />
       <AnimateTextOnPath />
