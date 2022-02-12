036969c6103668c794c618b9b410e500a77c9b9f
chrfalch <christian.falch@gmail.com>
2022-02-12 21:00:11 +0100

    Added useValueEffect

--- package/src/values/hooks/index.ts
+++ package/src/values/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./useTimestamp";
 export * from "./useDerivedValue";
 export * from "./useValue";
+export * from "./useValueEffect";
---
+++ package/src/values/hooks/useValueEffect.ts
@@ -0,0 +1,18 @@
+import { useEffect, useMemo } from "react";
+
+import type { IReadonlyValue } from "../types";
+
+/**
+ * Sets up an effect that will be run whenever the value changes
+ * @param value Value to subscribe to changes on
+ * @param cb Callback to run when value changes
+ */
+export const useValueEffect = <T>(
+  value: IReadonlyValue<T>,
+  cb: (v: T) => void
+) => {
+  const memoizedCb = useMemo(() => cb, [cb]);
+  useEffect(() => {
+    return value.addListener(memoizedCb);
+  }, [memoizedCb, value]);
+};
