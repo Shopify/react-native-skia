ff2ad2483a9ba3ff0df2ea26553001cbf4bc97f4
William Candillon <wcandillon@gmail.com>
2022-02-12 11:47:17 +0100

    :wrench:

--- package/src/renderer/processors/index.ts
+++ package/src/renderer/processors/index.ts
@@ -1,6 +1,7 @@
 export * from "./Paint";
 export * from "./Transform";
 export * from "./Animations";
+export * from "./Circles";
 export * from "./Shapes";
 export * from "./math";
 export * from "./Font";
