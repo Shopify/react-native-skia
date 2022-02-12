7bb36b2a932881c54c2a1282b4eec3ef7f896bb4
chrfalch <christian.falch@gmail.com>
2022-02-12 21:00:22 +0100

    Finished touch handler demo

--- example/src/Examples/Animation/AnimationWithTouchHandler.tsx
+++ example/src/Examples/Animation/AnimationWithTouchHandler.tsx
@@ -4,10 +4,10 @@ import {
   Canvas,
   Circle,
   Fill,
-  runSpring,
-  Spring,
+  useTimestamp,
   useTouchHandler,
   useValue,
+  useValueEffect,
 } from "@shopify/react-native-skia";
 
 import { AnimationDemo, Size, Padding } from "./Components";
@@ -15,21 +15,48 @@ import { AnimationDemo, Size, Padding } from "./Components";
 const { width } = Dimensions.get("window");
 
 export const AnimationWithTouchHandler = () => {
+  // Timestamp for driving the animation
+  const timestamp = useTimestamp();
+
+  // Translate X value for the circle
   const translateX = useValue((width - Size - Padding) / 2);
+  // Offset to let us pick up the circle from anywhere
   const offsetX = useValue(0);
+  // The circle's velocity
+  const circleVelocity = useValue(0);
+
+  // Effect that will listen for updates on the timestamp and
+  // calculate the next position of the cicrle
+  useValueEffect(timestamp, () => {
+    const leftBoundary = Size;
+    const rightBoundary = width - Size - Padding;
+    let nextValue = translateX.value + circleVelocity.value;
+    if (nextValue <= leftBoundary || nextValue >= rightBoundary) {
+      // Reverse direction
+      circleVelocity.value *= -1;
+      nextValue = Math.max(leftBoundary, Math.min(rightBoundary, nextValue));
+      // Reduce force on the circle
+      circleVelocity.value *= 0.75;
+    }
+    translateX.value = nextValue;
+    circleVelocity.value *= 0.95;
+  });
+
+  // Touch handler
   const touchHandler = useTouchHandler({
     onStart: ({ x }) => {
+      timestamp.stop();
       offsetX.value = x - translateX.value;
-      // Take over the value (stop any animations)
-      translateX.setDriver(undefined);
     },
-    onActive: ({ x }) => (translateX.value = x - offsetX.value),
-    onEnd: ({ velocityX }) => {
-      runSpring(
-        translateX,
-        (width - Size - Padding) / 2,
-        Spring.Wobbly({ velocity: velocityX })
+    onActive: ({ x }) => {
+      translateX.value = Math.max(
+        Size,
+        Math.min(width - Size - Padding, x - offsetX.value)
       );
+    },
+    onEnd: ({ velocityX }) => {
+      circleVelocity.value = -(velocityX * 0.015);
+      timestamp.start();
     },
   });
 
@@ -37,8 +64,8 @@ export const AnimationWithTouchHandler = () => {
     <AnimationDemo title={"Animation with touch handler."}>
       <Canvas style={styles.canvas} onTouch={touchHandler}>
         <Fill color="white" />
-        <Circle cx={translateX} cy={40} r={20} color="#EEE" />
-        <Circle cx={translateX} cy={40} r={15} color="#CECECE" />
+        <Circle cx={translateX} cy={40} r={20} color="#3E3E" />
+        <Circle cx={translateX} cy={40} r={15} color="#AEAE" />
       </Canvas>
     </AnimationDemo>
   );
