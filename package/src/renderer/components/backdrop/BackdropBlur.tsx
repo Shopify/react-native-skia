9c97ab195b767961919f3a24df1dd36836d981dc
William Candillon <wcandillon@gmail.com>
2022-03-04 13:08:26 +0100

    Fix build

--- package/src/renderer/components/backdrop/BackdropBlur.tsx
+++ package/src/renderer/components/backdrop/BackdropBlur.tsx
@@ -6,7 +6,7 @@ import type { AnimatedProps } from "../../processors";
 import type { BackdropFilterProps } from "./BackdropFilter";
 import { BackdropFilter } from "./BackdropFilter";
 
-interface BackdropBlurProps extends BackdropFilterProps {
+interface BackdropBlurProps extends Omit<BackdropFilterProps, "filter"> {
   blur: number;
 }
 
