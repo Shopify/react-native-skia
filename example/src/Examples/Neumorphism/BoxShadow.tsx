ee25f6b84b230fe3a95197fad2e1be0c36655634
William Candillon <wcandillon@gmail.com>
2022-03-22 16:29:15 +0100

    Improve shadow implementation

--- example/src/Examples/Neumorphism/BoxShadow.tsx
+++ example/src/Examples/Neumorphism/BoxShadow.tsx
@@ -12,12 +12,15 @@ import {
   rrect,
   rect,
   RoundedRect,
+  Circle,
+  Shadow,
 } from "@shopify/react-native-skia";
 import { Dimensions } from "react-native";
 
-const { width, height } = Dimensions.get("window");
-const c = vec(width / 2, height / 2);
+const { width } = Dimensions.get("window");
 const r = 150;
+const c = vec(width / 2, r);
+const c1 = vec(width / 2, 75 + 3 * r);
 
 const rct = rrect(rect(c.x - r, c.y - r, 2 * r, 2 * r), r, r);
 
@@ -28,38 +31,81 @@ interface BoxShadowProps {
   box: SkRRect;
   color: Color;
   inner?: boolean;
+  children?: React.ReactNode;
 }
 
-const BoxShadow = ({ box, blur, dx, dy, color }: BoxShadowProps) => {
+const BoxShadow = ({
+  box,
+  blur,
+  dx,
+  dy,
+  color,
+  inner,
+  children,
+}: BoxShadowProps) => {
   const padding = add(vec(10, 10), vec(Math.abs(dx), Math.abs(dy)));
+  const ajustedBox = rrect(
+    rect(box.rect.x + dx, box.rect.y + dy, box.rect.width, box.rect.height),
+    box.rx,
+    box.ry
+  );
+  if (!inner) {
+    return (
+      <>
+        <Group>
+          <Paint color={color}>
+            <BlurMask blur={blur * 2} style="normal" />
+          </Paint>
+          <RoundedRect rect={ajustedBox} />
+        </Group>
+        {children}
+      </>
+    );
+  }
   return (
-    <Group clip={box}>
-      <Paint color={color}>
-        <BlurMask blur={blur} style="solid" />
-      </Paint>
-      <DiffRect
-        inner={box}
-        outer={rrect(
-          rect(
-            box.rect.x - padding.x,
-            box.rect.y - padding.y,
-            box.rect.width + 2 * padding.x,
-            box.rect.height + 2 * padding.y
-          ),
-          0,
-          0
-        )}
-      />
-    </Group>
+    <>
+      {children}
+      <Group clip={box}>
+        <Paint color={color}>
+          <BlurMask blur={blur * 2} style="normal" />
+        </Paint>
+        <DiffRect
+          inner={ajustedBox}
+          outer={rrect(
+            rect(
+              ajustedBox.rect.x - padding.x,
+              ajustedBox.rect.y - padding.y,
+              ajustedBox.rect.width + 2 * padding.x,
+              box.rect.height + 2 * padding.y
+            ),
+            0,
+            0
+          )}
+        />
+      </Group>
+    </>
   );
 };
 
 export const Neumorphism = () => {
+  const dx = 10;
+  const dy = 10;
   return (
-    <Canvas style={{ flex: 1 }}>
+    <Canvas style={{ flex: 1 }} mode="continuous" debug>
       <Fill color="lightblue" />
-      <RoundedRect rect={rct} color="white" />
-      <BoxShadow box={rct} color="blue" dx={0} dy={0} blur={15} inner />
+      <BoxShadow box={rct} color="blue" dx={dx} dy={dy} blur={15} inner>
+        <RoundedRect rect={rct} color="white" />
+      </BoxShadow>
+      {/* <Group transform={[{ translateY: 300 }]}>
+        <RoundedRect rect={rct} color="white" />
+        <BoxShadow box={rct} color="blue" dx={dx} dy={dy} blur={15} inner />
+      </Group> */}
+      <Group>
+        <Paint>
+          <Shadow dx={dx} dy={dy} blur={15} color="blue" inner />
+        </Paint>
+        <Circle c={c1} r={r} color="white" />
+      </Group>
     </Canvas>
   );
 };
