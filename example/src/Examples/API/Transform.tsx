5a372e0487bce6e3f339bb381274e871547be866
Manuel Schiller <manuel.schiller@caligano.de>
2022-03-24 22:53:49 +0100

    fix Transform example
    `image` must be listed as dependency of `useDrawCallback`

--- example/src/Examples/API/Transform.tsx
+++ example/src/Examples/API/Transform.tsx
@@ -17,7 +17,7 @@ const SIZE = width;
 const center = { x: SIZE / 2, y: SIZE / 2 };
 const aspectRatio = 836 / 1324;
 const CARD_WIDTH = width - 64;
-const CARD_HEIHT = CARD_WIDTH * aspectRatio;
+const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
 
 const paint = Skia.Paint();
 paint.setAntiAlias(true);
@@ -32,69 +32,78 @@ strokePaint.setStrokeWidth(2);
 export const Transform = () => {
   const image = useImage(card);
 
-  const onRotateDraw = useDrawCallback((canvas) => {
-    const rect = Skia.XYWHRect(
-      center.x - CARD_WIDTH / 2,
-      center.y - CARD_HEIHT / 2,
-      CARD_WIDTH,
-      CARD_HEIHT
-    );
-    if (image) {
-      const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
-      canvas.save();
-      canvas.rotate(-30, center.x, center.y);
+  const onRotateDraw = useDrawCallback(
+    (canvas) => {
+      const rect = Skia.XYWHRect(
+        center.x - CARD_WIDTH / 2,
+        center.y - CARD_HEIGHT / 2,
+        CARD_WIDTH,
+        CARD_HEIGHT
+      );
+      if (image) {
+        const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
+        canvas.save();
+        canvas.rotate(-30, center.x, center.y);
 
-      //  we pivot on the center of the card
-      canvas.translate(center.x, center.y);
-      canvas.scale(0.75, 0.75);
-      canvas.translate(-center.x, -center.y);
+        //  we pivot on the center of the card
+        canvas.translate(center.x, center.y);
+        canvas.scale(0.75, 0.75);
+        canvas.translate(-center.x, -center.y);
 
-      canvas.drawImageRect(image, imgRect, rect, paint);
-      canvas.restore();
-    }
-  }, []);
+        canvas.drawImageRect(image, imgRect, rect, paint);
+        canvas.restore();
+      }
+    },
+    [image]
+  );
 
-  const onSkewDraw = useDrawCallback((canvas) => {
-    const rect = Skia.XYWHRect(
-      center.x - CARD_WIDTH / 2,
-      center.y - CARD_HEIHT / 2,
-      CARD_WIDTH,
-      CARD_HEIHT
-    );
-    if (image) {
-      const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
-      canvas.save();
+  const onSkewDraw = useDrawCallback(
+    (canvas) => {
+      const rect = Skia.XYWHRect(
+        center.x - CARD_WIDTH / 2,
+        center.y - CARD_HEIGHT / 2,
+        CARD_WIDTH,
+        CARD_HEIGHT
+      );
+      if (image) {
+        const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
+        canvas.save();
 
-      //  we pivot on the center of the card
-      canvas.translate(center.x, center.y);
-      canvas.skew(-Math.PI / 6, 0);
-      canvas.translate(-center.x, -center.y);
+        //  we pivot on the center of the card
+        canvas.translate(center.x, center.y);
+        canvas.skew(-Math.PI / 6, 0);
+        canvas.translate(-center.x, -center.y);
 
-      canvas.drawImageRect(image, imgRect, rect, paint);
-      canvas.restore();
-    }
-  }, []);
+        canvas.drawImageRect(image, imgRect, rect, paint);
+        canvas.restore();
+      }
+    },
+    [image]
+  );
 
-  const onMatrixDraw = useDrawCallback((canvas) => {
-    const rect = Skia.XYWHRect(
-      center.x - CARD_WIDTH / 2,
-      center.y - CARD_HEIHT / 2,
-      CARD_WIDTH,
-      CARD_HEIHT
-    );
-    if (image) {
-      const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
-      canvas.save();
+  const onMatrixDraw = useDrawCallback(
+    (canvas) => {
+      const rect = Skia.XYWHRect(
+        center.x - CARD_WIDTH / 2,
+        center.y - CARD_HEIGHT / 2,
+        CARD_WIDTH,
+        CARD_HEIGHT
+      );
+      if (image) {
+        const imgRect = Skia.XYWHRect(0, 0, image.width(), image.height());
+        canvas.save();
 
-      //  we pivot on the center of the card
-      canvas.translate(center.x, center.y);
-      canvas.skew(-Math.PI / 6, 0);
-      canvas.translate(-center.x, -center.y);
+        //  we pivot on the center of the card
+        canvas.translate(center.x, center.y);
+        canvas.skew(-Math.PI / 6, 0);
+        canvas.translate(-center.x, -center.y);
 
-      canvas.drawImageRect(image, imgRect, rect, paint);
-      canvas.restore();
-    }
-  }, []);
+        canvas.drawImageRect(image, imgRect, rect, paint);
+        canvas.restore();
+      }
+    },
+    [image]
+  );
 
   return (
     <ScrollView>
