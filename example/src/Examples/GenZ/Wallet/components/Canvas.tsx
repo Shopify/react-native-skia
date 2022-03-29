f40302b006a0b8d39f802fe248d3d1c18da8c20c
William Candillon <wcandillon@gmail.com>
2022-03-29 16:48:16 +0200

    :wrench:

--- example/src/Examples/GenZ/Wallet/components/Canvas.tsx
+++ example/src/Examples/GenZ/Wallet/components/Canvas.tsx
@@ -2,9 +2,19 @@ import { vec } from "@shopify/react-native-skia";
 
 const width = 375;
 const height = 812;
+export const Typefaces = {
+  DMSansRegular: require("../../assets/DM_Sans/DMSans-Regular.ttf"),
+  DMSansMedium: require("../../assets/DM_Sans/DMSans-Medium.ttf"),
+};
 
 export const CANVAS = {
   width: 375,
   height: 812,
   center: vec(width / 2, height / 2),
+};
+
+export const Images = {
+  CHF: require("../../assets/chf.png"),
+  EUR: require("../../assets/eur.png"),
+  USD: require("../../assets/usd.png"),
 };
