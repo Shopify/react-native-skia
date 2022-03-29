808ba7cca6b80c52cda219a44b79d2e8cf8c85c8
William Candillon <wcandillon@gmail.com>
2022-03-29 17:02:02 +0200

    :wrench:

--- example/src/Examples/GenZ/Wallet/components/Asset.tsx
+++ example/src/Examples/GenZ/Wallet/components/Asset.tsx
@@ -6,28 +6,37 @@ import {
   Rect,
   Image,
   RoundedRect,
+  Text,
 } from "@shopify/react-native-skia";
 import React from "react";
+
+import { useFont } from "../../components/AssetProvider";
 
 interface AssetProps {
   image: SkImage;
   title: string;
   subtitle: string;
-  value: number;
+  value: string;
   y: number;
+  plus?: boolean;
 }
 
 export const Asset = ({
-  children,
   title,
   subtitle,
   value,
   y,
   image,
+  plus,
 }: AssetProps) => {
+  const valueFont = useFont("DMSansMedium", 16);
+  const titleFont = useFont("DMSansMedium", 14);
+  const subtitleFont = useFont("DMSansMedium", 12);
+  const titlePos = titleFont.measureText(title);
+  const subtitlePos = subtitleFont.measureText(subtitle);
+  const valuePos = valueFont.measureText(value);
   return (
     <Group x={8} y={y}>
-      <Rect x={0} y={0} width={359} height={72} color="blue" />
       <Group>
         <Paint>
           <ImageShader
@@ -41,6 +50,27 @@ export const Asset = ({
         </Paint>
         <RoundedRect x={16} y={16} width={40} height={40} r={12} />
       </Group>
+      <Text
+        text={title}
+        x={72}
+        y={16.5 + titlePos.height}
+        font={titleFont}
+        color="#1E1E20"
+      />
+      <Text
+        text={subtitle}
+        x={72}
+        y={16.5 + 22 + subtitlePos.height}
+        font={subtitleFont}
+        color="rgba(172, 172, 176, 0.8)"
+      />
+      <Text
+        text={value}
+        x={286}
+        y={25 + valuePos.height}
+        font={valueFont}
+        color={plus ? "#20BC56" : "#1A1B27"}
+      />
     </Group>
   );
 };
