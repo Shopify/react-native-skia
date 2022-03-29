e634f36f614529bcbc654794eadc759771574869
William Candillon <wcandillon@gmail.com>
2022-03-29 12:55:32 +0200

    :wrench:

--- example/src/Examples/GenZ/Wallet/components/Topbar.tsx
+++ example/src/Examples/GenZ/Wallet/components/Topbar.tsx
@@ -1,7 +1,11 @@
+import { Text } from "@shopify/react-native-skia";
 import React from "react";
+
+import { useFont } from "../../components/AssetProvider";
 
 //interface TopbarProps {}
 
 export const Topbar = () => {
-  return <Text text="Wallet" font={font} />;
+  const font = useFont("DMSansMedium", 24);
+  return <Text text="Wallet" font={font} x={0} y={0} />;
 };
