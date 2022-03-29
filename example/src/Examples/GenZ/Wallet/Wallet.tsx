1e78ee8c98801e7d924733fb810a7157c016a655
William Candillon <wcandillon@gmail.com>
2022-03-29 12:51:26 +0200

    :wrench:

--- example/src/Examples/GenZ/Wallet/Wallet.tsx
+++ example/src/Examples/GenZ/Wallet/Wallet.tsx
@@ -1,6 +1,8 @@
-import { Canvas, FitBox, rect } from "@shopify/react-native-skia";
+import { Canvas, Fill, FitBox, rect } from "@shopify/react-native-skia";
 import React from "react";
 import { Dimensions } from "react-native";
+
+import { AssetProvider } from "../components/AssetProvider";
 
 const { width: w, height: h } = Dimensions.get("window");
 const width = 375;
@@ -12,12 +14,12 @@ export const Wallet = () => {
   return (
     <Canvas style={{ flex: 1 }}>
       <AssetProvider
-        fonts={{
+        typefaces={{
           DMSansRegular: require("../assets/DM_Sans/DMSans-Regular.ttf"),
         }}
       >
         <FitBox src={src} dst={dst}>
-          <Topbar />
+          <Fill color="red" />
         </FitBox>
       </AssetProvider>
     </Canvas>
