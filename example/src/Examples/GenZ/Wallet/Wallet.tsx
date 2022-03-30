d76dc69ef8ddafd46be1edd7a24118f49e7aa38f
William Candillon <wcandillon@gmail.com>
2022-03-30 08:55:52 +0200

    :wrench:

--- example/src/Examples/GenZ/Wallet/Wallet.tsx
+++ example/src/Examples/GenZ/Wallet/Wallet.tsx
@@ -23,6 +23,7 @@ import { Actions } from "./components/Actions";
 import { Modal } from "./components/Modal";
 import { Tabbar } from "./components/Tabbar";
 import { CANVAS, Images, Typefaces } from "./components/Canvas";
+import { Graph } from "./components/Graph";
 
 const { width: w, height: h } = Dimensions.get("window");
 const { width, height } = CANVAS;
@@ -31,8 +32,8 @@ const dst = rect(0, 0, w, h);
 const transform = fitbox("cover", src, dst);
 const pagination = rect(0, 340, width, 21);
 
-const isInRect = (pt: Vector, { x, y, width: w1, height: h1 }: SkRect) =>
-  pt.x >= x && pt.x <= x + w1 && pt.y >= y && pt.y <= y + h1;
+// const isInRect = (pt: Vector, { x, y, width: w1, height: h1 }: SkRect) =>
+//   pt.x >= x && pt.x <= x + w1 && pt.y >= y && pt.y <= y + h1;
 
 export const Wallet = () => {
   const translateX = useValue(0);
@@ -42,7 +43,7 @@ export const Wallet = () => {
   );
   const onTouch = useTouchHandler({
     onStart: (pt) => {
-      if (isInRect(pt, pagination)) {
+      if (pt.y > 346 * transform[3].scaleY) {
         runTiming(translateX, translateX.current !== 0 ? 0 : -width, {
           duration: 600,
         });
@@ -57,6 +58,9 @@ export const Wallet = () => {
           <Topbar />
           <Group transform={tr}>
             <Card />
+            <Group transform={[{ translateX: width }]}>
+              <Graph />
+            </Group>
           </Group>
           <Circle cx={176} cy={346} r={4} color="#828282" />
           <Circle cx={191} cy={346} r={4} color="#BDBDBD" />
@@ -64,7 +68,6 @@ export const Wallet = () => {
           <Modal />
           <Tabbar />
         </Group>
-        <Rect rect={pagination} color="red" />
       </AssetProvider>
     </Canvas>
   );
