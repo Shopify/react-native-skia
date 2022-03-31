88267497ce8ef46130d5416b09424b125b849b86
William Candillon <wcandillon@gmail.com>
2022-03-31 06:02:44 +0200

    :wrench:

--- example/src/Examples/Vertices/Vertices.tsx
+++ example/src/Examples/Vertices/Vertices.tsx
@@ -1,103 +1,65 @@
+import type { Vector } from "@shopify/react-native-skia";
 import {
+  useClockValue,
+  useDerivedValue,
+  Group,
+  mix,
+  vec,
   Points,
   Canvas,
-  Fill,
-  useValue,
-  useTouchHandler,
-  useDerivedValue,
   Vertices,
 } from "@shopify/react-native-skia";
 import React from "react";
 import { Dimensions } from "react-native";
+import SimplexNoise from "simplex-noise";
 
-import type { Matrix4, Transforms3d } from "./Matrix4";
-import { matrixVecMul4, processTransform3d } from "./Matrix4";
-
-const vec3 = (x: number, y: number, z: number) => ({ x, y, z });
-interface Vector3 {
-  x: number;
-  y: number;
-  z: number;
-}
+/// <reference path="cdt2d.d.ts" />
+import cdt2d from "cdt2d";
 
 const { width, height } = Dimensions.get("window");
-const TAU = Math.PI / 2;
-const COLS = 3;
-const ROWS = 8;
-const size = width / COLS;
+const COLS = 6;
+const ROWS = COLS * 2;
+const size = width / (COLS - 1);
 const cols = new Array(COLS).fill(0).map((_, i) => i);
 const rows = new Array(ROWS).fill(0).map((_, i) => i);
-const vertices: Vector3[] = [];
+const points: Vector[] = [];
 rows.forEach((i) => {
   cols.forEach((j) => {
-    vertices.push(vec3(j * size, i * size, Math.random() * size));
+    points.push(
+      vec(
+        j * size * mix(Math.random(), 0.5, 1),
+        i * size * mix(Math.random(), 0.5, 1)
+      )
+    );
   });
 });
-const center = vec3(width / 2, height / 2, 0);
 
-export const transformOrigin3d = (
-  { x, y, z }: Vector3,
-  transformations: Transforms3d
-): Transforms3d => {
-  return [
-    { translateX: x },
-    { translateY: y },
-    { translateZ: z },
-    ...transformations,
-    { translateX: -x },
-    { translateY: -y },
-    { translateZ: -z },
-  ];
-};
+const baseColors = ["#61DAFB", "#fb61da", "#dafb61", "#61fbcf"];
+const vertexColors = points.map((_, i) => baseColors[i % baseColors.length]);
 
-export const project = (p: Vector3, transform: Matrix4): Vector3 => {
-  "worklet";
-  const pr = matrixVecMul4(transform, [p.x, p.y, p.z, 1]);
-  return {
-    x: pr[0] / pr[3],
-    y: pr[1] / pr[3],
-    z: pr[2] / pr[3],
-  };
-};
-
-const base = ["#61dafb", "#fb61da", "#dafb61"];
-const colors = vertices.map((_, i) => base[i % base.length]);
+const triangles = cdt2d(points.map(({ x, y }) => [x, y])).flat();
+const defaultVertices = triangles.map((triangle) => points[triangle]);
+const colors = triangles.map((triangle) => vertexColors[triangle]);
+const noises = defaultVertices.map((_, i) => new SimplexNoise(`${i}`));
+const A = 50;
+const F = 5000;
 
 export const Demo = () => {
-  const offsetX = useValue(0);
-  const offsetY = useValue(0);
-  const rotateX = useValue(0);
-  const rotateY = useValue(0);
-  const onTouch = useTouchHandler({
-    onStart: () => {
-      offsetX.current = rotateX.current;
-      offsetY.current = rotateY.current;
-    },
-    onActive: (pt) => {
-      rotateX.current = (TAU * pt.y) / width;
-      rotateY.current = (TAU * pt.x) / height;
-    },
-  });
-  const points = useDerivedValue(
-    () =>
-      vertices.map((point) => {
-        return project(
-          point,
-          processTransform3d(
-            transformOrigin3d(center, [
-              { rotateX: rotateX.current },
-              { rotateY: rotateY.current },
-            ])
-          )
-        );
-      }),
-    [rotateX, rotateY]
-  );
+  const clock = useClockValue();
+  const vertices = useDerivedValue(() => {
+    return defaultVertices.map(({ x, y }, i) =>
+      vec(
+        x + noises[i].noise2D(clock.current / F, 0) * A,
+        y + noises[i].noise2D(0, clock.current / F) * A
+      )
+    );
+  }, [clock]);
   return (
-    <Canvas style={{ width, height }} onTouch={onTouch}>
-      <Fill color="white" />
-      <Points points={points} color="black" style="stroke" strokeWidth={4} />
-      <Vertices vertices={points} colors={colors} />
+    <Canvas style={{ width, height }}>
+      <Group transform={[{ scale: 1.5 }]}>
+        <Vertices vertices={vertices} colors={colors} />
+        <Points points={points} color="black" style="stroke" strokeWidth={4} />
+      </Group>
     </Canvas>
   );
 };
