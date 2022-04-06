86bf5c96a52fce2ee16a1a31b4062e60a74df415
William Candillon <wcandillon@gmail.com>
2022-04-06 11:57:52 +0200

    Clean up unused examples (#344)

--- example/src/Examples/API/Gradients.tsx
+++ example/src/Examples/API/Gradients.tsx
@@ -2,105 +2,90 @@ import React from "react";
 import { StyleSheet, Dimensions, ScrollView } from "react-native";
 import {
   Skia,
-  useDrawCallback,
-  SkiaView,
-  BlendMode,
-  TileMode,
+  rect,
+  Canvas,
+  Rect,
+  LinearGradient,
+  Paint,
+  topLeft,
+  bottomRight,
+  center,
+  RadialGradient,
+  TwoPointConicalGradient,
+  SweepGradient,
+  vec,
+  Turbulence,
+  ColorShader,
+  Blend,
 } from "@shopify/react-native-skia";
 
 const { width } = Dimensions.get("window");
 const SIZE = width / 2;
+const R = SIZE / 2;
 
 const paint = Skia.Paint();
 paint.setAntiAlias(true);
+const r1 = rect(0, 0, SIZE, SIZE);
+const r2 = rect(SIZE, 0, SIZE, SIZE);
+const r3 = rect(0, SIZE, SIZE, SIZE);
+const r4 = rect(SIZE, SIZE, SIZE, SIZE);
+const r5 = rect(0, 2 * SIZE, SIZE, SIZE);
+const r6 = rect(SIZE, 2 * SIZE, SIZE, SIZE);
 
 export const Gradients = () => {
-  const onGradientDraw = useDrawCallback((canvas) => {
-    // 1. Linear Gradient
-    const r1 = Skia.XYWHRect(0, 0, SIZE, SIZE);
-    const p1 = paint.copy();
-    const colors = [Skia.Color("#61DAFB"), Skia.Color("#fb61da")];
-    p1.setShader(
-      Skia.Shader.MakeLinearGradient(
-        Skia.Point(0, 0),
-        Skia.Point(SIZE, SIZE),
-        colors,
-        null,
-        TileMode.Decal
-      )
-    );
-    canvas.drawRect(r1, p1);
-
-    // 2. Radial Gradient
-    const r2 = Skia.XYWHRect(SIZE, 0, SIZE, SIZE);
-    const p2 = paint.copy();
-    p2.setShader(
-      Skia.Shader.MakeRadialGradient(
-        Skia.Point(SIZE + SIZE / 2, SIZE / 2),
-        SIZE / 2,
-        colors.reverse(),
-        null,
-        TileMode.Decal
-      )
-    );
-    canvas.drawRect(r2, p2);
-
-    // 3. Two Point Canonical
-    const r3 = Skia.XYWHRect(0, SIZE, SIZE, SIZE);
-    const p3 = paint.copy();
-    const R = SIZE / 2;
-    p3.setShader(
-      Skia.Shader.MakeTwoPointConicalGradient(
-        Skia.Point(R, SIZE),
-        R,
-        Skia.Point(R, SIZE),
-        SIZE / 4,
-        colors.reverse(),
-        null,
-        TileMode.Clamp
-      )
-    );
-    canvas.drawRect(r3, p3);
-
-    // 4. Angular Gradient
-    const r4 = Skia.XYWHRect(SIZE, SIZE, SIZE, SIZE);
-    const p4 = paint.copy();
-    const sweepColors = [
-      Skia.Color("#61DAFB"),
-      Skia.Color("#fb61da"),
-      Skia.Color("#dafb61"),
-      Skia.Color("#61DAFB"),
-    ];
-    p4.setShader(
-      Skia.Shader.MakeSweepGradient(
-        SIZE + R,
-        SIZE + R,
-        sweepColors,
-        null,
-        TileMode.Clamp
-      )
-    );
-    canvas.drawRect(r4, p4);
-
-    // 5. Turbulence
-    const r5 = Skia.XYWHRect(0, 2 * SIZE, SIZE, SIZE);
-    const p5 = paint.copy();
-    // p5.setColor(Skia.Color("#61DAFB"));
-    const one = Skia.Shader.MakeSweepGradient(
-      R,
-      2 * SIZE + R,
-      sweepColors,
-      null,
-      TileMode.Clamp
-    );
-    const two = Skia.Shader.MakeTurbulence(0.05, 0.05, 4, 0, 0, 0);
-    p5.setShader(Skia.Shader.MakeBlend(BlendMode.Difference, one, two));
-    canvas.drawRect(r5, p5);
-  }, []);
-
   return (
     <ScrollView>
-      <SkiaView style={styles.container} onDraw={onGradientDraw} />
+      <Canvas style={styles.container}>
+        <Paint>
+          <LinearGradient
+            start={topLeft(r1)}
+            end={bottomRight(r1)}
+            colors={["#61DAFB", "#fb61da"]}
+          />
+        </Paint>
+        <Rect rect={r1} />
+        <Paint>
+          <RadialGradient
+            c={center(r2)}
+            r={SIZE / 2}
+            colors={["#fb61da", "#61DAFB"]}
+          />
+        </Paint>
+        <Rect rect={r2} />
+        <Paint>
+          <TwoPointConicalGradient
+            start={vec(R, SIZE)}
+            startR={R}
+            end={vec(R, R)}
+            endR={R}
+            colors={["#61DAFB", "#fb61da"]}
+          />
+        </Paint>
+        <Rect rect={r3} />
+        <Paint>
+          <SweepGradient
+            c={vec(SIZE + R, SIZE + R)}
+            colors={["#61DAFB", "#fb61da", "#dafb61", "#61DAFB"]}
+          />
+        </Paint>
+        <Rect rect={r4} />
+        <Paint>
+          <Blend mode="difference">
+            <ColorShader color="#61DAFB" />
+            <Turbulence freqX={0.05} freqY={0.05} octaves={4} />
+          </Blend>
+        </Paint>
+        <Rect rect={r5} />
+        <Paint>
+          <LinearGradient
+            start={topLeft(r6)}
+            end={bottomRight(r6)}
+            colors={["#dafb61", "#61DAFB", "#fb61da"]}
+            positions={[0, 0.85, 1]}
+          />
+        </Paint>
+        <Rect rect={r6} />
+      </Canvas>
     </ScrollView>
   );
 };
