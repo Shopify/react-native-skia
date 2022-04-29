9a2ddc2d75cf3888cbe6dcc57ab4b2088965550f
William Candillon <wcandillon@gmail.com>
2022-04-29 16:02:33 +0200

    🐛 Fix circle typechecking

--- package/src/renderer/processors/Circles.ts
+++ package/src/renderer/processors/Circles.ts
@@ -15,8 +15,9 @@ interface ScalarCircleDef {
 export type CircleDef = PointCircleDef | ScalarCircleDef;
 
 const isCircleScalarDef = (def: CircleDef): def is ScalarCircleDef =>
+  // We have an issue to check property existence on JSI backed instances
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (def as any).cx;
+  (def as any).cx !== undefined;
 export const processCircle = (def: CircleDef) => {
   if (isCircleScalarDef(def)) {
     return { c: vec(def.cx, def.cy), r: def.r };
