0746b1a72aa1d26409c2b1568e609d21db513a0f
William Candillon <wcandillon@gmail.com>
2022-05-03 13:47:12 +0200

    💚 Fix build

--- example/src/Examples/Wallet/components/Label.tsx
+++ example/src/Examples/Wallet/components/Label.tsx
@@ -2,7 +2,6 @@ import type { SkiaReadonlyValue } from "@shopify/react-native-skia";
 import {
   interpolate,
   Text,
-  interpolateColors,
   Skia,
   useDerivedValue,
 } from "@shopify/react-native-skia";
