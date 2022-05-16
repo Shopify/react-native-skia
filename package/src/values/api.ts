c4bb6fa285fd9adee29767d51bc7a7e2a6724642
chrfalch <christian.falch@gmail.com>
2022-05-16 19:10:14 +0200

    Added JS implementation of RNSkValues
    - Added RNSkReadonlyValue, RNSkValue, RNSkAnimation, RNSkClockValue with Unit tests
    
    Added switch in the api file to serve the JS implementation when platform is web.

--- package/src/values/api.ts
+++ package/src/values/api.ts
@@ -1,10 +1,19 @@
-import type { ISkiaValueApi } from "./types";
+import { Platform } from "react-native";
 
+import type { ISkiaValueApi } from "./types";
+import { ValueApi as ValueApiWeb } from "./web";
+
+let exportedValueApi: ISkiaValueApi;
 declare global {
   var SkiaValueApi: ISkiaValueApi;
 }
 
-const { SkiaValueApi } = global;
-export const ValueApi = SkiaValueApi;
+if (Platform.OS === "web") {
+  exportedValueApi = ValueApiWeb;
+} else {
+  const { SkiaValueApi } = global;
+  exportedValueApi = SkiaValueApi;
+}
+export const ValueApi = exportedValueApi;
 
 export const { createValue, createDerivedValue } = ValueApi;
