b387d572a4e58e1fe4228214f5caa2e4bcf2bd92
William Candillon <wcandillon@gmail.com>
2022-05-20 15:54:23 +0200

    Faster Rect and RRect

--- package/src/renderer/processors/Rects.ts
+++ package/src/renderer/processors/Rects.ts
@@ -1,24 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { SkRect, SkRRect } from "../../skia";
+import { Skia } from "../../skia";
 
 import { vec } from "./math/Vector";
 import type { Radius } from "./Radius";
 import { processRadius } from "./Radius";
 
-export const point = (x: number, y: number) => ({ x, y });
+export const point = (x: number, y: number) => Skia.Point(x, y);
 
-export const rect = (x: number, y: number, width: number, height: number) => ({
-  x,
-  y,
-  width,
-  height,
-});
+export const rect = (x: number, y: number, width: number, height: number) =>
+  Skia.XYWHRect(x, y, width, height);
 
-export const rrect = (r: SkRect, rx: number, ry: number) => ({
-  rect: r,
-  rx,
-  ry,
-});
+export const rrect = (r: SkRect, rx: number, ry: number) =>
+  Skia.RRectXY(r, rx, ry);
 
 export const bounds = (rects: SkRect[]) => {
   const x = Math.min(...rects.map((r) => r.x));
