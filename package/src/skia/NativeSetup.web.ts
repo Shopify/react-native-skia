211389cabcebf7a24282cb0aaafa89e2e0503762
chrfalch <christian.falch@gmail.com>
2022-06-02 12:14:45 +0200

    Added comment from CR.

--- package/src/skia/NativeSetup.web.ts
+++ package/src/skia/NativeSetup.web.ts
@@ -1,1 +1,3 @@
+// We need to remove the native version of NativeSetup - to avoid typescript errors.
+// The file should not do anything, so to avoid typescript errors we export a dummy variable
 export const dummy = 0;
