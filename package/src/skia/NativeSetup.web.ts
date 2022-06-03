82b44486deed90e923b13c9213ce2551adef031b
chrfalch <christian.falch@gmail.com>
2022-06-03 09:37:47 +0200

    Removed NativeSetup.web.ts and add platform switch instead.

--- package/src/skia/NativeSetup.web.ts
+++
@@ -1,3 +0,0 @@
-// We need to remove the native version of NativeSetup - to avoid typescript errors.
-// The file should not do anything, so to avoid typescript errors we export a dummy variable
-export const dummy = 0;
