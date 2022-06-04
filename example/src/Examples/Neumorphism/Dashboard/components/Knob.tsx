28c624e466a3b2b23a0904ed26225e7ce7fedb4c
William Candillon <wcandillon@gmail.com>
2022-06-04 17:32:04 +0200

    Update Neumorphic demo

--- example/src/Examples/Neumorphism/Dashboard/components/Knob.tsx
+++ example/src/Examples/Neumorphism/Dashboard/components/Knob.tsx
@@ -4,24 +4,20 @@ import {
   Group,
   LinearGradient,
   vec,
-  Paint,
 } from "@shopify/react-native-skia";
 
 export const Knob = () => {
   return (
     <Group>
       <Group>
-        <Paint>
-          <LinearGradient
-            start={vec(0, 0)}
-            end={vec(27.5, 17)}
-            colors={["#2E3236", "#141515"]}
-          />
-        </Paint>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(27.5, 17)}
+          colors={["#2E3236", "#141515"]}
+        />
         <RoundedRect x={0} y={0} width={27.5} height={17} r={6} />
       </Group>
       {/* <Group>
-        <Paint>
           <InnerShadow
             dx={-1}
             dy={-1}
@@ -29,11 +25,9 @@ export const Knob = () => {
             color="rgba(255, 255, 255, 0.7)"
           />
           <InnerShadow dx={1} dy={1} blur={1} color="rgba(0, 0, 0, 0.39)" />
-        </Paint>
         <RoundedRect width={3} height={13} color="#272A2E" x={9} y={2} r={1} />
       </Group>
       <Group>
-        <Paint>
           <InnerShadow
             dx={-1}
             dy={-1}
@@ -41,7 +35,6 @@ export const Knob = () => {
             color="rgba(255, 255, 255, 0.7)"
           />
           <InnerShadow dx={1} dy={1} blur={1} color="rgba(0, 0, 0, 0.39)" />
-        </Paint>
         <RoundedRect
           width={3}
           height={13}
