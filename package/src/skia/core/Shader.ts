08d8de80459af8f18e3367c2ddb69fd03457ed42
William Candillon <wcandillon@gmail.com>
2022-06-14 10:41:56 +0200

    Deprecate memoization only hooks (#577)

--- package/src/skia/core/Shader.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-
-import { Skia } from "../Skia";
-
-export const useShader = (sksl: string) =>
-  useMemo(() => {
-    console.warn("Use shader will be deprecated.");
-    return Skia.RuntimeEffect.Make(sksl);
-  }, [sksl]);
