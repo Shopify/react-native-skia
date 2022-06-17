9e6adcd466ff67dcdd71f45853dafc705518f26d
William Candillon <wcandillon@gmail.com>
2022-06-17 11:33:37 +0200

    Merge enum fix branch

--- package/src/skia/web/JsiSkPaint.ts
+++ package/src/skia/web/JsiSkPaint.ts
@@ -1,7 +1,7 @@
 import type { CanvasKit, Paint } from "canvaskit-wasm";
 
-import { StrokeJoin } from "../types";
 import type {
+  StrokeJoin,
   BlendMode,
   SkColor,
   SkColorFilter,
@@ -15,16 +15,6 @@ import type {
 } from "../types";
 
 import { HostObject, toNullableValue, ckEnum } from "./Host";
-
-// Fixme: there is an issue in canvaskit where form strokeJoin the external declaration
-// doesn't match the order of the enum
-// The current implementation relies on the enum order to be identical between the C++ and the JS code.
-// See: https://github.com/google/skia/blob/main/modules/canvaskit/externs.js#L965
-const strokeJoinMap = {
-  [StrokeJoin.Miter]: { value: 0 },
-  [StrokeJoin.Round]: { value: 1 },
-  [StrokeJoin.Bevel]: { value: 2 },
-} as const;
 
 export class JsiSkPaint extends HostObject<Paint, "Paint"> implements SkPaint {
   constructor(CanvasKit: CanvasKit, ref: Paint) {
@@ -97,7 +87,7 @@ export class JsiSkPaint extends HostObject<Paint, "Paint"> implements SkPaint {
   }
 
   setStrokeJoin(join: StrokeJoin) {
-    this.ref.setStrokeJoin(strokeJoinMap[join]);
+    this.ref.setStrokeJoin(ckEnum(join));
   }
 
   setStrokeMiter(limit: number) {
