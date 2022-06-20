a50f86ba886951627d3bd021035b8dd55cc239f7
William Candillon <wcandillon@gmail.com>
2022-06-20 14:00:41 +0200

    Update example

--- example/src/Examples/API/Warmup/Warmup.tsx
+++ example/src/Examples/API/Warmup/Warmup.tsx
@@ -5,7 +5,15 @@ import { Icon } from "./Icon";
 
 export const Warmup = () => {
   return (
-    <View style={{ flex: 1, margin: 16 }}>
+    <View
+      style={{ flex: 1, margin: 16, flexDirection: "row", flexWrap: "wrap" }}
+    >
+      <Icon name="close" size={128} />
+      <Icon name="activity" size={128} color="red" />
+      <Icon name="battery" size={128} color="purple" />
+      <Icon name="bluetooth" size={128} color="lightblue" />
+      <Icon name="book" size={64} />
+      <Icon name="cpu" size={64} />
       <Icon name="close" size={128} />
       <Icon name="activity" size={128} color="red" />
       <Icon name="battery" size={128} color="purple" />
--- package/src/skia/__tests__/Picture.spec.ts
+++ package/src/skia/__tests__/Picture.spec.ts
@@ -33,11 +33,11 @@ describe("Drawings", () => {
     paint.setColor(Skia.Color("lightblue"));
     paint.setStrokeCap(StrokeCap.Round);
     paint.setStrokeJoin(StrokeJoin.Round);
-    const path = Skia.Path.MakeFromSVGString(
+    const svgPath = Skia.Path.MakeFromSVGString(
       // eslint-disable-next-line max-len
       "M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
     )!;
-    expect(path).toBeTruthy();
+    expect(svgPath).toBeTruthy();
     canvas.save();
     canvas.concat(
       processTransform(
@@ -49,7 +49,7 @@ describe("Drawings", () => {
         )
       )
     );
-    canvas.drawPath(path, paint);
+    canvas.drawPath(svgPath, paint);
     canvas.restore();
     const picture = recorder.finishRecordingAsPicture();
 
