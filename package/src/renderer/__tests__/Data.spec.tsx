991f0c5c4ea98e3af395b98c57b96be5947c5783
William Candillon <wcandillon@gmail.com>
2022-06-22 15:11:15 +0200

    Add test collection

--- package/src/renderer/__tests__/Data.spec.tsx
+++ package/src/renderer/__tests__/Data.spec.tsx
@@ -5,15 +5,16 @@ import React from "react";
 import { processResult } from "../../__tests__/setup";
 import { Fill } from "../components";
 import * as SkiaRenderer from "../index";
+import type { SkData } from "../../skia/types/Data/Data";
 
-import { mountCanvas, nodeRequire } from "./setup";
+import { mountCanvas, nodeRequire, Skia } from "./setup";
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface MyCompProps {}
+interface EmptyProps {}
 
-const MyComp = ({}: MyCompProps) => {
+const CheckFont = ({}: EmptyProps) => {
   const { useFont } = require("../../skia/core/Font");
   const font = useFont(
     nodeRequire(
@@ -26,12 +27,40 @@ const MyComp = ({}: MyCompProps) => {
   return <Fill color="green" />;
 };
 
+const CheckDataCollection = ({}: EmptyProps) => {
+  const { useDataCollection } = require("../../skia/core/Data");
+  const font = useDataCollection(
+    [
+      nodeRequire(
+        path.resolve(__dirname, "../../skia/__tests__/assets/Roboto-Medium.ttf")
+      ),
+      nodeRequire(
+        path.resolve(__dirname, "../../skia/__tests__/assets/Roboto-Medium.ttf")
+      ),
+    ],
+    (data: SkData) => Skia.Typeface.MakeFreeTypeFaceFromData(data)
+  );
+  if (!font) {
+    return <Fill color="red" />;
+  }
+  return <Fill color="green" />;
+};
+
 describe("Data Loading", () => {
   it("Loads renderer without Skia", async () => {
     expect(SkiaRenderer).toBeDefined();
   });
   it("Should load a font file", async () => {
-    const { surface, draw } = mountCanvas(<MyComp />);
+    const { surface, draw } = mountCanvas(<CheckFont />);
+    draw();
+    processResult(surface, "snapshots/font/red.png");
+    await wait(500);
+    draw();
+    processResult(surface, "snapshots/font/green.png");
+  });
+
+  it("Should load many font files", async () => {
+    const { surface, draw } = mountCanvas(<CheckDataCollection />);
     draw();
     processResult(surface, "snapshots/font/red.png");
     await wait(500);
