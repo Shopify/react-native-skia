e7d3bb9895008de7fba5b0ba94451bf055f73325
chrfalch <christian.falch@gmail.com>
2022-06-25 20:42:27 +0200

    Added support for missing macro for worklets

--- package/cpp/jsi/JsiHostObject.h
+++ package/cpp/jsi/JsiHostObject.h
@@ -51,6 +51,17 @@
   }
 
 /**
+ * Creates a JSI export function declaration
+ */
+#define JSI_EXPORT_FUNC_NAMED(CLASS, FUNCTION, NAME)                           \
+  {                                                                            \
+#NAME, (jsi::Value(JsiHostObject::*)(                                          \
+                   jsi::Runtime & runtime, const jsi::Value &thisValue,        \
+                   const jsi::Value *arguments, size_t)) &                     \
+                   CLASS::FUNCTION                                             \
+  }
+
+/**
  * Creates a JSI export functions statement
  */
 #define JSI_EXPORT_FUNCTIONS(...)                                              \
