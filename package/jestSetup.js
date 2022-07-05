74a447b12e04bfad7e382d9bf72223b4deb7641c
William Candillon <wcandillon@gmail.com>
2022-07-05 16:49:01 +0200

    Fix mock export

--- package/jestSetup.js
+++ package/jestSetup.js
@@ -1,4 +1,5 @@
 /* globals jest */
-jest.mock("@shopify/react-native-skia", () =>
-  require("@shopify/react-native-skia/lib/commonjs/mock")
+jest.mock(
+  "@shopify/react-native-skia",
+  () => require("@shopify/react-native-skia/lib/commonjs/mock").Mock
 );
