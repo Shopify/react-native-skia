99dd71b7e2044a49e409f7d32092e2c283d435ac
William Candillon <wcandillon@gmail.com>
2022-07-19 09:24:57 +0200

    Fix CI

--- package/src/web/index.tsx
+++ package/src/web/index.tsx
@@ -1,21 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import type { ComponentProps, ComponentType, LazyExoticComponent } from "react";
+import type { ComponentProps, ComponentType } from "react";
 import React, { useMemo, lazy, Suspense } from "react";
-import CanvasKitInit from "canvaskit-wasm/bin/full/canvaskit";
-import type { CanvasKit as CanvasKitType } from "canvaskit-wasm";
 import { Platform } from "react-native";
 
-declare global {
-  var CanvasKit: CanvasKitType;
-}
-
-export const LoadSkia = async () => {
-  const CanvasKit = await CanvasKitInit();
-  // The CanvasKit API is stored on the global object and used
-  // to create the JsiSKApi in the Skia.web.ts file.
-  global.CanvasKit = CanvasKit;
-};
+import { LoadSkia } from "./LoadSkia";
 
 interface WithSkiaProps {
   fallback: ComponentProps<typeof Suspense>["fallback"];
