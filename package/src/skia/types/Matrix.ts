835117ca078c1b79c81a321dbfab50783597e170
William Candillon <wcandillon@gmail.com>
2022-07-20 17:10:24 +0200

    Faster transforms in the group

--- package/src/skia/types/Matrix.ts
+++ package/src/skia/types/Matrix.ts
@@ -1,4 +1,5 @@
 import type { SkJSIInstance } from "./JsiInstance";
+import type { SkCanvas } from "./Canvas";
 export enum MatrixIndex {
   ScaleX = 0,
   SkewX = 1,
@@ -91,6 +92,51 @@ export const processTransform = (m: SkMatrix, transforms: Transforms2d) => {
   return m;
 };
 
+export const concatTransform = (m: SkCanvas, transforms: Transforms2d) => {
+  for (const transform of transforms) {
+    const key = Object.keys(transform)[0] as Transform2dName;
+    const value = (transform as Pick<Transformations, typeof key>)[key];
+    if (key === "translateX") {
+      m.translate(value, 0);
+      continue;
+    }
+    if (key === "translateY") {
+      m.translate(0, value);
+      continue;
+    }
+    if (key === "scale") {
+      m.scale(value, value);
+      continue;
+    }
+    if (key === "scaleX") {
+      m.scale(value, 1);
+      continue;
+    }
+    if (key === "scaleY") {
+      m.scale(1, value);
+      continue;
+    }
+    if (key === "skewX") {
+      m.skew(value, 0);
+      continue;
+    }
+    if (key === "skewY") {
+      m.skew(0, value);
+      continue;
+    }
+    if (key === "rotate" || key === "rotateZ") {
+      m.rotate(toDegrees(value), 0, 0);
+      continue;
+    }
+    exhaustiveCheck(key);
+  }
+  return m;
+};
+
 const exhaustiveCheck = (a: never): never => {
   throw new Error(`Unknown transformation: ${a}`);
+};
+
+export const toDegrees = (rad: number) => {
+  return (rad * 180) / Math.PI;
 };
