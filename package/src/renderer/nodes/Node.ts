d7a84031f00d030a9f6bae12fdedd91645265d10
chrfalch <christian.falch@gmail.com>
2022-07-27 10:17:47 +0200

    Added support for isSelector

--- package/src/renderer/nodes/Node.ts
+++ package/src/renderer/nodes/Node.ts
@@ -3,7 +3,7 @@ import type { SkiaValue } from "../../values";
 import type { DependencyManager } from "../DependencyManager";
 import type { DrawingContext } from "../DrawingContext";
 import type { AnimatedProps } from "../processors";
-import { isIndexedAccess, isValue } from "../processors";
+import { isValue, isSelector } from "../processors";
 import { mapKeys } from "../typeddash";
 
 export enum NodeType {
@@ -79,20 +79,19 @@ export abstract class Node<P = unknown> {
         });
         // Set initial value
         this._props[key] = (propvalue as SkiaValue<P[typeof key]>).current;
-      } else if (isIndexedAccess(propvalue)) {
-        const i = propvalue.index;
+      } else if (isSelector(propvalue)) {
         // Subscribe to changes
         this._propSubscriptions.push({
           key,
           value: propvalue.value,
           unsub: undefined,
           listener: (v) => {
-            this._props[key] = (v as P[typeof key][])[i];
+            this._props[key] = propvalue.selector(v) as P[typeof key];
             this.setDirty(key);
           },
         });
         // Set initial value
-        const v = propvalue.value.current[i];
+        const v = propvalue.selector(propvalue.value.current) as P[typeof key];
         this._props[key] = v as P[typeof key];
       }
     });
