3ca42138e6ed1e2f27cf6c2498d2e477a5cd67dd
William Candillon <wcandillon@gmail.com>
2022-08-16 07:27:42 +0200

    Test useComputedValue dependencies

--- package/src/values/web/__tests__/RNSkComputedValue.spec.tsx
+++ package/src/values/web/__tests__/RNSkComputedValue.spec.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Fill } from "../../../renderer/components";
 import {
@@ -10,6 +10,25 @@ import { processResult } from "../../../__tests__/setup";
 import type { SkiaValue } from "../../types";
 import { RNSkComputedValue } from "../RNSkComputedValue";
 import { RNSkValue } from "../RNSkValue";
+
+interface TestComputedDepsProps {
+  id1: SkiaValue<number>;
+  id2: SkiaValue<number>;
+}
+
+const TestComputedDeps = ({ id1, id2 }: TestComputedDepsProps) => {
+  const { useComputedValue } = importSkia();
+  const [dep, setDep] = useState(id1);
+  const color = useComputedValue(() => {
+    return dep.current % 2 ? "green" : "red";
+  }, [dep]);
+  useEffect(() => {
+    setTimeout(() => {
+      setDep(id2);
+    }, 100);
+  }, [id2]);
+  return <Fill color={color} />;
+};
 
 interface TestComputedProps {
   id: SkiaValue<number>;
@@ -42,6 +61,19 @@ describe("RNSkComputedValue", () => {
     processResult(surface, "snapshots/animations/red.png");
     id.current = 1;
     await wait(50);
+    draw();
+    processResult(surface, "snapshots/animations/green.png");
+  });
+
+  it("useComputedValue() should unsubscribe when stable", async () => {
+    const id1 = global.SkiaValueApi.createValue(0);
+    const id2 = global.SkiaValueApi.createValue(1);
+    const { surface, draw } = mountCanvas(
+      <TestComputedDeps id1={id1} id2={id2} />
+    );
+    draw();
+    processResult(surface, "snapshots/animations/red.png");
+    await wait(250);
     draw();
     processResult(surface, "snapshots/animations/green.png");
   });
