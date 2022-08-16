33fe2d709dbc963203d430501dbbc7ecf713f270
William Candillon <wcandillon@gmail.com>
2022-08-16 12:26:11 +0200

    Update tests

--- package/src/values/hooks/__tests__/useComputedValue.spec.tsx
+++ package/src/values/hooks/__tests__/useComputedValue.spec.tsx
@@ -8,12 +8,8 @@ import {
 } from "../../../renderer/__tests__/setup";
 import { processResult } from "../../../__tests__/setup";
 import type { SkiaValue } from "../../types";
-import { RNSkComputedValue } from "../RNSkComputedValue";
-import { RNSkValue } from "../RNSkValue";
 
-const sideEffects = {
-  id: 0,
-};
+const counter = { value: 0 };
 
 interface TestComputedDepsProps {
   id1: SkiaValue<number>;
@@ -24,7 +20,7 @@ const TestComputedDeps = ({ id1, id2 }: TestComputedDepsProps) => {
   const { useComputedValue } = importSkia();
   const [dep, setDep] = useState(id1);
   const color = useComputedValue(() => {
-    sideEffects.id++;
+    counter.value++;
     return dep.current % 2 ? "green" : "red";
   }, [dep]);
   useEffect(() => {
@@ -47,19 +43,8 @@ const TestComputed = ({ id }: TestComputedProps) => {
   return <Fill color={color} />;
 };
 
-describe("RNSkComputedValue", () => {
-  it("should update when dependency changes", () => {
-    const dependency = new RNSkValue(10);
-    const computed = new RNSkComputedValue(
-      () => 10 * dependency.current,
-      [dependency]
-    );
-    expect(computed.current).toBe(100);
-    dependency.current = 20;
-    expect(computed.current).toBe(200);
-  });
-
-  it("useComputedValue() should update as expected", async () => {
+describe("useComputedValue", () => {
+  it("should update as expected", async () => {
     const id = global.SkiaValueApi.createValue(0);
     const { surface, draw } = mountCanvas(<TestComputed id={id} />);
     draw();
@@ -70,31 +55,42 @@ describe("RNSkComputedValue", () => {
     processResult(surface, "snapshots/animations/green.png");
   });
 
-  it("useComputedValue() should respect its dependencies", async () => {
+  it("should respect dependency changes", async () => {
+    counter.value = 0;
     const id1 = global.SkiaValueApi.createValue(0);
     const id2 = global.SkiaValueApi.createValue(1);
+    // We draw
     const { surface, draw } = mountCanvas(
       <TestComputedDeps id1={id1} id2={id2} />
     );
     draw();
     processResult(surface, "snapshots/animations/red.png");
+    // We update the dependency
     await wait(250);
     draw();
     processResult(surface, "snapshots/animations/green.png");
+    expect(counter.value).toBe(2);
   });
 
-  it("useComputedValue() should unsubscribe when the dependencies have changed", async () => {
-    sideEffects.id = 0;
+  it.failing("should clean up stable version", async () => {
+    counter.value = 0;
     const id1 = global.SkiaValueApi.createValue(0);
     const id2 = global.SkiaValueApi.createValue(1);
+    // We draw
     const { surface, draw } = mountCanvas(
       <TestComputedDeps id1={id1} id2={id2} />
     );
     draw();
     processResult(surface, "snapshots/animations/red.png");
+    // We update the dependency
     await wait(250);
     draw();
     processResult(surface, "snapshots/animations/green.png");
-    expect(sideEffects.id).toBe(2);
+    expect(counter.value).toBe(2);
+    // We update the values and check that only useComputedValue() is called
+    id1.current++;
+    id2.current++;
+    draw();
+    expect(counter.value).toBe(3);
   });
 });
