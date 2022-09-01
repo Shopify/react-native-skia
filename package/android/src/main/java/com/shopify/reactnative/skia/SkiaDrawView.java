f7380dad2d80957d7b1677a20e5482ad629fb930
chrfalch <christian.falch@gmail.com>
2022-09-01 15:31:20 +0200

    Removed unused imports

--- package/android/src/main/java/com/shopify/reactnative/skia/SkiaDrawView.java
+++ package/android/src/main/java/com/shopify/reactnative/skia/SkiaDrawView.java
@@ -2,9 +2,6 @@ package com.shopify.reactnative.skia;
 
 import android.content.Context;
 
-import com.facebook.jni.HybridData;
-import com.facebook.react.bridge.ReactContext;
-
 public class SkiaDrawView extends SkiaBaseView {
     public SkiaDrawView(Context ctx) {
         super(ctx);
