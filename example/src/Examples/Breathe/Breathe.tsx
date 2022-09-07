b2a78fb1fae14b7b3b72344de042e36b5053a0ee
William Candillon <wcandillon@gmail.com>
2022-09-07 21:17:56 +0200

    Revert bogus change

--- example/src/Examples/Breathe/Breathe.tsx
+++ example/src/Examples/Breathe/Breathe.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import { StyleSheet, useWindowDimensions } from "react-native";
+import type { SkiaValue } from "@shopify/react-native-skia";
 import {
   useComputedValue,
   useLoop,
@@ -7,12 +8,45 @@ import {
   vec,
   Canvas,
   Circle,
+  Fill,
   Group,
+  polar2Canvas,
   Easing,
   mix,
 } from "@shopify/react-native-skia";
 
+const c1 = "#61bea2";
 const c2 = "#529ca0";
+
+interface RingProps {
+  index: number;
+  progress: SkiaValue<number>;
+}
+
+const Ring = ({ index, progress }: RingProps) => {
+  const { width, height } = useWindowDimensions();
+  const R = width / 4;
+  const center = useMemo(
+    () => vec(width / 2, height / 2 - 64),
+    [height, width]
+  );
+
+  const theta = (index * (2 * Math.PI)) / 6;
+  const transform = useComputedValue(() => {
+    const { x, y } = polar2Canvas(
+      { theta, radius: progress.current * R },
+      { x: 0, y: 0 }
+    );
+    const scale = mix(progress.current, 0.3, 1);
+    return [{ translateX: x }, { translateY: y }, { scale }];
+  }, [progress]);
+
+  return (
+    <Group origin={center} transform={transform}>
+      <Circle c={center} r={R} color={index % 2 ? c1 : c2} />
+    </Group>
+  );
+};
 
 export const Breathe = () => {
   const { width, height } = useWindowDimensions();
@@ -33,9 +67,12 @@ export const Breathe = () => {
 
   return (
     <Canvas style={styles.container} debug>
+      <Fill color="rgb(36,43,56)" />
       <Group origin={center} transform={transform} blendMode="screen">
         <BlurMask style="solid" blur={40} />
-        <Circle c={center} r={200} color={c2} />
+        {new Array(6).fill(0).map((_, index) => {
+          return <Ring key={index} index={index} progress={progress} />;
+        })}
       </Group>
     </Canvas>
   );
