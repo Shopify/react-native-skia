23a975d9eb4dbc84b3c112593728367bb95642f0
William Candillon <wcandillon@gmail.com>
2022-09-13 16:54:57 +0200

    Implement review comments

--- package/src/dom/nodes/drawings/PatchNode.ts
+++ package/src/dom/nodes/drawings/PatchNode.ts
@@ -1,30 +1,31 @@
+import type { SkColor, SkPoint } from "../../../skia/types";
 import { BlendMode } from "../../../skia/types";
 import type { DrawingContext, PatchProps } from "../../types";
 import { NodeType } from "../../types";
-import { enumKey, processColor } from "../datatypes";
+import { ALPHA, enumKey, processColor } from "../datatypes";
 import { JsiDrawingNode } from "../DrawingNode";
 import type { NodeContext } from "../Node";
 
-export class PatchNode extends JsiDrawingNode<PatchProps, null> {
+export class PatchNode extends JsiDrawingNode<
+  PatchProps,
+  { points: SkPoint[]; colors: SkColor[] | undefined; mode: BlendMode }
+> {
   constructor(ctx: NodeContext, props: PatchProps) {
     super(ctx, NodeType.Patch, props);
   }
 
   deriveProps() {
-    return null;
-  }
-
-  draw({ canvas, paint, opacity }: DrawingContext) {
-    const { colors, blendMode, patch, texture } = this.props;
+    const { colors, blendMode, patch } = this.props;
     const defaultBlendMode = colors ? BlendMode.DstOver : BlendMode.SrcOver;
     const mode = blendMode ? BlendMode[enumKey(blendMode)] : defaultBlendMode;
-    canvas.drawPatch(
-      // Patch requires a path with the following constraints:
-      // M tl
-      // C c1 c2 br
-      // C c1 c2 bl
-      // C c1 c2 tl (the redundant point in the last command is removed)
-      [
+    // Patch requires a path with the following constraints:
+    // M tl
+    // C c1 c2 br
+    // C c1 c2 bl
+    // C c1 c2 tl (the redundant point in the last command is removed)
+    return {
+      mode,
+      points: [
         patch[0].pos,
         patch[0].c2,
         patch[1].c1,
@@ -38,9 +39,27 @@ export class PatchNode extends JsiDrawingNode<PatchProps, null> {
         patch[3].c2,
         patch[0].c1,
       ],
-      colors
-        ? colors.map((c) => processColor(this.Skia, c, opacity))
+      colors: colors
+        ? colors.map((c) => processColor(this.Skia, c, 1))
         : undefined,
+    };
+  }
+
+  draw({ canvas, paint, opacity }: DrawingContext) {
+    if (!this.derived) {
+      throw new Error("PatchNode: derived props not set");
+    }
+    const { texture } = this.props;
+    const { colors, points, mode } = this.derived;
+    canvas.drawPatch(
+      points,
+      opacity === 1
+        ? colors
+        : colors &&
+            colors.map((c) => {
+              c[ALPHA] = c[ALPHA] * opacity;
+              return c;
+            }),
       texture,
       mode,
       paint
