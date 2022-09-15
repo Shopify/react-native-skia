4872389a81a856481c4d4e1e58503c07407660a4
William Candillon <wcandillon@gmail.com>
2022-09-15 16:51:26 +0200

    Implement review comments

--- package/src/renderer/__tests__/Picture.spec.tsx
+++ package/src/renderer/__tests__/Picture.spec.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import { processResult } from "../../__tests__/setup";
 import { Fill, Picture, Group } from "../components";
@@ -7,7 +7,7 @@ import type { EmptyProps } from "./setup";
 import { wait, importSkia, mountCanvas, width } from "./setup";
 
 const CheckPicture = ({}: EmptyProps) => {
-  const { usePicture, Skia } = importSkia();
+  const { createPicture, Skia } = importSkia();
   const [color, setColor] = useState("green");
   const r = width / 2;
   useEffect(() => {
@@ -15,13 +15,15 @@ const CheckPicture = ({}: EmptyProps) => {
       setColor("red");
     }, 16);
   }, [Skia]);
-  const picture = usePicture(
-    Skia.XYWHRect(0, 0, r * 2, r * 2),
-    (canvas) => {
-      const paint = Skia.Paint();
-      paint.setColor(Skia.Color(color));
-      canvas.drawCircle(r, r, r, paint);
-    },
+  const picture = useMemo(
+    () =>
+      createPicture(Skia.XYWHRect(0, 0, r * 2, r * 2), (canvas) => {
+        const paint = Skia.Paint();
+        paint.setColor(Skia.Color(color));
+        canvas.drawCircle(r, r, r, paint);
+      }),
+    // In a non-test environment, createPicture and Skia will be top-level, not part of the array
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [color]
   );
   return (
@@ -33,7 +35,7 @@ const CheckPicture = ({}: EmptyProps) => {
 };
 
 const CheckPicture2 = ({}: EmptyProps) => {
-  const { usePicture, Skia } = importSkia();
+  const { createPicture, Skia } = importSkia();
   const [color, setColor] = useState("green");
   const r = width / 2;
   useEffect(() => {
@@ -41,11 +43,16 @@ const CheckPicture2 = ({}: EmptyProps) => {
       setColor("red");
     }, 16);
   }, [Skia]);
-  const picture = usePicture(Skia.XYWHRect(0, 0, r * 2, r * 2), (canvas) => {
-    const paint = Skia.Paint();
-    paint.setColor(Skia.Color(color));
-    canvas.drawCircle(r, r, r, paint);
-  });
+  const picture = useMemo(
+    () =>
+      createPicture(Skia.XYWHRect(0, 0, r * 2, r * 2), (canvas) => {
+        const paint = Skia.Paint();
+        paint.setColor(Skia.Color(color));
+        canvas.drawCircle(r, r, r, paint);
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
   return (
     <Group>
       <Fill color="white" />
