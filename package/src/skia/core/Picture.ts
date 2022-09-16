f37493718811d4587839a94e2e557f58aa8a5363
William Candillon <wcandillon@gmail.com>
2022-09-16 10:07:55 +0200

    Deprecate usePicture in favor of createPicture (#845)

--- package/src/skia/core/Picture.ts
+++ package/src/skia/core/Picture.ts
@@ -2,7 +2,7 @@ import type { DependencyList } from "react";
 import { useMemo } from "react";
 
 import { Skia } from "../Skia";
-import type { SkCanvas, SkPicture, SkRect } from "../types";
+import type { SkCanvas, SkRect } from "../types";
 
 /**
  * Memoizes and returns an SkPicture that can be drawn to another canvas.
@@ -14,12 +14,26 @@ export const usePicture = (
   rect: SkRect,
   cb: (canvas: SkCanvas) => void,
   deps: DependencyList = []
-): SkPicture => {
+) => {
+  console.warn("usePicture() is deprecated. Use createPicture() instead.");
   return useMemo(() => {
     const recorder = Skia.PictureRecorder();
     const canvas = recorder.beginRecording(rect);
     cb(canvas);
     return recorder.finishRecordingAsPicture();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cb, rect, deps]);
+  }, deps);
+};
+
+/**
+ * Memoizes and returns an SkPicture that can be drawn to another canvas.
+ * @param rect Picture bounds
+ * @param cb Callback for drawing to the canvas
+ * @returns SkPicture
+ */
+export const createPicture = (rect: SkRect, cb: (canvas: SkCanvas) => void) => {
+  const recorder = Skia.PictureRecorder();
+  const canvas = recorder.beginRecording(rect);
+  cb(canvas);
+  return recorder.finishRecordingAsPicture();
 };
