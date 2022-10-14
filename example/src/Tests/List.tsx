55fd929f60b1f8a326eed70f9c7cfe8e87b3921f
William Candillon <wcandillon@gmail.com>
2022-10-14 10:20:42 +0200

    Fix CI

--- example/src/Tests/List.tsx
+++ example/src/Tests/List.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
 
 export const List = () => {
   const { navigate } =
-    useNavigation<NativeStackNavigationProp<Routes, "List">>();
+    useNavigation<NativeStackNavigationProp<Routes, "TestList">>();
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.content}>
       {examples.map((thumbnail) => (
