4fedb9f7a2541de3a3bcf0109885311bfee32aad
chrfalch <christian.falch@gmail.com>
2022-10-19 20:44:19 +0200

    Implemented Glyphs node

--- package/cpp/rnskia/dom/nodes/JsiGlyphsNode.h
+++ package/cpp/rnskia/dom/nodes/JsiGlyphsNode.h
@@ -2,6 +2,9 @@
 
 #include "JsiDomDrawingNode.h"
 
+#include "FontProp.h"
+#include "GlyphsProp.h"
+
 namespace RNSkia {
 
 class JsiGlyphsNode : public JsiDomDrawingNode, public JsiDomNodeCtor<JsiGlyphsNode> {
@@ -11,13 +14,36 @@ public:
     
 protected:
   void draw(DrawingContext* context) override {
-    // TODO: Implement
+    auto x = _xProp->value()->getAsNumber();
+    auto y = _yProp->value()->getAsNumber();
+    auto font = _fontProp->getDerivedValue();
+    auto glyphInfo = _glyphsProp->getDerivedValue();
+    context->getCanvas()->drawGlyphs(static_cast<int>(glyphInfo->glyphIds.size()),
+                                     glyphInfo->glyphIds.data(),
+                                     glyphInfo->positions.data(),
+                                     SkPoint::Make(x, y),
+                                     *font,
+                                     *context->getPaint());
   }
   
   void defineProperties(NodePropsContainer* container) override {
     JsiDomDrawingNode::defineProperties(container);
+    
+    _fontProp = container->defineProperty(std::make_shared<FontProp>(JsiPropId::get("font")));
+    _glyphsProp = container->defineProperty(std::make_shared<GlyphsProp>(JsiPropId::get("glyphs")));
+    _xProp = container->defineProperty(std::make_shared<NodeProp>(JsiPropId::get("x")));
+    _yProp = container->defineProperty(std::make_shared<NodeProp>(JsiPropId::get("y")));
+    
+    _fontProp->require();
+    _glyphsProp->require();
+    _xProp->require();
+    _yProp->require();
   }
 private:
+  FontProp* _fontProp;
+  GlyphsProp* _glyphsProp;
+  NodeProp* _xProp;
+  NodeProp* _yProp;
 };
 
 }
