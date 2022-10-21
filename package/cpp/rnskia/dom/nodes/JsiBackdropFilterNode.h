303628de3bfd2aa5a5ab52884291cfd1e5b87c13
chrfalch <christian.falch@gmail.com>
2022-10-21 18:58:03 +0200

    Added backdropfilter node implementation

--- package/cpp/rnskia/dom/nodes/JsiBackdropFilterNode.h
+++ package/cpp/rnskia/dom/nodes/JsiBackdropFilterNode.h
@@ -5,23 +5,33 @@
 
 namespace RNSkia {
 
-class JsiBackdropFilterNode : public JsiDomDrawingNode, public JsiDomNodeCtor<JsiBackdropFilterNode> {
+class JsiBackdropFilterNode :
+public JsiDomDrawingNode,
+public JsiDomNodeCtor<JsiBackdropFilterNode> {
 public:
   JsiBackdropFilterNode(std::shared_ptr<RNSkPlatformContext> context) :
   JsiDomDrawingNode(context, "skBackdropFilter") {}
     
 protected:
   void draw(DrawingContext* context) override {
-    // TODO: Implement
-  }
-  
-  void defineProperties(NodePropsContainer* container) override {
-    JsiDomDrawingNode::defineProperties(container);
+    if (getChildren().size() == 0) {
+      throw std::runtime_error("Expected at least one child in the BackdropFilter node.");
+    }
+    auto child = getChildren()[0];
+    auto colorFilter = std::dynamic_pointer_cast<JsiBaseColorFilterNode>(child);
+    auto imageFilter = std::dynamic_pointer_cast<JsiBaseImageFilterNode>(child);
     
-  }
-  
-private:
-  
+    auto canvas = context->getCanvas();
+    auto filter = colorFilter != nullptr ?
+      SkImageFilters::ColorFilter(colorFilter->getCurrent(),
+                                  nullptr) : imageFilter->getCurrent();
+    
+    canvas->saveLayer(SkCanvas::SaveLayerRec(nullptr,
+                                             nullptr,
+                                             filter.get(),
+                                             0));
+    canvas->restore();
+  }  
 };
 
 }
