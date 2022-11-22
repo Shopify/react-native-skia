3284d8ce66b6435fbb3cb90ab1f0b183f935466b
William Candillon <wcandillon@gmail.com>
2022-11-22 21:20:58 +0100

    :wrench:

--- example/src/Tests/Tests.tsx
+++ example/src/Tests/Tests.tsx
@@ -1,45 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { SkiaDomView } from "@shopify/react-native-skia";
-import { Skia, Canvas } from "@shopify/react-native-skia";
+import { Canvas } from "@shopify/react-native-skia";
 import React, { useEffect, useRef, useState } from "react";
-import { Platform, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-export const ANDROID_WS_HOST = "10.0.2.2";
-export const IOS_WS_HOST = "localhost";
-const url = `ws://${
-  Platform.OS === "android" ? ANDROID_WS_HOST : IOS_WS_HOST
-}:4242`;
+import type { SerializedNode } from "./deserialize";
+import { parseNode } from "./deserialize";
+import { useClient } from "./useClient";
 
 export const Tests = () => {
   const ref = useRef<SkiaDomView>(null);
-  const [client, setClient] = useState<WebSocket | null>(null);
-
+  const client = useClient();
   const [drawing, setDrawing] = useState<any>(null);
-  useEffect(() => {
-    if (client === null) {
-      const makeConnection = () => {
-        const ws = new WebSocket(url);
-        ws.onopen = () => {
-          setClient(ws);
-        };
-        ws.onclose = () => {
-          setClient(null);
-        };
-        ws.onerror = () => {
-          it = setTimeout(() => {
-            ws.close();
-            makeConnection();
-          }, 500);
-        };
-      };
-      makeConnection();
-      let it: ReturnType<typeof setTimeout>;
-      return () => {
-        clearTimeout(it);
-      };
-    }
-    return;
-  }, [client]);
+
   useEffect(() => {
     if (client !== null) {
       client.onmessage = (e) => {
@@ -76,59 +49,4 @@ export const Tests = () => {
       </Canvas>
     </View>
   );
-};
-
-interface SerializedProps {
-  [key: string]: any;
-}
-
-interface SerializedNode {
-  type: string;
-  props: SerializedProps;
-  children: SerializedNode[];
-}
-
-const parseNode = (serializedNode: SerializedNode): any => {
-  const { type, props, children } = serializedNode;
-  return React.createElement(
-    type,
-    { ...parseProps(props), key: `${Math.random()}` },
-    children.map(parseNode)
-  );
-};
-
-const parseProps = (props: SerializedProps) => {
-  const newProps: SerializedProps = {};
-  Object.keys(props).forEach((key) => {
-    newProps[key] = parseProp(props[key]);
-  });
-  return newProps;
-};
-
-const parseProp = (value: any) => {
-  if (value && typeof value === "object" && "__typename__" in value) {
-    if (value.__typename__ === "Point") {
-      return Skia.Point(value.x, value.y);
-    } else if (value.__typename__ === "Rect") {
-      return Skia.XYWHRect(value.x, value.y, value.width, value.height);
-    } else if (value.__typename__ === "RRect") {
-      return Skia.RRectXY(
-        Skia.XYWHRect(value.x, value.y, value.width, value.height),
-        value.rx,
-        value.ry
-      );
-    } else if (value.__typename__ === "Path") {
-      return Skia.Path.MakeFromCmds(value.cmds);
-    } else if (value.__typename__ === "Image") {
-      const raw = new Uint8Array(value.bytes);
-      const data = Skia.Data.fromBytes(raw);
-      return Skia.Image.MakeImageFromEncoded(data);
-    } else if (value.__typename__ === "Font") {
-      const raw = new Uint8Array(value.typeface);
-      const data = Skia.Data.fromBytes(raw);
-      const typeface = Skia.Typeface.MakeFreeTypeFaceFromData(data)!;
-      return Skia.Font(typeface, value.size);
-    }
-  }
-  return value;
 };
