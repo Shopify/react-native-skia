1bc29f0dde13a4f7db9f03fb113234f9e11ca448
William Candillon <wcandillon@gmail.com>
2022-12-04 14:18:08 +0100

    Add test

--- package/src/renderer/__tests__/e2e/RuntimeShader.spec.tsx
+++ package/src/renderer/__tests__/e2e/RuntimeShader.spec.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 
 import { surface, importSkia } from "../setup";
-import { Circle, Group, RuntimeShader } from "../../components";
+import { Circle, Fill, Group, RuntimeShader } from "../../components";
 import { checkImage, itRunsE2eOnly } from "../../../__tests__/setup";
+
+const spiral = `
+uniform float scale;
+uniform float2   center;
+uniform float4 colors[2];
+uniform shader image;
+
+half4 main(float2 p) {
+    float2 pp = p - center;
+    float radius = sqrt(dot(pp, pp));
+    radius = sqrt(radius);
+    float angle = atan(pp.y / pp.x);
+    float t = (angle + 3.1415926/2) / (3.1415926);
+    t += radius * scale;
+    t = fract(t);
+    return half4(mix(image.eval(vec2(0, 0)), colors[1], t));
+}`;
 
 describe("Runtime Shader", () => {
   itRunsE2eOnly("should use the sdf of a circle", async () => {
@@ -27,5 +44,32 @@ half4 main(float2 xy) {
       </Group>
     );
     checkImage(img, "snapshots/runtime-shader/simple.png");
+  });
+  itRunsE2eOnly("should display a green and red spiral", async () => {
+    const { width, height } = surface;
+    const { Skia } = importSkia();
+    const source = Skia.RuntimeEffect.Make(spiral)!;
+    expect(source).toBeTruthy();
+    const img = await surface.draw(
+      <>
+        <Group>
+          <RuntimeShader
+            source={source}
+            uniforms={{
+              scale: 0.6,
+              center: { x: width / 2, y: height / 2 },
+              colors: [
+                [1, 0, 0, 1], // red
+                [0, 1, 0, 1], // green
+              ],
+            }}
+          />
+          <Fill color="blue" />
+        </Group>
+      </>
+    );
+    checkImage(img, "snapshots/runtime-shader/spiral.png", {
+      maxPixelDiff: 1,
+    });
   });
 });
