3d78cd0ee5f3e16e2a9af046a4b6f5abcc9b2eb8
William Candillon <wcandillon@gmail.com>
2022-12-04 16:32:18 +0100

    Don't crash the app if image is not set

--- package/cpp/rnskia/dom/props/ImageProps.h
+++ package/cpp/rnskia/dom/props/ImageProps.h
@@ -30,7 +30,7 @@ public:
   }
 
   void updateDerivedValue() override {
-    if (_imageProp->value().getType() != PropType::HostObject) {
+    if (!_imageProp->isSet() && _imageProp->value().getType() != PropType::HostObject) {
       throw std::runtime_error("Expected SkImage object for the " +
                                std::string(getName()) + " property.");
     }
