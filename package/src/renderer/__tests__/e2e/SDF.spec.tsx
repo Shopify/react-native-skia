36105e0ce78dac46c67ab93157a4b59a2fae50aa
William Candillon <wcandillon@gmail.com>
2022-12-09 19:27:24 +0100

    Add some sdf tests

--- package/src/renderer/__tests__/e2e/SDF.spec.tsx
+++ package/src/renderer/__tests__/e2e/SDF.spec.tsx
@@ -69,6 +69,37 @@ vec4 main(vec2 xy) {
         <Shader source={source} uniforms={{ c1, c2 }} />
       </Fill>
     );
-    checkImage(img, "snapshots/sdf/line.png", { overwrite: true });
+    checkImage(img, "snapshots/sdf/line.png");
+  });
+  it("should use the sdf of a rectangle", async () => {
+    const { Skia } = importSkia();
+    const source = Skia.RuntimeEffect.Make(`
+uniform float4 c1;
+uniform float4 c2;
+
+float sdRect( in vec2 p, in vec2 b ) {
+    vec2 d = abs(p)-b;
+    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+}
+
+vec4 main(vec2 xy) { 
+  vec2 b = vec2(64, 64);
+  vec2 c = vec2(128.0, 128.0);
+  float d = sdRect(xy - c, b);
+  if (d < 0) {
+    return c2;
+  }
+  return c1;
+}
+`)!;
+    const c1 = Skia.Color("#61dafb");
+    const c2 = Skia.Color("#fb61da");
+    expect(source).toBeDefined();
+    const img = await surface.draw(
+      <Fill>
+        <Shader source={source} uniforms={{ c1, c2 }} />
+      </Fill>
+    );
+    checkImage(img, "snapshots/sdf/rectangle.png");
   });
 });
