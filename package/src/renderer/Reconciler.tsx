9bf46e8ad947121103fed70f47838a42f02ea3b6
William Candillon <wcandillon@gmail.com>
2022-12-17 13:21:56 +0100

    Get Image From CPU

--- package/src/renderer/Reconciler.tsx
+++ package/src/renderer/Reconciler.tsx
@@ -5,6 +5,7 @@ import ReactReconciler from "react-reconciler";
 import type { Skia } from "../skia/types";
 import type { SkiaValue } from "../values/types";
 import type { SkiaDomView } from "../views";
+import { SkiaView } from "../views/SkiaView.web";
 
 import { DependencyManager } from "./DependencyManager";
 import { skHostConfig, debug as hostDebug } from "./HostConfig";
@@ -40,7 +41,7 @@ export class SkiaRoot {
       }
       return ref.current.registerValues(values);
     };
-    const depMgr = createDependencyManager(registerValues);
+    const depMgr = createDependencyManager(registerValues, js);
     this.container = new Container(Skia, depMgr, redraw);
     this.root = skiaReconciler.createContainer(
       this.container,
@@ -71,3 +72,34 @@ export class SkiaRoot {
     return this.container.root;
   }
 }
+
+export const getImageFromCPU = (
+  width: number,
+  height: number,
+  node: ReactNode
+) => {
+  const Skia = global.SkiaApi;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ref = { current: new SkiaView({}) as any };
+  const root = new SkiaRoot(Skia, ref, () => {});
+  const surface = Skia.Surface.Make(width, height);
+  if (!surface) {
+    throw new Error("Failed to create surface");
+  }
+  const canvas = surface.getCanvas();
+  const ctx = {
+    width,
+    height,
+    timestamp: 0,
+    canvas,
+    paint: Skia.Paint(),
+    ref,
+    center: Skia.Point(width / 2, height / 2),
+    Skia,
+  };
+  root.render(node);
+  root.dom.render(ctx);
+  const image = surface.makeImageSnapshot();
+  root.unmount();
+  return image;
+};
--- package/src/renderer/index.ts
+++ package/src/renderer/index.ts
@@ -1,3 +1,4 @@
 export * from "./components";
 export * from "./useContextBridge";
 export * from "./DependencyManager";
+export * from "./Reconciler";
