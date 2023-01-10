a5398d4f037ab4a80130601452c57b54a2a81fce
chrfalch <christian.falch@gmail.com>
2023-01-10 13:42:28 +0100

    Android: Upgrade to RN 0.71.0-rc.6

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    "react-native-reanimated/plugin",
-    "transform-inline-environment-variables",
-  ],
+  plugins: ["transform-inline-environment-variables"],
 };
