a8795055140d2fb6ee47f9e805525360be42ecd7
chrfalch <christian.falch@gmail.com>
2023-03-17 12:58:50 +0100

    Added MakeImageFromViewTag on JsiSkImageFactory
    The method returns a promise that will be resolved when the view screenshot is done.

--- package/cpp/api/JsiSkImageFactory.h
+++ package/cpp/api/JsiSkImageFactory.h
@@ -38,7 +38,36 @@ public:
         runtime, std::make_shared<JsiSkImage>(getContext(), std::move(image)));
   }
 
+  JSI_HOST_FUNCTION(MakeImageFromViewTag) {
+    auto viewTag = arguments[0].asNumber();
+    auto context = getContext();
+    return react::createPromiseAsJSIValue(
+        runtime,
+        [context = std::move(context),
+         viewTag](jsi::Runtime &runtime,
+                  std::shared_ptr<react::Promise> promise) -> void {
+          // Create a stream operation - this will be run on the main thread
+          context->makeViewScreenshot(
+              viewTag, [&runtime, context = std::move(context),
+                        promise = std::move(promise)](sk_sp<SkImage> image) {
+                context->runOnJavascriptThread([&runtime,
+                                                context = std::move(context),
+                                                promise = std::move(promise),
+                                                result = std::move(image)]() {
+                  if (result == nullptr) {
+                    promise->reject("Failed to create image from view tag");
+                    return;
+                  }
+                  promise->resolve(jsi::Object::createFromHostObject(
+                      runtime, std::make_shared<JsiSkImage>(
+                                   std::move(context), std::move(result))));
+                });
+              });
+        });
+  }
+
   JSI_EXPORT_FUNCTIONS(JSI_EXPORT_FUNC(JsiSkImageFactory, MakeImageFromEncoded),
+                       JSI_EXPORT_FUNC(JsiSkImageFactory, MakeImageFromViewTag),
                        JSI_EXPORT_FUNC(JsiSkImageFactory, MakeImage), )
 
   explicit JsiSkImageFactory(std::shared_ptr<RNSkPlatformContext> context)
