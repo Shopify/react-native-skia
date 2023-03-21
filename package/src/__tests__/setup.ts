cffcfdbaed82ffb43c16dc468b21bbc7e6a7bc9a
William Candillon <wcandillon@gmail.com>
2023-03-21 15:59:02 +0100

    Update tests

--- package/src/__tests__/setup.ts
+++ package/src/__tests__/setup.ts
@@ -3,10 +3,8 @@ import fs from "fs";
 
 import { PNG } from "pngjs";
 import pixelmatch from "pixelmatch";
-import type { Surface } from "canvaskit-wasm";
 
 import type { SkSurface, SkImage } from "../skia/types";
-import { JsiSkSurface } from "../skia/web/JsiSkSurface";
 
 export const E2E = process.env.E2E === "true";
 export const CI = process.env.CI === "true";
@@ -23,18 +21,9 @@ export const processResult = (
   relPath: string,
   overwrite = false
 ) => {
-  const ckSurface = JsiSkSurface.fromValue<Surface>(surface);
-  ckSurface.flush();
+  surface.flush();
   const image = surface.makeImageSnapshot();
-  const png = image.encodeToBytes();
-  const p = path.resolve(__dirname, relPath);
-  if (fs.existsSync(p) && !overwrite) {
-    const ref = fs.readFileSync(p);
-    expect(ref.equals(png)).toBe(true);
-  } else {
-    fs.writeFileSync(p, png);
-  }
-  ckSurface.getCanvas().clear(Float32Array.of(0, 0, 0, 0));
+  return checkImage(image, relPath, { overwrite });
 };
 
 interface CheckImageOptions {
