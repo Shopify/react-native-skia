f22311ab77a82b819175b64173fd42922577d052
William Candillon <wcandillon@gmail.com>
2023-03-29 16:21:27 +0200

    Fix bug with clipping props

--- package/cpp/rnskia/dom/props/ClipProp.h
+++ package/cpp/rnskia/dom/props/ClipProp.h
@@ -22,43 +22,84 @@ public:
   explicit ClipProp(PropId name,
                     const std::function<void(BaseNodeProp *)> &onChange)
       : BaseDerivedProp(onChange) {
-    _pathProp = defineProperty<PathProp>(name);
-    _rectProp = defineProperty<RectProp>(name);
-    _rrectProp = defineProperty<RRectProp>(name);
+    _clipProp = defineProperty<NodeProp>(name);
   }
 
   void updateDerivedValue() override {
-    if (_pathProp->isSet()) {
+    if (_clipProp->isSet()) {
+      auto value = _clipProp->value();
       _rect = nullptr;
       _rrect = nullptr;
-      _path = _pathProp->getDerivedValue();
-    } else if (_rrectProp->isSet()) {
-      _rect = nullptr;
-      _rrect = _rrectProp->getDerivedValue();
       _path = nullptr;
-    } else if (_rectProp->isSet()) {
-      _rect = _rectProp->getDerivedValue();
-      _rrect = nullptr;
-      _path = nullptr;
+      if (value.getType() == PropType::HostObject) {
+        auto rect =
+            std::dynamic_pointer_cast<JsiSkRect>(value.getAsHostObject());
+        if (rect != nullptr) {
+          _rect = rect->getObject();
+        } else {
+          auto rrect =
+              std::dynamic_pointer_cast<JsiSkRRect>(value.getAsHostObject());
+          if (rrect != nullptr) {
+            _rrect = rrect->getObject();
+          } else {
+            auto path =
+                std::dynamic_pointer_cast<JsiSkPath>(value.getAsHostObject());
+            _path = path->getObject();
+          }
+        }
+      } else if (value.getType() == PropType::String) {
+        // Read as string
+        auto pathString = value.getAsString();
+        SkPath path;
+        if (SkParsePath::FromSVGString(pathString.c_str(), &path)) {
+          _path = std::make_shared<SkPath>(path);
+        } else {
+          throw std::runtime_error("Could not parse path from string.");
+        }
+      } else if (value.getType() == PropType::Object) {
+        if (value.hasValue(PropNameX) && value.hasValue(PropNameY) &&
+            value.hasValue(PropNameWidth) && value.hasValue(PropNameHeight)) {
+          // Save props for fast access
+          auto x = value.getValue(PropNameX);
+          auto y = value.getValue(PropNameY);
+          auto width = value.getValue(PropNameWidth);
+          auto height = value.getValue(PropNameHeight);
+          // Update cache from js object value
+          _rect = std::make_shared<SkRect>(
+              SkRect::MakeXYWH(x.getAsNumber(), y.getAsNumber(),
+                               width.getAsNumber(), height.getAsNumber()));
+        } else if (value.hasValue(PropNameX) && value.hasValue(PropNameY) &&
+                   value.hasValue(PropNameWidth) &&
+                   value.hasValue(PropNameHeight) &&
+                   value.hasValue(PropNameRx) && value.hasValue(PropNameRy)) {
+          auto x = value.getValue(PropNameX);
+          auto y = value.getValue(PropNameY);
+          auto width = value.getValue(PropNameWidth);
+          auto height = value.getValue(PropNameHeight);
+          auto rx = value.getValue(PropNameRx);
+          auto ry = value.getValue(PropNameRy);
+          // Update cache from js object value
+          _rrect = std::make_shared<SkRRect>(SkRRect::MakeRectXY(
+              SkRect::MakeXYWH(x.getAsNumber(), y.getAsNumber(),
+                               width.getAsNumber(), height.getAsNumber()),
+              rx.getAsNumber(), ry.getAsNumber()));
+        }
+      }
     }
   }
 
-  bool isSet() override {
-    return _pathProp->isSet() || _rectProp->isSet() || _rrectProp->isSet();
-  }
+  bool isSet() override { return _clipProp->isSet(); }
 
-  const SkPath *getPath() { return _path.get(); }
-  const SkRect *getRect() { return _rect.get(); }
-  const SkRRect *getRRect() { return _rrect.get(); }
+  SkPath *getPath() { return _path.get(); }
+  SkRect *getRect() { return _rect.get(); }
+  SkRRect *getRRect() { return _rrect.get(); }
+
+  std::shared_ptr<SkPath> _path;
+  std::shared_ptr<SkRect> _rect;
+  std::shared_ptr<SkRRect> _rrect;
 
 private:
-  PathProp *_pathProp;
-  RectProp *_rectProp;
-  RRectProp *_rrectProp;
-
-  std::shared_ptr<const SkPath> _path;
-  std::shared_ptr<const SkRect> _rect;
-  std::shared_ptr<const SkRRect> _rrect;
+  NodeProp *_clipProp;
 };
 
 } // namespace RNSkia
