eeab23dd0e260ba9741b42a2f116b0770de4e223
William Candillon <wcandillon@gmail.com>
2023-03-30 15:43:09 +0200

    Remove unecessary copy

--- package/cpp/rnskia/dom/props/RRectProp.h
+++ package/cpp/rnskia/dom/props/RRectProp.h
@@ -27,9 +27,7 @@ std::shared_ptr<SkRRect> processRRect(const JsiValue &value) {
         std::dynamic_pointer_cast<JsiSkRRect>(value.getAsHostObject());
     if (rectPtr != nullptr) {
       auto rrect = rectPtr->getObject();
-      return std::make_shared<SkRRect>(SkRRect::MakeRectXY(
-          SkRect::MakeXYWH(rrect->rect().x(), rrect->rect().y(),
-                           rrect->rect().width(), rrect->rect().height()),
+      return std::make_shared<SkRRect>(SkRRect::MakeRectXY(rrect->rect(),
           rrect->getSimpleRadii().x(), rrect->getSimpleRadii().y()));
     }
   } else {
