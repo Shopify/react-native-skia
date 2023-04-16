bd2908447fcca5810f0154cc69cb661a4ca0c55a
William Candillon <wcandillon@gmail.com>
2023-04-16 10:51:11 +0200

    Example

--- example/src/Examples/API/Backbuffer.tsx
+++ example/src/Examples/API/Backbuffer.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import type { SkImage } from "@shopify/react-native-skia";
 import {
   FilterMode,
   MipmapMode,
@@ -13,30 +14,23 @@ import { frag } from "../../components/Tags";
 
 const bufferA = frag`
 uniform vec3 iMouse;
-uniform vec2 iResolution;
 uniform shader iChannel0;
 
 half4 main(vec2 fragCoord)
 {
-    vec2 uv = fragCoord.xy / iResolution.xy;
-    
-    vec3 color = iChannel0.eval(uv).rgb;
-
     float distance = length(fragCoord.xy - iMouse.xy);
     if (distance < 9.0 && iMouse.z > 0.01) {
-        color = vec3(1.0, 0.0, 0.0);
+        return vec4(1.0, 0.0, 0.0, 1.0);
     }
 
-    return vec4(color, 1.0);
+    return iChannel0.eval(fragCoord);
 }`;
 
 const { width, height } = Dimensions.get("window");
-const backbuffer = Skia.Surface.Make(width, height)!;
-
-const bg = Skia.Shader.MakeColor(Float32Array.of(0.3, 0.6, 0.9, 1));
+const offscreen = Skia.Surface.Make(width, height)!;
 
 export const Backbuffer = () => {
-  const backshader = useRef(bg);
+  const backbuffer = useRef<SkImage | null>(null);
   const onDraw = useDrawCallback((canvas, info) => {
     const touch = { x: 0, y: 0, z: 0 };
     const lastTouch = info.touches[0];
@@ -50,25 +44,34 @@ export const Backbuffer = () => {
     const paint = Skia.Paint();
     paint.setShader(
       bufferA.makeShaderWithChildren(
-        [touch.x, touch.y, touch.z, width, height],
-        [backshader.current]
+        [touch.x, touch.y, touch.z],
+        [
+          backbuffer.current
+            ? backbuffer.current.makeShaderOptions(
+                TileMode.Decal,
+                TileMode.Decal,
+                FilterMode.Nearest,
+                MipmapMode.None
+              )
+            : Skia.Shader.MakeColor(Float32Array.of(0, 0, 0, 1)),
+        ]
       )
     );
-    backbuffer.getCanvas().drawPaint(paint);
+    offscreen.getCanvas().drawPaint(paint);
 
     // 2. Swap
-    backshader.current = backbuffer
-      .makeImageSnapshot()
-      .makeShaderOptions(
+    backbuffer.current = offscreen.makeImageSnapshot();
+
+    // 3. Draw Result
+    const frontPaint = Skia.Paint();
+    frontPaint.setShader(
+      backbuffer.current.makeShaderOptions(
         TileMode.Decal,
         TileMode.Decal,
         FilterMode.Nearest,
         MipmapMode.None
-      );
-
-    // 3. Draw Result
-    const frontPaint = Skia.Paint();
-    frontPaint.setShader(backshader.current);
+      )
+    );
     canvas.drawPaint(frontPaint);
   });
   return <SkiaView style={{ flex: 1 }} onDraw={onDraw} />;
