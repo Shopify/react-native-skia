263448ee3b02a0d3f20a6ae8b4bb93be3537c4e3
William Candillon <wcandillon@gmail.com>
2023-04-20 10:28:58 +0200

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,66 +1,175 @@
-import { View, useWindowDimensions } from "react-native";
-import { Canvas, Circle, Fill } from "@shopify/react-native-skia";
-import { GestureDetector, Gesture } from "react-native-gesture-handler";
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-} from "react-native-reanimated";
+import type { LinkingOptions } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "react-native";
+import type { HeaderBackButtonProps } from "@react-navigation/elements";
+import { HeaderBackButton } from "@react-navigation/elements";
+import { FiberProvider } from "its-fine";
 
-const radius = 30;
+import {
+  AnimationExample,
+  ReanimatedExample,
+  API,
+  Aurora,
+  Breathe,
+  Filters,
+  Gooey,
+  GraphsScreen,
+  Hue,
+  Matrix,
+  Glassmorphism,
+  Neumorphism,
+  PerformanceDrawingTest,
+  Wallpaper,
+  Vertices,
+  Wallet,
+  Severance,
+} from "./Examples";
+import { CI, Tests } from "./Tests";
+import { HomeScreen } from "./Home";
+import type { StackParamList } from "./types";
+import { useAssets } from "./Tests/useAssets";
 
-const useBallTracker = (defaultX: number, defaultY: number) => {
-  // The position of the ball
-  const x = useSharedValue(defaultX);
-  const y = useSharedValue(defaultY);
-  // This is the style we will apply to the "invisible" animated view
-  // that will overlay the ball
-  const style = useAnimatedStyle(() => ({
-    position: "absolute",
-    top: -radius,
-    left: -radius,
-    width: radius * 2,
-    height: radius * 2,
-    backgroundColor: "rgba(100, 200, 300, 0.4)",
-    transform: [{ translateX: x.value }, { translateY: y.value }],
-  }));
-  // The gesture handler for the ball
-  const gesture = Gesture.Pan().onChange((e) => {
-    x.value += e.x;
-    y.value += e.y;
-  });
-  return [x, y, gesture, style];
+const linking: LinkingOptions<StackParamList> = {
+  config: {
+    screens: {
+      Home: "",
+      Vertices: "vertices",
+      API: "api",
+      Breathe: "breathe",
+      Filters: "filters",
+      Gooey: "gooey",
+      Hue: "hue",
+      Matrix: "matrix",
+      Severance: "severance",
+      Aurora: "aurora",
+      Glassmorphism: "glassmorphism",
+      Neumorphism: "neumorphism",
+      Wallpaper: "wallpaper",
+      Wallet: "wallet",
+      Graphs: "graphs",
+      Animation: "animation",
+      Reanimated: "reanimated",
+      Performance: "performance",
+      Tests: "test",
+    },
+  },
+  prefixes: ["rnskia://"],
 };
 
-export const App = () => {
-  // The position of the ball
-  const x = useSharedValue(100);
-  const y = useSharedValue(100);
-  // This is the style we will apply to the "invisible" animated view
-  // that will overlay the ball
-  const style = useAnimatedStyle(() => ({
-    position: "absolute",
-    top: -radius,
-    left: -radius,
-    width: radius * 2,
-    height: radius * 2,
-    transform: [{ translateX: x.value }, { translateY: y.value }],
-  }));
-  // The gesture handler for the ball
-  const gesture = Gesture.Pan().onChange((e) => {
-    x.value += e.x;
-    y.value += e.y;
-  });
+const HeaderLeft = (props: HeaderBackButtonProps) => {
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1 }}>
-      <Canvas style={{ flex: 1 }}>
-        <Fill color="white" />
-        <Circle cx={x} cy={y} r={radius} color="cyan" />
-      </Canvas>
-      <GestureDetector gesture={gesture}>
-        <Animated.View style={style} />
-      </GestureDetector>
-    </View>
+    <HeaderBackButton
+      {...props}
+      onPress={() => {
+        if (navigation.canGoBack()) {
+          navigation.goBack();
+        }
+      }}
+      testID="back"
+    />
   );
 };
 
+const App = () => {
+  const Stack = createNativeStackNavigator<StackParamList>();
+  const assets = useAssets();
+  if (assets === null) {
+    return null;
+  }
+  return (
+    <FiberProvider>
+      <StatusBar hidden />
+      <NavigationContainer linking={linking}>
+        <Stack.Navigator
+          screenOptions={{
+            headerLeft: HeaderLeft,
+          }}
+          initialRouteName={CI ? "Tests" : "Home"}
+        >
+          <Stack.Screen
+            name="Home"
+            key="Home"
+            component={HomeScreen}
+            options={{
+              title: "🎨 Skia",
+            }}
+          />
+          <Stack.Screen
+            key="Tests"
+            name="Tests"
+            options={{
+              title: "🔧 Tests",
+            }}
+          >
+            {(props) => <Tests {...props} assets={assets} />}
+          </Stack.Screen>
+          <Stack.Screen
+            name="Vertices"
+            component={Vertices}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen name="API" component={API} />
+          <Stack.Screen name="Breathe" component={Breathe} />
+          <Stack.Screen name="Filters" component={Filters} />
+          <Stack.Screen name="Gooey" component={Gooey} />
+          <Stack.Screen name="Hue" component={Hue} />
+          <Stack.Screen
+            name="Matrix"
+            component={Matrix}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen
+            name="Severance"
+            component={Severance}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen
+            name="Aurora"
+            component={Aurora}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen
+            name="Glassmorphism"
+            component={Glassmorphism}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen name="Neumorphism" component={Neumorphism} />
+          <Stack.Screen
+            name="Wallpaper"
+            component={Wallpaper}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen
+            name="Wallet"
+            component={Wallet}
+            options={{
+              header: () => null,
+            }}
+          />
+          <Stack.Screen name="Graphs" component={GraphsScreen} />
+          <Stack.Screen name="Animation" component={AnimationExample} />
+          <Stack.Screen name="Reanimated" component={ReanimatedExample} />
+          <Stack.Screen name="Performance" component={PerformanceDrawingTest} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FiberProvider>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
 export default App;
