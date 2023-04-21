61f7f6c90a0d2d424f24a84cab0b42c20e77db35
chrfalch <christian.falch@gmail.com>
2023-04-21 12:52:14 +0200

    Clang format

--- package/ios/RNSkia-iOS/ViewScreenshotService.mm
+++ package/ios/RNSkia-iOS/ViewScreenshotService.mm
@@ -28,36 +28,45 @@
 
   // Get size
   CGSize size = view.frame.size;
-  
-  // Setup context
-  UIGraphicsImageRendererFormat *format = [UIGraphicsImageRendererFormat defaultFormat];
-  format.opaque = NO;
-  UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:size format:format];
 
-  // Render to context - this is now the only part of this function that shows up in the profiler!
-  UIImage *image = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull context) {
-      [view drawViewHierarchyInRect:(CGRect){CGPointZero, size} afterScreenUpdates:YES];
-  }];
+  // Setup context
+  UIGraphicsImageRendererFormat *format =
+      [UIGraphicsImageRendererFormat defaultFormat];
+  format.opaque = NO;
+  UIGraphicsImageRenderer *renderer =
+      [[UIGraphicsImageRenderer alloc] initWithSize:size format:format];
+
+  // Render to context - this is now the only part of this function that shows
+  // up in the profiler!
+  UIImage *image = [renderer
+      imageWithActions:^(UIGraphicsImageRendererContext *_Nonnull context) {
+        [view drawViewHierarchyInRect:(CGRect){CGPointZero, size}
+                   afterScreenUpdates:YES];
+      }];
 
   // Convert from UIImage -> CGImage -> SkImage
   CGImageRef cgImage = image.CGImage;
-  
+
   // Get some info about the image
   auto width = CGImageGetWidth(cgImage);
   auto height = CGImageGetHeight(cgImage);
   auto bytesPerRow = CGImageGetBytesPerRow(cgImage);
-    
-  // Convert from UIImage -> SkImage, start by getting the pixels directly from the CGImage:
+
+  // Convert from UIImage -> SkImage, start by getting the pixels directly from
+  // the CGImage:
   auto dataRef = CGDataProviderCopyData(CGImageGetDataProvider(cgImage));
   auto length = CFDataGetLength(dataRef);
   void *data = CFDataGetMutableBytePtr((CFMutableDataRef)dataRef);
-  
+
   // Now we'll capture the data in an SkData object and control releasing it:
-  auto skData = SkData::MakeWithProc(data, length, [](const void* ptr, void* context) {
-      CFDataRef dataRef = (CFDataRef)context;
-      CFRelease(dataRef);
-  }, (void*)dataRef);
-  
+  auto skData = SkData::MakeWithProc(
+      data, length,
+      [](const void *ptr, void *context) {
+        CFDataRef dataRef = (CFDataRef)context;
+        CFRelease(dataRef);
+      },
+      (void *)dataRef);
+
   // Make SkImageInfo
   SkImageInfo info =
       SkImageInfo::Make(static_cast<int>(width), static_cast<int>(height),
