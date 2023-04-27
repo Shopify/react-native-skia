eb46b032198c34b25880d321aa02fd01ff77cee4
Christian Falch <875252+chrfalch@users.noreply.github.com>
2023-04-27 20:00:46 +0200

    Feature: View screenshots -> SkImage (#1433)

--- package/ios/RNSkia-iOS/RNSkiOSPlatformContext.mm
+++ package/ios/RNSkia-iOS/RNSkiOSPlatformContext.mm
@@ -62,6 +62,14 @@ sk_sp<SkSurface> RNSkiOSPlatformContext::makeOffscreenSurface(int width,
   return MakeOffscreenMetalSurface(width, height);
 }
 
+void RNSkiOSPlatformContext::runOnMainThread(std::function<void()> func) {
+  _dispatchMainThread(func);
+}
+
+sk_sp<SkImage> RNSkiOSPlatformContext::takeScreenshotFromViewTag(size_t tag) {
+  return _takeViewScreenshot(tag);
+}
+
 void RNSkiOSPlatformContext::startDrawLoop() {
   if (_displayLink == nullptr) {
     _displayLink = [[DisplayLink alloc] init];
