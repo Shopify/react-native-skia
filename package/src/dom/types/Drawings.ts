9e7e5532d3dd870535e36f345470550e71f6e73c
chrfalch <christian.falch@gmail.com>
2023-04-30 10:04:11 +0200

    Added support for accepting null for font prop
    - Removed required from props in C++
    - Removed required from props in TS
    - Added tests to not render if font is null.

--- package/src/dom/types/Drawings.ts
+++ package/src/dom/types/Drawings.ts
@@ -109,14 +109,14 @@ export interface DiffRectProps extends DrawingNodeProps {
 }
 
 export interface TextProps extends DrawingNodeProps {
-  font: SkFont;
+  font: SkFont | null;
   text: string;
   x: number;
   y: number;
 }
 
 export interface TextPathProps extends DrawingNodeProps {
-  font: SkFont;
+  font: SkFont | null;
   text: string;
   path: PathDef;
   initialOffset: number;
@@ -134,7 +134,7 @@ export interface Glyph {
 }
 
 export interface GlyphsProps extends DrawingNodeProps {
-  font: SkFont;
+  font: SkFont | null;
   x: number;
   y: number;
   glyphs: Glyph[];
