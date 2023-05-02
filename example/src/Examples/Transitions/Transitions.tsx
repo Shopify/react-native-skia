5a6166adb59ead775ba50706b978b2646e236f80
William Candillon <wcandillon@gmail.com>
2023-05-02 14:56:52 +0200

    :wrench:

--- example/src/Examples/Transitions/Transitions.tsx
+++ example/src/Examples/Transitions/Transitions.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useState,
+} from "react";
 import { Dimensions, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import {
@@ -43,29 +49,35 @@ const arr = [1, 2, 3, 4, 5];
 console.log(getElementAtIndex(arr, 7)); // Output: 3
 console.log(getElementAtIndex(arr, -2)); // Output: 4
 */
-const at = <T,>(array: T[], index: number) => {
-  if (array.length === 0) {
-    throw new Error("Array is empty.");
+const at = <T,>(array: T[] | null, index: number): T => {
+  "worklet";
+  if (array === null) {
+    return null;
   }
   return array[((index % array.length) + array.length) % array.length];
 };
 
+const duration = 100;
+
 export const Transitions = () => {
-  const [offset, setOffset] = useState(0);
+  const offset = useSharedValue(0);
   const progressLeft = useSharedValue(0);
   const progressRight = useSharedValue(0);
   const assets = useAssets();
   const next = useCallback(() => {
-    setOffset((o) => (o + 1) % transitions.length);
+    offset.value += 1;
     progressLeft.value = 0;
-  }, [progressLeft]);
+  }, [offset, progressLeft]);
   const previous = useCallback(() => {
-    setOffset((o) => (o - 1) % transitions.length);
+    offset.value -= 1;
     progressRight.value = 0;
-  }, [progressRight]);
+  }, [offset, progressRight]);
   const panRight = useMemo(
     () =>
       Gesture.Pan()
+        .onBegin(() => {
+          progressRight.value = 0;
+        })
         .activeOffsetX(10)
         .onChange((pos) => {
           progressRight.value = clamp(
@@ -76,7 +88,7 @@ export const Transitions = () => {
         })
         .onEnd(({ velocityX }) => {
           const dst = snapPoint(progressRight.value, velocityX / width, [0, 1]);
-          progressRight.value = withTiming(dst, { duration: 250 }, () => {
+          progressRight.value = withTiming(dst, { duration }, () => {
             runOnJS(previous)();
           });
         }),
@@ -95,7 +107,7 @@ export const Transitions = () => {
         })
         .onEnd(({ velocityX }) => {
           const dst = snapPoint(progressLeft.value, -velocityX / width, [0, 1]);
-          progressLeft.value = withTiming(dst, { duration: 250 }, () => {
+          progressLeft.value = withTiming(dst, { duration }, () => {
             runOnJS(next)();
           });
         }),
@@ -115,6 +127,25 @@ export const Transitions = () => {
       resolution: [width, height],
     };
   });
+  const transition1 = useDerivedValue(() => {
+    return at(transitions, offset.value - 1);
+  });
+
+  const transition2 = useDerivedValue(() => {
+    return at(transitions, offset.value);
+  });
+
+  const assets1 = useDerivedValue(() => {
+    return at(assets, offset.value - 1);
+  });
+
+  const assets2 = useDerivedValue(() => {
+    return at(assets, offset.value);
+  });
+
+  const assets3 = useDerivedValue(() => {
+    return at(assets, offset.value + 1);
+  });
 
   if (!assets) {
     return null;
@@ -124,26 +155,23 @@ export const Transitions = () => {
       <GestureDetector gesture={Gesture.Race(panRight, panLeft)}>
         <Canvas style={{ flex: 1 }}>
           <Fill>
-            <Shader
-              source={at(transitions, offset - 1)}
-              uniforms={uniformsRight}
-            >
-              <Shader source={at(transitions, offset)} uniforms={uniformsLeft}>
+            <Shader source={transition1} uniforms={uniformsRight}>
+              <Shader source={transition2} uniforms={uniformsLeft}>
                 <ImageShader
-                  image={at(assets, offset)}
+                  image={assets2}
                   fit="cover"
                   width={width}
                   height={height}
                 />
                 <ImageShader
-                  image={at(assets, offset + 1)}
+                  image={assets3}
                   fit="cover"
                   width={width}
                   height={height}
                 />
               </Shader>
               <ImageShader
-                image={at(assets, offset - 1)}
+                image={assets1}
                 fit="cover"
                 width={width}
                 height={height}
