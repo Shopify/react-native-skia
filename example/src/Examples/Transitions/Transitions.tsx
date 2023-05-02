579279ec45fad8650651f54d648026716ef68536
William Candillon <wcandillon@gmail.com>
2023-05-02 14:00:17 +0200

    :wrench:

--- example/src/Examples/Transitions/Transitions.tsx
+++ example/src/Examples/Transitions/Transitions.tsx
@@ -38,8 +38,37 @@ const transitions = [
 ].map((t) => transition(t));
 
 export const Transitions = () => {
+  const [offset, setOffset] = useState(0);
   const progressLeft = useSharedValue(0);
+  const progressRight = useSharedValue(0);
   const assets = useAssets();
+  const next = useCallback(() => {
+    setOffset((o) => (o + 1) % transitions.length);
+    progressLeft.value = 0;
+  }, [progressLeft]);
+  const previous = useCallback(() => {
+    setOffset((o) => (o - 1) % transitions.length);
+    progressRight.value = 0;
+  }, [progressRight]);
+  const panRight = useMemo(
+    () =>
+      Gesture.Pan()
+        .activeOffsetX(10)
+        .onChange((pos) => {
+          progressRight.value = clamp(
+            progressRight.value + pos.changeX / width,
+            0,
+            1
+          );
+        })
+        .onEnd(({ velocityX }) => {
+          const dst = snapPoint(progressRight.value, velocityX / width, [0, 1]);
+          progressRight.value = withTiming(dst, { duration: 250 }, () => {
+            runOnJS(previous)();
+          });
+        }),
+    [previous, progressRight]
+  );
   const panLeft = useMemo(
     () =>
       Gesture.Pan()
@@ -53,10 +82,19 @@ export const Transitions = () => {
         })
         .onEnd(({ velocityX }) => {
           const dst = snapPoint(progressLeft.value, -velocityX / width, [0, 1]);
-          progressLeft.value = withTiming(dst, { duration: 250 });
+          progressLeft.value = withTiming(dst, { duration: 250 }, () => {
+            runOnJS(next)();
+          });
         }),
-    [progressLeft]
+    [next, progressLeft]
   );
+
+  const uniformsRight = useDerivedValue(() => {
+    return {
+      progress: progressRight.value,
+      resolution: [width, height],
+    };
+  });
 
   const uniformsLeft = useDerivedValue(() => {
     return {
@@ -70,18 +108,44 @@ export const Transitions = () => {
   }
   return (
     <View style={{ flex: 1 }}>
-      <GestureDetector gesture={panLeft}>
+      <GestureDetector gesture={Gesture.Race(panRight, panLeft)}>
         <Canvas style={{ flex: 1 }}>
           <Fill>
-            <Shader source={transitions[0]} uniforms={uniformsLeft}>
+            <Shader
+              source={
+                transitions[
+                  offset === 0
+                    ? transitions.length - 1
+                    : (offset - 1) % transitions.length
+                ]
+              }
+              uniforms={uniformsRight}
+            >
+              <Shader
+                source={transitions[offset % transitions.length]}
+                uniforms={uniformsLeft}
+              >
+                <ImageShader
+                  image={assets[offset % assets.length]}
+                  fit="cover"
+                  width={width}
+                  height={height}
+                />
+                <ImageShader
+                  image={assets[(offset + 1) % assets.length]}
+                  fit="cover"
+                  width={width}
+                  height={height}
+                />
+              </Shader>
               <ImageShader
-                image={assets[0]}
-                fit="cover"
-                width={width}
-                height={height}
-              />
-              <ImageShader
-                image={assets[1]}
+                image={
+                  assets[
+                    offset === 0
+                      ? assets.length - 1
+                      : (offset - 1) % assets.length
+                  ]
+                }
                 fit="cover"
                 width={width}
                 height={height}
