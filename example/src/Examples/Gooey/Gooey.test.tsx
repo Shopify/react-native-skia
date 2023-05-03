4a8cf31572cf9c2926c32497496ff35c01b2d90e
William Candillon <wcandillon@gmail.com>
2023-05-03 11:17:03 +0200

    More tests

--- example/src/Examples/Gooey/Gooey.test.tsx
+++ example/src/Examples/Gooey/Gooey.test.tsx
@@ -1,18 +1,8 @@
-import { Canvas, Fill, useTouchHandler } from "@shopify/react-native-skia";
 import { render } from "@testing-library/react-native";
 import * as React from "react";
 
-//import { Gooey } from "./Gooey";
-
-const Test = () => {
-  const onTouch = useTouchHandler({});
-  return (
-    <Canvas style={{ flex: 1 }} onTouch={onTouch}>
-      <Fill color="cyan" />
-    </Canvas>
-  );
-};
+import { Gooey } from "./Gooey";
 
 it("renders correctly", () => {
-  render(<Test />);
+  render(<Gooey />);
 });
--- package/src/mock/index.ts
+++ package/src/mock/index.ts
@@ -1,29 +1,32 @@
 import type { CanvasKit } from "canvaskit-wasm";
 
 import { JsiSkApi } from "../skia/web";
+import { ValueApi } from "../values/web";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Noop: () => any = () => undefined;
+const NoopValue = () => ({ current: 0 });
 
 export const Mock = (CanvasKit: CanvasKit) => {
   global.SkiaApi = JsiSkApi(CanvasKit);
-  //  global.SkiaValueApi = ValueApi;
-  // console.log(global.SkiaValueApi);
+  global.SkiaValueApi = ValueApi;
   const Skia = global.SkiaApi;
   return {
     Skia,
     ...require("../renderer/components"),
     ...require("../skia"),
+    ...require("../values"),
     ...require("../animation"),
     ...require("../dom/types"),
     ...require("../dom/nodes"),
     // We could use the real Canvas if we mock the SkiaView component for node
     Canvas: Noop,
-    useComputedValue: Noop,
+    useValue: NoopValue,
+    useComputedValue: NoopValue,
     useTouchHandler: Noop,
-    useTiming: Noop,
-    useLoop: Noop,
-    useSpring: Noop,
+    useTiming: NoopValue,
+    useLoop: NoopValue,
+    useSpring: NoopValue,
     useRawData: Noop,
     useData: Noop,
     useFont: Noop,
