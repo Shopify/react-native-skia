05d9f1e524ca47d1eec837ffc8df7de97b5a2de5
William Candillon <wcandillon@gmail.com>
2023-05-04 10:28:42 +0200

    :green_heart:

--- example/src/Examples/Reanimated/index.tsx
+++ example/src/Examples/Reanimated/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 import { AnimateTextOnPath } from "./AnimateTextOnPath";
