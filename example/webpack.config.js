de91176cddf134ad471989b3044aa1ba0b0feac0
Jonny Burger <jonathanburger11@gmail.com>
2023-05-14 19:31:34 +0200

    add back to webpack config - react navigation app still needs it

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -74,12 +74,17 @@ module.exports = {
     },
     extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", ".web.js", ".js"],
     alias: {
+      "react-native$": "react-native-web",
       // The next line is to be able to correctly resolve react dependencies
       // in the library (package folder) avoiding the dreaded error
       // "Hooks can only be called inside the body of a function component"
       // where we end up with two different react modules. This should
       // NOT be necessary in production when installing from NPM.
       react: path.resolve(__dirname, "./node_modules/react"),
+      "react-native-web": path.resolve(
+        __dirname,
+        "./node_modules/react-native-web"
+      ),
       "react-native-reanimated/package.json": require.resolve(
         "react-native-reanimated/package.json"
       ),
