49f0b3d88fd5ce7633e3d84a9d78202e60f61c82
William Candillon <wcandillon@gmail.com>
2023-07-13 11:53:00 +0200

    Fix CI

--- package/src/skia/web/JsiSkTypefaceFontProvider.ts
+++ package/src/skia/web/JsiSkTypefaceFontProvider.ts
@@ -22,22 +22,55 @@ export class JsiSkTypefaceFontProvider
     throw new Error("Method not implemented.");
   }
   registerFont(typeface: SkTypeface, familyName: string) {
-    //https://emscripten.org/docs/api_reference/preamble.js.html#stringToUTF8
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     const strLen = lengthBytesUTF8(familyName) + 1;
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     const strPtr = this.CanvasKit._malloc(strLen);
+    stringToUTF8(this.CanvasKit, familyName, strPtr, strLen);
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
-    stringToUTF8(familyName, strPtr, strLen);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    this.CanvasKit._registerFont(typeface, strPtr);
+    this.ref._registerFont(typeface.ref, strPtr);
   }
 
   dispose() {
     this.ref.delete();
   }
 }
+
+const lengthBytesUTF8 = (str: string) => {
+  // TextEncoder will give us the byte length in UTF8 form
+  const encoder = new TextEncoder();
+  const utf8 = encoder.encode(str);
+  return utf8.length;
+};
+
+const stringToUTF8 = (
+  CanvasKit: CanvasKit,
+  str: string,
+  outPtr: number,
+  maxBytesToWrite: number
+) => {
+  // TextEncoder will give us the byte array in UTF8 form
+  const encoder = new TextEncoder();
+  const utf8 = encoder.encode(str);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const heap: Int8Array = CanvasKit.HEAPU8;
+
+  // Check if there's enough space
+  if (utf8.length > maxBytesToWrite) {
+    throw new Error("Not enough space to write UTF8 encoded string");
+  }
+
+  // Copy the bytes
+  for (let i = 0; i < utf8.length; i++) {
+    heap[outPtr + i] = utf8[i];
+  }
+
+  // Null terminate
+  if (utf8.length < maxBytesToWrite) {
+    heap[outPtr + utf8.length] = 0;
+  }
+};
