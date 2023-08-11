f4e3c8a1d497ef04ca2d5f8a89dc61152f5cbfba
chrfalch <christian.falch@gmail.com>
2023-08-11 15:33:22 +0200

    Code review:
    - Moved construction/destruction of display/config to singleton
    - Made shared/parent context initialised and destroyed in singleton

--- package/android/cpp/rnskia-android/SkiaOpenGLHelper.h
+++ package/android/cpp/rnskia-android/SkiaOpenGLHelper.h
@@ -28,7 +28,35 @@ namespace RNSkia {
  */
 class OpenGLResourceHolder {
 private:
-  OpenGLResourceHolder() {}
+  OpenGLResourceHolder() {
+    // Initialize OpenGL
+    glDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (glDisplay == EGL_NO_DISPLAY) {
+      RNSkLogger::logToConsole("eglGetDisplay failed : %i", glGetError());
+      return;
+    }
+
+    EGLint major;
+    EGLint minor;
+    if (eglInitialize(glDisplay, &major, &minor) != EGL_TRUE) {
+      RNSkLogger::logToConsole("eglInitialize failed : %i", glGetError());
+      return;
+    }
+
+    // Create a default shared context
+    glConfig = getConfig(glDisplay);
+
+    // Create OpenGL context attributes
+    EGLint contextAttribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
+
+    // Initialize the offscreen context for this thread
+    glContext =
+        eglCreateContext(glDisplay, glConfig, glContext, contextAttribs);
+    if (glContext == EGL_NO_CONTEXT) {
+      RNSkLogger::logToConsole("eglCreateContext failed : %i", glGetError());
+    }
+  }
+
   ~OpenGLResourceHolder() {
     if (glContext != EGL_NO_CONTEXT) {
       eglDestroyContext(glDisplay, glContext);
@@ -61,13 +89,56 @@ public:
    */
   std::atomic<EGLDisplay> glDisplay = EGL_NO_DISPLAY;
 
+  /**
+   * Shared eglConfig
+   */
+  std::atomic<EGLConfig> glConfig = 0;
+
+private:
+  /**
+   * Finds the correct EGL Config for the given parameters
+   * @param glDisplay
+   * @return Config or zero if no matching context could be found.
+   */
+  static EGLConfig getConfig(EGLDisplay glDisplay) {
+
+    EGLint att[] = {EGL_RENDERABLE_TYPE,
+                    EGL_OPENGL_ES2_BIT,
+                    EGL_ALPHA_SIZE,
+                    8,
+                    EGL_BLUE_SIZE,
+                    8,
+                    EGL_GREEN_SIZE,
+                    8,
+                    EGL_RED_SIZE,
+                    8,
+                    EGL_DEPTH_SIZE,
+                    0,
+                    EGL_STENCIL_SIZE,
+                    0,
+                    EGL_SAMPLE_BUFFERS,
+                    0,
+                    EGL_NONE};
+
+    EGLint numConfigs;
+    EGLConfig glConfig = 0;
+    if (eglChooseConfig(glDisplay, att, &glConfig, 1, &numConfigs) !=
+            EGL_TRUE ||
+        numConfigs == 0) {
+      RNSkLogger::logToConsole(
+          "Failed to choose a config for %s surface. Error code: %d\n",
+          eglGetError());
+      return 0;
+    }
+
+    return glConfig;
+  }
 };
 
 struct SkiaOpenGLContext {
   SkiaOpenGLContext() {
     glContext = EGL_NO_CONTEXT;
     gl1x1Surface = EGL_NO_SURFACE;
-    glConfig = 0;
     directContext = nullptr;
   }
   ~SkiaOpenGLContext() {
@@ -91,7 +162,6 @@ struct SkiaOpenGLContext {
     }
   }
   EGLContext glContext;
-  EGLConfig glConfig;
   EGLSurface gl1x1Surface;
   sk_sp<GrDirectContext> directContext;
 };
@@ -130,10 +200,9 @@ public:
    */
   static EGLSurface createWindowedSurface(ANativeWindow *window) {
     const EGLint attribs[] = {EGL_NONE};
-    return eglCreateWindowSurface(
-        OpenGLResourceHolder::getInstance().glDisplay,
-        getConfig(OpenGLResourceHolder::getInstance().glDisplay), window,
-        attribs);
+    return eglCreateWindowSurface(OpenGLResourceHolder::getInstance().glDisplay,
+                                  OpenGLResourceHolder::getInstance().glConfig,
+                                  window, attribs);
   }
 
   /**
@@ -176,11 +245,6 @@ public:
    * @return true if the call to create a skia direct context suceeded.
    */
   static bool createSkiaDirectContext(SkiaOpenGLContext *context) {
-    // Initialize OpenGL
-    if (!initializeOpenGL()) {
-      return false;
-    }
-
     // Create the OpenGL context
     if (!createOpenGLContext(context)) {
       return false;
@@ -194,7 +258,8 @@ public:
 
       context->gl1x1Surface =
           eglCreatePbufferSurface(OpenGLResourceHolder::getInstance().glDisplay,
-                                  context->glConfig, offScreenSurfaceAttribs);
+                                  OpenGLResourceHolder::getInstance().glConfig,
+                                  offScreenSurfaceAttribs);
 
       if (context->gl1x1Surface == EGL_NO_SURFACE) {
         RNSkLogger::logToConsole("Failed creating a 1x1 pbuffer surface");
@@ -222,37 +287,6 @@ public:
 
 private:
   /**
-   * Initializes OpenGL using the provided SkiaOpenGLContext. This means
-   * getting the default display and calling eglInitializeContext. If the
-   * context already have a valid initialised display nothing will be done and
-   * the method will return true.
-   * @return True if already initialized or if eglGetDisplay and eglInitialize
-   * was successfull.
-   */
-  static bool initializeOpenGL() {
-    if (OpenGLResourceHolder::getInstance().glDisplay == EGL_NO_DISPLAY) {
-      // Get default display
-      OpenGLResourceHolder::getInstance().glDisplay =
-          eglGetDisplay(EGL_DEFAULT_DISPLAY);
-      if (OpenGLResourceHolder::getInstance().glDisplay == EGL_NO_DISPLAY) {
-        RNSkLogger::logToConsole("eglGetDisplay failed : %i", glGetError());
-        return false;
-      }
-
-      EGLint major;
-      EGLint minor;
-
-      if (eglInitialize(OpenGLResourceHolder::getInstance().glDisplay, &major,
-                        &minor) != EGL_TRUE) {
-        RNSkLogger::logToConsole("eglInitialize failed : %i", glGetError());
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  /**
    * Creates a new GLContext. If the context has a valid context no new context
    * will be created.
    * @param context Context to save results in
@@ -265,19 +299,13 @@ private:
       return true;
     }
 
-    // Create config
-    context->glConfig =
-        getConfig(OpenGLResourceHolder::getInstance().glDisplay);
-    if (context->glConfig == 0) {
-      return false;
-    }
-
     // Create OpenGL context attributes
     EGLint contextAttribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
 
     // Initialize the offscreen context for this thread
     context->glContext = eglCreateContext(
-        OpenGLResourceHolder::getInstance().glDisplay, context->glConfig,
+        OpenGLResourceHolder::getInstance().glDisplay,
+        OpenGLResourceHolder::getInstance().glConfig,
         OpenGLResourceHolder::getInstance().glContext, contextAttribs);
 
     if (context->glContext == EGL_NO_CONTEXT) {
@@ -285,53 +313,7 @@ private:
       return EGL_NO_CONTEXT;
     }
 
-    // Save to shared context so that we can share data between threads in
-    // OpenGL. The first context created will be the one passed to any
-    // subsequent EGLContexts created.
-    if (OpenGLResourceHolder::getInstance().glContext == EGL_NO_CONTEXT) {
-      OpenGLResourceHolder::getInstance().glContext = context->glContext;
-    }
-
     return true;
-  }
-
-  /**
-   * Finds the correct EGL Config for the given parameters
-   * @param glDisplay
-   * @return Config or zero if no matching context could be found.
-   */
-  static EGLConfig getConfig(EGLDisplay glDisplay) {
-
-    EGLint att[] = {EGL_RENDERABLE_TYPE,
-                    EGL_OPENGL_ES2_BIT,
-                    EGL_ALPHA_SIZE,
-                    8,
-                    EGL_BLUE_SIZE,
-                    8,
-                    EGL_GREEN_SIZE,
-                    8,
-                    EGL_RED_SIZE,
-                    8,
-                    EGL_DEPTH_SIZE,
-                    0,
-                    EGL_STENCIL_SIZE,
-                    0,
-                    EGL_SAMPLE_BUFFERS,
-                    0,
-                    EGL_NONE};
-
-    EGLint numConfigs;
-    EGLConfig glConfig = 0;
-    if (eglChooseConfig(glDisplay, att, &glConfig, 1, &numConfigs) !=
-            EGL_TRUE ||
-        numConfigs == 0) {
-      RNSkLogger::logToConsole(
-          "Failed to choose a config for %s surface. Error code: %d\n",
-          eglGetError());
-      return 0;
-    }
-
-    return glConfig;
   }
 };
 } // namespace RNSkia
