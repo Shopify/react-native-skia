4b66d237cd7dd06acdfc8000e519ef8deeac20fd
William Candillon <wcandillon@gmail.com>
2023-09-28 11:58:32 +0200

    Add test

--- package/src/renderer/__tests__/e2e/AnimatedImages.spec.tsx
+++ package/src/renderer/__tests__/e2e/AnimatedImages.spec.tsx
@@ -1,7 +1,7 @@
 import { surface } from "../setup";
 
 describe("Animated Images", () => {
-  it("should decode gifs", async () => {
+  it("should decode gifs (1)", async () => {
     const result = await surface.eval((Skia) => {
       const data = Skia.Data.fromBase64(
         "R0lGODlhAQABAIAAAGGqHwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
@@ -17,5 +17,22 @@ describe("Animated Images", () => {
       return true;
     });
     expect(result).toEqual(true);
+  });
+  it("should decode gifs (2)", async () => {
+    const result = await surface.eval((Skia) => {
+      const data = Skia.Data.fromBase64(
+        "R0lGODlhAQABAIAAAGGqHwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+      );
+      const animatedImage =
+        Skia.AnimatedImage.MakeAnimatedImageFromEncoded(data);
+      if (!animatedImage) {
+        return "";
+      }
+      return animatedImage.getCurrentFrame()!.encodeToBase64();
+    });
+    expect(result).toEqual(
+      // eslint-disable-next-line max-len
+      "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAANzQklUCAgI2+FP4AAAAAxJREFUCJljSFwlDwACmgErMAA+hwAAAABJRU5ErkJggg=="
+    );
   });
 });
