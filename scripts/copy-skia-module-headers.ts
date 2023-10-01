51fc559e3482862697b7972d7e862d6e9bba11ae
William Candillon <wcandillon@gmail.com>
2023-10-01 20:23:33 +0200

    Fix toCmds

--- scripts/copy-skia-module-headers.ts
+++ scripts/copy-skia-module-headers.ts
@@ -13,6 +13,7 @@ const copyModule = (module: string) => [
   `cp -a ./externals/skia/modules/skcms/. ./package/cpp/skia/modules/skcms`,
   `mkdir -p ./package/cpp/skia/src/`,
   `mkdir -p ./package/cpp/skia/src/core/`,
+  `cp -a ./externals/skia/src/core/SkPathPriv.h ./package/cpp/skia/src/core/.`,
   `cp -a ./externals/skia/src/core/SkChecksum.h ./package/cpp/skia/src/core/.`,
   `cp -a ./externals/skia/src/core/SkTHash.h ./package/cpp/skia/src/core/.`,
   `rm ./package/cpp/skia/include/core/SkICC.h`, // Remove since there are now (Skia M108) two headers with the same name
