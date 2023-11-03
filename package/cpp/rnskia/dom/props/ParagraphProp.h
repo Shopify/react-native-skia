479af59b3a2c907da31fdb6f2aab2a40e9edc70e
chrfalch <christian.falch@gmail.com>
2023-11-03 16:48:24 +0100

    Added Paragraphprop (to previous commit)

---
+++ package/cpp/rnskia/dom/props/ParagraphProp.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "DerivedNodeProp.h"
+
+#include "JsiSkParagraph.h"
+
+#include <memory>
+#include <string>
+
+namespace RNSkia {
+
+class ParagraphProp : public DerivedProp<Paragraph *> {
+public:
+  explicit ParagraphProp(PropId name,
+                         const std::function<void(BaseNodeProp *)> &onChange)
+      : DerivedProp<Paragraph *>(onChange) {
+    _paragraphProp = defineProperty<NodeProp>(name);
+  }
+
+  void updateDerivedValue() override {
+    if (_paragraphProp->value().getType() != PropType::HostObject) {
+      throw std::runtime_error("Expected Paragraph object for the " +
+                               std::string(getName()) + " property.");
+    }
+
+    auto ptr = std::dynamic_pointer_cast<JsiSkParagraph>(
+        _paragraphProp->value().getAsHostObject());
+
+    if (ptr == nullptr) {
+      throw std::runtime_error("Expected paragraph object for the " +
+                               std::string(getName()) + " property.");
+    }
+
+    setDerivedValue(ptr->getParagraph());
+  }
+
+private:
+  NodeProp *_paragraphProp;
+};
+
+} // namespace RNSkia
