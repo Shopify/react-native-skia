85e3f9999082680d7b0187d559444653f083bc26
William Candillon <wcandillon@gmail.com>
2023-11-23 11:12:46 +0100

    💚

--- example/src/Tests/Screens/Snapshot2.tsx
+++ example/src/Tests/Screens/Snapshot2.tsx
@@ -8,13 +8,7 @@ import {
   useWindowDimensions,
   ScrollView,
 } from "react-native";
-import {
-  Canvas,
-  RoundedRect,
-  useImage,
-  Image,
-  Skia,
-} from "@shopify/react-native-skia";
+import { Canvas, RoundedRect, Image, Skia } from "@shopify/react-native-skia";
 import { Switch } from "react-native-gesture-handler";
 
 export const Snapshot2 = () => {
