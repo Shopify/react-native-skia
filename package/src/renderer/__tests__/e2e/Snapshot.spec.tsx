3a1567b8a8d3aaa330f8877b0d7dd5477e8df920
William Candillon <wcandillon@gmail.com>
2023-11-23 18:29:41 +0100

    update test suite

--- package/src/renderer/__tests__/e2e/Snapshot.spec.tsx
+++ package/src/renderer/__tests__/e2e/Snapshot.spec.tsx
@@ -1,17 +1,26 @@
-import { checkImage, itRunsE2eOnly } from "../../../__tests__/setup";
+import { CI, checkImage, itRunsE2eOnly } from "../../../__tests__/setup";
 import { surface } from "../setup";
 
 describe("Snapshot", () => {
   itRunsE2eOnly("should capture a simple snapshot", async () => {
     const img = await surface.screen("Snapshot1");
-    checkImage(img, `snapshots/screens/snapshot1-${surface.OS}.png`);
+    checkImage(
+      img,
+      `snapshots/screens/snapshot1-${surface.OS}${CI ? "-ci" : ""}.png`
+    );
   });
   itRunsE2eOnly("should capture a somewhat complex snapshot", async () => {
     const img = await surface.screen("Snapshot2");
-    checkImage(img, `snapshots/screens/snapshot2-${surface.OS}.png`);
+    checkImage(
+      img,
+      `snapshots/screens/snapshot2-${surface.OS}${CI ? "-ci" : ""}.png`
+    );
   });
   itRunsE2eOnly("should respect overflow: hidden", async () => {
     const img = await surface.screen("Snapshot3");
-    checkImage(img, `snapshots/screens/snapshot3-${surface.OS}.png`);
+    checkImage(
+      img,
+      `snapshots/screens/snapshot3-${surface.OS}${CI ? "-ci" : ""}.png`
+    );
   });
 });
