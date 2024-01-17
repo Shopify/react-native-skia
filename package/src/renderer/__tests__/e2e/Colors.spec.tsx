9b71bddcd327258de76747a4e468e223c4a4f780
louix <28579246+louix@users.noreply.github.com>
2024-01-17 12:11:12 +0000

    Add offscreen canvas test

--- package/src/renderer/__tests__/e2e/Colors.spec.tsx
+++ package/src/renderer/__tests__/e2e/Colors.spec.tsx
@@ -35,4 +35,42 @@ describe("Colors", () => {
     });
     expect(result).toBe(true);
   });
+  it("should render the same color regardless of constructor input types", async () => {
+    // given (different inputs representing the same color
+    const colors = [
+      { kind: "string" as const, value: "red" },
+      { kind: "float32Array" as const, value: [1, 0, 0, 1] },
+      { kind: "number" as const, value: 0xffff0000 },
+      { kind: "array" as const, value: [1, 0, 0, 1] },
+    ];
+
+    // when (for each input we draw a colored canvas and encode it to bytes)
+    const buffers = await Promise.all(
+      colors.map((color) =>
+        surface
+          .drawOffscreen(
+            (Skia, canvas, ctx) => {
+              const c =
+                ctx.color.kind === "float32Array"
+                  ? // we cannot pass in a Float32Array via ctx, need to construct it inside
+                    new Float32Array(ctx.color.value)
+                  : ctx.color.value;
+              canvas.drawColor(Skia.Color(c));
+            },
+            { color }
+          )
+          .then((image) => image.encodeToBytes())
+      )
+    );
+
+    // then (expect the encoded bytes are equal)
+    for (let i = 1; i < buffers.length; i++) {
+      const prev = buffers[i - 1];
+      const curr = buffers[i];
+      expect(prev.length).toBe(curr.length);
+      for (let j = 0; j < prev.length; j++) {
+        expect(prev[j]).toBe(curr[j]);
+      }
+    }
+  });
 });
