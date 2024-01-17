81395fdf62a84a67ad3ddf660b8fa17245c87f2a
William Candillon <wcandillon@gmail.com>
2024-01-17 17:07:53 +0100

    Update Uniform type to accept readonly arrays

--- package/src/skia/types/Shader/Shader.ts
+++ package/src/skia/types/Shader/Shader.ts
@@ -7,7 +7,7 @@ export const isShader = (obj: SkJSIInstance<string> | null): obj is SkShader =>
 
 export type SkShader = SkJSIInstance<"Shader">;
 
-export type Uniform = number | Vector | Float32Array | Uniform[];
+export type Uniform = number | Vector | Float32Array | Uniform[] | readonly Uniform[];
 
 export interface Uniforms {
   [name: string]: Uniform;
