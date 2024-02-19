b8a7d050a716ff4b5a259a20451df8120991e805
William Candillon <wcandillon@gmail.com>
2024-02-19 10:07:01 +0100

    Fix bug in reanimated buffers

--- package/src/external/reanimated/buffers.ts
+++ package/src/external/reanimated/buffers.ts
@@ -1,15 +1,19 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 import type { SkColor, SkHostRect, SkPoint, SkRSXform } from "../../skia/types";
 import { Skia } from "../../skia";
 
-import { useDerivedValue, useSharedValue } from "./moduleWrapper";
+import { useSharedValue } from "./moduleWrapper";
 import { notifyChange } from "./interpolators";
+import { startMapper, stopMapper } from "react-native-reanimated";
+import { WorkletFunction } from "react-native-reanimated/lib/typescript/reanimated2/commonTypes";
+
+type Modifier<T> = (input: T, index: number) => void;
 
 const useBuffer = <T>(
   size: number,
   bufferInitializer: () => T,
-  modifier: (input: T, index: number) => void
+  modifier: Modifier<T>
 ) => {
   const buffer = useMemo(
     () => new Array(size).fill(0).map(bufferInitializer),
@@ -17,37 +21,44 @@ const useBuffer = <T>(
     [size]
   );
   const transforms = useSharedValue(buffer);
-
-  useDerivedValue(() => {
+  const mod = modifier as WorkletFunction;
+  const deps = Object.values(mod.__closure ?? {});
+  const mapperId = startMapper(() => {
+    "worklet";
     buffer.forEach((val, index) => {
       modifier(val, index);
     });
-    // Assuming notifyChange is a function that notifies about the change in transforms.
-    notifyChange(transforms);
-  });
+   notifyChange(transforms);
+  }, deps);
+
+  useEffect(() => {
+    return () => {
+      stopMapper(mapperId);
+    }
+  }, [mapperId]);
 
   return transforms;
 };
 
 export const useRectBuffer = (
   size: number,
-  modifier: (input: SkHostRect, index: number) => void
+  modifier: Modifier<SkHostRect>
 ) => useBuffer(size, () => Skia.XYWHRect(0, 0, 0, 0), modifier);
 
 // Usage for RSXform Buffer
 export const useRSXformBuffer = (
   size: number,
-  modifier: (input: SkRSXform, index: number) => void
+  modifier: Modifier<SkRSXform>,
 ) => useBuffer(size, () => Skia.RSXform(1, 0, 0, 0), modifier);
 
 // Usage for Point Buffer
 export const usePointBuffer = (
   size: number,
-  modifier: (input: SkPoint, index: number) => void
+  modifier: Modifier<SkPoint>,
 ) => useBuffer(size, () => Skia.Point(0, 0), modifier);
 
 // Usage for Color Buffer
 export const useColorBuffer = (
   size: number,
-  modifier: (input: SkColor, index: number) => void
+  modifier: Modifier<SkColor>,
 ) => useBuffer(size, () => Skia.Color("black"), modifier);
