75359c16f78c02d8910813dee30e36c0357c4b51
William Candillon <wcandillon@gmail.com>
2024-02-19 11:52:35 +0100

    :green_heart:

--- package/src/external/reanimated/buffers.ts
+++ package/src/external/reanimated/buffers.ts
@@ -1,12 +1,12 @@
 import { useEffect, useMemo } from "react";
+import { startMapper, stopMapper } from "react-native-reanimated";
+import type { WorkletFunction } from "react-native-reanimated/lib/typescript/reanimated2/commonTypes";
 
 import type { SkColor, SkHostRect, SkPoint, SkRSXform } from "../../skia/types";
 import { Skia } from "../../skia";
 
 import { useSharedValue } from "./moduleWrapper";
 import { notifyChange } from "./interpolators";
-import { startMapper, stopMapper } from "react-native-reanimated";
-import { WorkletFunction } from "react-native-reanimated/lib/typescript/reanimated2/commonTypes";
 
 type Modifier<T> = (input: T, index: number) => void;
 
@@ -28,37 +28,29 @@ const useBuffer = <T>(
     buffer.forEach((val, index) => {
       modifier(val, index);
     });
-   notifyChange(values);
+    notifyChange(values);
   }, deps);
 
   useEffect(() => {
     return () => {
       stopMapper(mapperId);
-    }
+    };
   }, [mapperId]);
 
   return values;
 };
 
-export const useRectBuffer = (
-  size: number,
-  modifier: Modifier<SkHostRect>
-) => useBuffer(size, () => Skia.XYWHRect(0, 0, 0, 0), modifier);
+export const useRectBuffer = (size: number, modifier: Modifier<SkHostRect>) =>
+  useBuffer(size, () => Skia.XYWHRect(0, 0, 0, 0), modifier);
 
 // Usage for RSXform Buffer
-export const useRSXformBuffer = (
-  size: number,
-  modifier: Modifier<SkRSXform>,
-) => useBuffer(size, () => Skia.RSXform(1, 0, 0, 0), modifier);
+export const useRSXformBuffer = (size: number, modifier: Modifier<SkRSXform>) =>
+  useBuffer(size, () => Skia.RSXform(1, 0, 0, 0), modifier);
 
 // Usage for Point Buffer
-export const usePointBuffer = (
-  size: number,
-  modifier: Modifier<SkPoint>,
-) => useBuffer(size, () => Skia.Point(0, 0), modifier);
+export const usePointBuffer = (size: number, modifier: Modifier<SkPoint>) =>
+  useBuffer(size, () => Skia.Point(0, 0), modifier);
 
 // Usage for Color Buffer
-export const useColorBuffer = (
-  size: number,
-  modifier: Modifier<SkColor>,
-) => useBuffer(size, () => Skia.Color("black"), modifier);
+export const useColorBuffer = (size: number, modifier: Modifier<SkColor>) =>
+  useBuffer(size, () => Skia.Color("black"), modifier);
