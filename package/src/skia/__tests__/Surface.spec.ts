d6b0af09a0f965dcbd73357390e1e165c3a8df0a
William Candillon <wcandillon@gmail.com>
2024-04-02 10:12:59 +0200

    📝

--- package/src/skia/__tests__/Surface.spec.ts
+++ package/src/skia/__tests__/Surface.spec.ts
@@ -3,6 +3,8 @@ import { setupSkia } from "./setup";
 describe("Surface", () => {
   it("A raster surface shouldn't leak", () => {
     const { Skia } = setupSkia();
+    // When leaking, the WASM memory limit will be reached quite quickly
+    // causing the test to fail
     for (let i = 0; i < 500; i++) {
       const surface = Skia.Surface.Make(1920, 1080)!;
       const canvas = surface.getCanvas();
