1d8051f612a21b9f93c42672783d4bdb54fc5169
William Candillon <wcandillon@gmail.com>
2024-04-03 20:50:11 +0200

    fix(🐛): Fix memory leak with MakeOffscreen when using CPU fallback (#2333)

--- package/src/renderer/__tests__/Surfaces.spec.tsx
+++ package/src/renderer/__tests__/Surfaces.spec.tsx
@@ -22,6 +22,20 @@ describe("Surface", () => {
     }
   });
   it("A raster surface shouldn't leak (2)", () => {
+    const { Skia } = setupSkia();
+    // When leaking, the WASM memory limit will be reached quite quickly
+    // causing the test to fail
+    for (let i = 0; i < 500; i++) {
+      const surface = Skia.Surface.MakeOffscreen(1920, 1080)!;
+      const canvas = surface.getCanvas();
+      canvas.clear(Skia.Color("cyan"));
+      surface.flush();
+      const image = surface.makeImageSnapshot();
+      image.dispose();
+      surface.dispose();
+    }
+  });
+  it("A raster surface shouldn't leak (3)", () => {
     for (let i = 0; i < 1000; i++) {
       //const t = performance.now();
       const r = 128;
