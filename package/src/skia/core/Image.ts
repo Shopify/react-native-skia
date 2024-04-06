e15989010a4ff27c3a7dbbd6227e3c44eec5f646
William Candillon <wcandillon@gmail.com>
2024-04-06 16:13:38 +0200

    :wrench:

--- package/src/skia/core/Image.ts
+++ package/src/skia/core/Image.ts
@@ -47,7 +47,8 @@ export const makeImageFromView = <
       );
     }
   }
-  viewRef.current.setNativeProps({ collapsable: false });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (viewRef.current! as any).setNativeProps({ collapsable: false });
   const viewTag = Platform.findNodeHandle(viewRef.current);
   if (viewTag !== null && viewTag !== 0) {
     return Skia.Image.MakeImageFromViewTag(viewTag);
