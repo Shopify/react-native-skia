da6dd4ad8cda1eb8ee52b4f49bf2cce5d927b625
William Candillon <wcandillon@gmail.com>
2024-04-13 12:41:37 +0200

    :arrow_up:

--- fabricexample/android/app/src/main/java/com/fabricexample/MainActivity.java
+++ fabricexample/android/app/src/main/java/com/fabricexample/MainActivity.java
@@ -1,4 +1,5 @@
 package com.fabricexample;
+import expo.modules.ReactActivityDelegateWrapper;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -23,10 +24,10 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 }
