6b2645f7fa0d9444850e66e1d4bc87745c7f103e
William Candillon <wcandillon@gmail.com>
2024-04-15 21:31:33 +0200

    :wrench:

--- package/android/cpp/rnskia-android/RNSkAndroidVideo.cpp
+++ package/android/cpp/rnskia-android/RNSkAndroidVideo.cpp
@@ -63,8 +63,7 @@ void RNSkAndroidVideo::initializeDecoder(const std::string &url) {
     // Remember to clean up resources after using them
     AMediaExtractor_delete(extractor);
     fclose(fp);
-    throw std::runtime_error(
-        "Failed to create extractor from file descriptor");
+    throw std::runtime_error("Failed to create extractor from file descriptor");
   }
   size_t numTracks = AMediaExtractor_getTrackCount(extractor);
   if (numTracks == 0) {
@@ -82,41 +81,62 @@ void RNSkAndroidVideo::initializeDecoder(const std::string &url) {
         if (codec == nullptr) {
           throw std::runtime_error("Failed to create codec");
         }
-        AMediaCodec_configure(codec, format, NULL, NULL, 0);
+        auto status = AMediaCodec_configure(codec, format, NULL, NULL, 0);
+        if (status != AMEDIA_OK) {
+          throw std::runtime_error("Failed to configure codec");
+        }
         AMediaCodec_start(codec);
         break;
       }
     }
-    AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_WIDTH, &width);
-    AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_HEIGHT, &height);
-    AMediaFormat_delete(format);
+    // AMediaFormat_delete(format);
   }
+  AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_WIDTH, &width);
+  AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_HEIGHT, &height);
 }
 
 sk_sp<SkImage> RNSkAndroidVideo::nextImage(double *timeStamp) {
   if (isExtractorAtEnd) {
-    RNSkLogger::logToConsole("RNSkAndroidVideo Extractor at end");
+    RNSkLogger::logToConsole("Extractor is at the end of the stream.");
     return nullptr;
   }
   AMediaCodecBufferInfo info;
   ssize_t bufIdx = AMediaCodec_dequeueInputBuffer(codec, 5000);
-  RNSkLogger::logToConsole("bufIdx: %d", bufIdx);
-  if (bufIdx >= 0) {
-    size_t bufSize;
-    uint8_t *buf = AMediaCodec_getInputBuffer(codec, bufIdx, &bufSize);
-    ssize_t sampleSize = AMediaExtractor_readSampleData(extractor, buf, bufSize);
-    RNSkLogger::logToConsole("bufSize: %d", bufSize);
-    RNSkLogger::logToConsole("sampleSize: %d", sampleSize);
-    if (sampleSize < 0) {
-      isExtractorAtEnd = true;
-      AMediaCodec_queueInputBuffer(codec, bufIdx, 0, 0, 0,
-                                   AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM);
-    } else {
-      int64_t presentationTimeUs = AMediaExtractor_getSampleTime(extractor);
-      AMediaCodec_queueInputBuffer(codec, bufIdx, 0, sampleSize,
-                                   presentationTimeUs, 0);
-      AMediaExtractor_advance(extractor);
-    }
+  RNSkLogger::logToConsole("Dequeued Input Buffer Index: %d", bufIdx);
+  if (bufIdx < 0) {
+    RNSkLogger::logToConsole("Failed to dequeue input buffer.");
+    return nullptr;
+  }
+  size_t bufSize;
+  uint8_t *buf = AMediaCodec_getInputBuffer(codec, bufIdx, &bufSize);
+  if (!buf) {
+    RNSkLogger::logToConsole("Failed to get input buffer.");
+    return nullptr;
+  }
+  ssize_t size = AMediaExtractor_getSampleSize(extractor);
+  if (size == -1) {
+    throw std::runtime_error("Failed to get sample size.");
+  }
+  int64_t pts = AMediaExtractor_getSampleTime(extractor);
+  if (size > bufSize) {
+    std::runtime_error(
+        "extractor sample size exceeds codec input buffer size"); //: %zu, %zd",
+                                                                  //: bufSize,
+                                                                  //: size);
+  }
+  ssize_t sampleSize = AMediaExtractor_readSampleData(extractor, buf, bufSize);
+  RNSkLogger::logToConsole("Buffer Size: %zu, Sample Size: %zd", bufSize,
+                           sampleSize);
+  if (sampleSize < 0) {
+    isExtractorAtEnd = true;
+    AMediaCodec_queueInputBuffer(codec, bufIdx, 0, 0, 0,
+                                 AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM);
+    RNSkLogger::logToConsole("No more samples to read. Marking end of stream.");
+  } else {
+    int64_t presentationTimeUs = AMediaExtractor_getSampleTime(extractor);
+    AMediaCodec_queueInputBuffer(codec, bufIdx, 0, sampleSize,
+                                 presentationTimeUs, 0);
+    AMediaExtractor_advance(extractor);
   }
 
   bufIdx = AMediaCodec_dequeueOutputBuffer(codec, &info, 0);
@@ -128,12 +148,13 @@ sk_sp<SkImage> RNSkAndroidVideo::nextImage(double *timeStamp) {
 
     uint8_t *outputBuffer = AMediaCodec_getOutputBuffer(codec, bufIdx, NULL);
     sk_sp<SkData> data = SkData::MakeWithoutCopy(outputBuffer, height * 4);
-    auto info = SkImageInfo::Make(width, height, kRGBA_8888_SkColorType,
-                                  kUnpremul_SkAlphaType);
-    sk_sp<SkImage> image = SkImages::RasterFromData(info, data, 4);
+    auto imageInfo = SkImageInfo::Make(width, height, kRGBA_8888_SkColorType,
+                                       kUnpremul_SkAlphaType);
+    sk_sp<SkImage> image = SkImages::RasterFromData(imageInfo, data, width * 4);
     AMediaCodec_releaseOutputBuffer(codec, bufIdx, false);
-    return image; // Return the created SkImage
+    return image;
   }
+
   return nullptr;
 }
 
