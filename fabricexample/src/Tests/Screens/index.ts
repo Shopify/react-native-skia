957212a25541d62bd9ccb23b6021454610587f79
William Candillon <wcandillon@gmail.com>
2024-05-07 20:53:25 +0200

    Add test for fabric example

--- fabricexample/src/Tests/Screens/index.ts
+++ fabricexample/src/Tests/Screens/index.ts
@@ -5,6 +5,7 @@ import { Snapshot2 } from "./Snapshot2";
 import { Snapshot3 } from "./Snapshot3";
 import { Snapshot4 } from "./Snapshot4";
 import { Snapshot5 } from "./Snapshot5";
+import { Snapshot6 } from "./Snapshot6";
 
 export const Screens: Record<string, FC> = {
   Snapshot1,
@@ -12,4 +13,5 @@ export const Screens: Record<string, FC> = {
   Snapshot3,
   Snapshot4,
   Snapshot5,
+  Snapshot6,
 };
