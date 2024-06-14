d222222691af4d1be06aa43867cbd659307ff532
William Candillon <wcandillon@gmail.com>
2024-06-14 23:25:30 +0200

    :green_heart:

--- package/src/external/reanimated/useVideoLoading.ts
+++ package/src/external/reanimated/useVideoLoading.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import type { Video } from "../../skia/types";
 import { Skia } from "../../skia";
@@ -10,17 +10,20 @@ const runtime = Rea.createWorkletRuntime("video-metadata-runtime");
 type VideoSource = string | null;
 
 export const useVideoLoading = (source: VideoSource) => {
-  const runOnJS = Rea.runOnJS;
+  const { runOnJS } = Rea;
   const [video, setVideo] = useState<Video | null>(null);
-  const cb = (src: string) => {
-    "worklet";
-    const vid = Skia.Video(src) as Video;
-    runOnJS(setVideo)(vid);
-  };
+  const cb = useCallback(
+    (src: string) => {
+      "worklet";
+      const vid = Skia.Video(src) as Video;
+      runOnJS(setVideo)(vid);
+    },
+    [runOnJS]
+  );
   useEffect(() => {
     if (source) {
       Rea.runOnRuntime(runtime, cb)(source);
     }
-  }, [source]);
+  }, [cb, source]);
   return video;
 };
