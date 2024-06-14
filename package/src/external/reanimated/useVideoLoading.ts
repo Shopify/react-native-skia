3be25d21aee2cc6996c61bf6b65c01394d1902b7
William Candillon <wcandillon@gmail.com>
2024-06-14 22:12:12 +0200

    fix(🐎): Fix reanimated regression

--- package/src/external/reanimated/useVideoLoading.ts
+++ package/src/external/reanimated/useVideoLoading.ts
@@ -10,11 +10,12 @@ const runtime = Rea.createWorkletRuntime("video-metadata-runtime");
 type VideoSource = string | null;
 
 export const useVideoLoading = (source: VideoSource) => {
+  const runOnJS = Rea.runOnJS;
   const [video, setVideo] = useState<Video | null>(null);
   const cb = (src: string) => {
     "worklet";
     const vid = Skia.Video(src) as Video;
-    Rea.runOnJS(setVideo)(vid);
+    runOnJS(setVideo)(vid);
   };
   useEffect(() => {
     if (source) {
