7137940f0fd997e6fa4870900a7628f0b9efd18b
William Candillon <wcandillon@gmail.com>
2024-06-18 13:26:29 +0200

    Implement SVG support for Web

--- example/src/Examples/API/SVG.tsx
+++ example/src/Examples/API/SVG.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useWindowDimensions } from "react-native";
-import { Canvas, ImageSVG, useSVG } from "@shopify/react-native-skia";
+import { Canvas, ImageSVG, Skia } from "@shopify/react-native-skia";
 
 export const SVG = () => {
   const { width, height } = useWindowDimensions();
-  const svg = useSVG(require("./tiger.svg"));
+  const svg = Skia.SVG.MakeFromString(require("./tiger.svg").default);
+  // useSVG(require("./tiger.svg"));
   return (
     <Canvas style={{ flex: 1 }}>
       <ImageSVG svg={svg} x={0} y={0} width={width / 2} height={height / 2} />
