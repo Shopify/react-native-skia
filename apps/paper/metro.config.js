a52d1d12a21673a832f8c15197fe4ee233a9d3ca
William Candillon <wcandillon@gmail.com>
2024-08-30 18:14:50 +0200

    :wrench:

--- apps/paper/metro.config.js
+++ apps/paper/metro.config.js
@@ -1,14 +1,39 @@
 const path = require('path');
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [path.resolve(__dirname, '../../node_modules')],
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(__dirname, `../../node_modules/${name}`),
-      },
-    ),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    projectRoot: __dirname,
+    watchFolders: [
+      path.resolve(__dirname, '../../node_modules'),
+      path.resolve(__dirname, '../../packages')
+    ],
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      extraNodeModules: new Proxy(
+        {},
+        {
+          get: (target, name) => {
+            if (name === '@shopify/react-native-skia') {
+              return path.join(__dirname, '../../packages/react-native-skia');
+            }
+            return path.join(__dirname, `../../node_modules/${name}`);
+          },
+        },
+      ),
+    },
+  };
+})();
