12ccdd7a9f614279da8b6a9abb2298cbbfa46556
William Candillon <wcandillon@gmail.com>
2024-08-31 14:27:30 +0200

    :wrench:

--- apps/paper/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    "transform-inline-environment-variables",
-    "react-native-reanimated/plugin",
-    "@babel/plugin-transform-runtime",
-  ],
-};
