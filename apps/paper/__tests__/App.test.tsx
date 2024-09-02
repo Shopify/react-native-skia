83a740457baccf9ec03611d44b57b176ce211f62
William Candillon <wcandillon@gmail.com>
2024-09-02 15:00:26 +0200

    :wrench:

--- apps/paper/__tests__/App.test.tsx
+++ apps/paper/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import "react-native";
 import React from "react";
 // Test renderer must be required after react-native.
-import { cleanup, render } from "@testing-library/react-native";
+import {cleanup, render} from "@testing-library/react-native";
 
 import App from "../src/App";
 
