51a4a471ab28d34f2eb37208a58bcaa67e6fa86f
William Candillon <wcandillon@gmail.com>
2024-09-12 18:08:35 +0200

    Add missing file

---
+++ apps/paper/src/Examples/WebGPU/utils.ts
@@ -0,0 +1,67 @@
+import { BlendMode, BlurStyle, mix, polar2Canvas, Skia, SkiaContext } from "@shopify/react-native-skia";
+import { Dimensions, PixelRatio } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import { useCanvasEffect } from "react-native-wgpu";
+
+export const useSkiaContext = () => {
+  const context = useSharedValue<SkiaContext | null>(null);
+  const ref = useCanvasEffect(() => {
+      const nativeSurface = ref.current!.getNativeSurface();
+      context.value = Skia.Context(
+      nativeSurface.surface,
+      nativeSurface.width * pd,
+      nativeSurface.height * pd
+      );
+  });
+  return {
+    context,
+    ref
+  }
+};
+
+const pd = PixelRatio.get();
+const { width, height } = Dimensions.get("window");
+const center = { x: width / 2, y: height / 2 };
+const R = width / 4;
+
+const c1 = "#61bea2";
+const c2 = "#529ca0";
+const root = Skia.Paint();
+root.setBlendMode(BlendMode.Screen);
+root.setMaskFilter(Skia.MaskFilter.MakeBlur(BlurStyle.Solid, 10, true));
+const p1 = root.copy();
+p1.setColor(Skia.Color(c1));
+const p2 = root.copy();
+p2.setColor(Skia.Color(c2));
+
+
+export const drawBreatheDemo = (ctx: SkiaContext, progress: number) => {
+  "worklet";
+   
+  const surface = ctx.getSurface();
+  const canvas = surface.getCanvas();
+  canvas.clear(Skia.Color(`rgb(36,43,56)`));
+  canvas.save();
+  canvas.scale(pd, pd);
+  canvas.rotate(progress * -180, center.x, center.y);
+  {new Array(6).fill(0).map((_, index) => {
+    canvas.save();
+    const theta = (index * (2 * Math.PI)) / 6;
+    const { x, y } = polar2Canvas(
+      { theta, radius: progress * R },
+      { x: 0, y: 0 }
+    );
+    const scale = mix(progress, 0.3, 1);
+
+    canvas.translate(center.x, center.y);
+    canvas.translate(x, y);
+    canvas.scale(scale, scale);
+    canvas.translate(-center.x, -center.y);
+
+    const paint = index % 2 ? p1 : p2;
+    canvas.drawCircle(center.x, center.y, R, paint);
+    canvas.restore();
+  })}
+  canvas.restore();
+
+}
