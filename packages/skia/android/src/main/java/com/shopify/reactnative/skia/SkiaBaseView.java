cf1c9c6b7f521a7115eb55a50702aecec6aefeb4
William Candillon <wcandillon@gmail.com>
2024-10-01 10:50:48 +0200

    :fire:

--- packages/skia/android/src/main/java/com/shopify/reactnative/skia/SkiaBaseView.java
+++ packages/skia/android/src/main/java/com/shopify/reactnative/skia/SkiaBaseView.java
@@ -54,76 +54,6 @@ public abstract class SkiaBaseView extends ReactViewGroup implements TextureView
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        // https://developer.android.com/training/gestures/multi
-        int action = ev.getActionMasked();
-
-        MotionEvent.PointerCoords r = new MotionEvent.PointerCoords();
-
-        double[] points;
-
-        // If this is a pointer_up/down event we need to handle it a bit specialized
-        switch (action) {
-            case MotionEvent.ACTION_POINTER_DOWN:
-            case MotionEvent.ACTION_POINTER_UP: {
-                points = new double[5];
-                int pointerIndex = ev.getActionIndex();
-                ev.getPointerCoords(pointerIndex, r);
-                points[0] = r.x;
-                points[1] = r.y;
-                points[2] = ev.getPressure(pointerIndex);
-                points[3] = motionActionToType(action);
-                points[4] = ev.getPointerId(pointerIndex);
-
-                updateTouchPoints(points);
-
-                break;
-            }
-            default: {
-                // For the rest we can just handle it like expected
-                int count = ev.getPointerCount();
-                int pointerIndex = 0;
-                points = new double[5 * count];
-                for (int i = 0; i < count; i++) {
-                    ev.getPointerCoords(i, r);
-                    points[pointerIndex++] = r.x;
-                    points[pointerIndex++] = r.y;
-                    points[pointerIndex++] = ev.getPressure(i);
-                    points[pointerIndex++] = motionActionToType(action);
-                    points[pointerIndex++] = ev.getPointerId(i);
-                }
-
-                updateTouchPoints(points);
-
-                break;
-            }
-        }
-
-        return true;
-    }
-
-    private static int motionActionToType(int action) {
-        int actionType = 3;
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_POINTER_DOWN:
-                actionType = 0;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                actionType = 1;
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_POINTER_UP:
-                actionType = 2;
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                actionType = 3;
-                break;
-        }
-        return actionType;
-    }
-
-    @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
         Log.i(tag, "onSurfaceTextureAvailable " + width + "/" + height);
         surfaceAvailable(surface, width, height);
@@ -171,8 +101,6 @@ public abstract class SkiaBaseView extends ReactViewGroup implements TextureView
     protected abstract void setMode(String mode);
 
     protected abstract void setDebugMode(boolean show);
-
-    protected abstract void updateTouchPoints(double[] points);
 
     protected abstract void registerView(int nativeId);
 
