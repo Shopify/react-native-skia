fbb83189d02f4112ae97ce0ababfc3a13514f7e4
Ollie <niu15076177821@hotmail.com>
2024-10-28 17:05:18 +0800

    feat: add intersect method types declaration

--- packages/skia/src/skia/web/JsiSkRect.ts
+++ packages/skia/src/skia/web/JsiSkRect.ts
@@ -30,6 +30,16 @@ export class JsiSkRect
     this.ref[3] = y + height;
   }
 
+
+  intersects(other: SkRect): boolean {
+    return (
+      this.x < other.x + other.width &&
+      this.x + this.width > other.x &&
+      this.y < other.y + other.height &&
+      this.y + this.height > other.y
+    );
+  }
+
   get x() {
     return this.ref[0];
   }
