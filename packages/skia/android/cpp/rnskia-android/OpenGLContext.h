03d1f61b509c9451d1a968af9d522abcc053206a
William Candillon <wcandillon@gmail.com>
2024-11-19 19:58:17 +0100

    :wrench:

--- packages/skia/android/cpp/rnskia-android/OpenGLContext.h
+++ packages/skia/android/cpp/rnskia-android/OpenGLContext.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#include "SkiaOpenGLSurfaceFactory.h"
-
-#include "WindowContext.h"
+#include "OpenGLWindowContext.h"
 #include "opengl/Display.h"
 
+#include "include/core/SkCanvas.h"
+#include "include/core/SkColorSpace.h"
 #include "include/core/SkSurface.h"
+#include "include/gpu/ganesh/GrDirectContext.h"
+#include "include/gpu/ganesh/gl/GrGLDirectContext.h"
+#include "include/gpu/ganesh/gl/GrGLInterface.h"
 
 class OpenGLContext {
 public:
@@ -18,28 +21,76 @@ public:
   }
 
   sk_sp<SkSurface> MakeOffscreen(int width, int height) {
-    return RNSkia::SkiaOpenGLSurfaceFactory::makeOffscreenSurface(
-        &_context, width, height);
+  auto colorType = kRGBA_8888_SkColorType;
+
+  SkSurfaceProps props(0, kUnknown_SkPixelGeometry);
+
+  auto result = _ctx->makeCurrent(*_surface);
+  if (!result) {
+    return nullptr;
+  }
+
+  // Create texture
+  auto texture = _directContext->createBackendTexture(
+      width, height, colorType, skgpu::Mipmapped::kNo, GrRenderable::kYes);
+
+  if (!texture.isValid()) {
+    RNSkia::RNSkLogger::logToConsole("couldn't create offscreen texture %dx%d", width,
+                             height);
+  }
+
+  struct ReleaseContext {
+    GrDirectContext* directContext;
+    GrBackendTexture texture;
+  };
+
+  auto releaseCtx = new ReleaseContext{
+      .directContext = _directContext.get(),
+      .texture = texture
+  };
+
+  // Create a SkSurface from the GrBackendTexture
+  return SkSurfaces::WrapBackendTexture(
+      _directContext.get(), texture, kTopLeft_GrSurfaceOrigin, 0,
+      colorType, nullptr, &props,
+      [](void *addr) {
+        auto releaseCtx = reinterpret_cast<ReleaseContext *>(addr);
+        releaseCtx->directContext->deleteBackendTexture(
+            releaseCtx->texture);
+        delete releaseCtx;
+      },
+      releaseCtx);
   }
 
   sk_sp<SkImage> MakeImageFromBuffer(void *buffer) {
-    return RNSkia::SkiaOpenGLSurfaceFactory::makeImageFromHardwareBuffer(
-        &_context, buffer);
+    return nullptr;
+    // return RNSkia::SkiaOpenGLSurfaceFactory::makeImageFromHardwareBuffer(
+    //     &_context, buffer);
   }
 
   std::unique_ptr<RNSkia::WindowContext> MakeWindow(ANativeWindow *window,
                                                     int width, int height) {
-    return RNSkia::SkiaOpenGLSurfaceFactory::makeContext(&_context, window,
-                                                         width, height);
+    return std::make_unique<RNSkia::OpenGLWindowContext>(_display.get(), _ctx.get(), _directContext.get(), window,width,
+                                                height);
   }
 
 private:
-  RNSkia::SkiaOpenGLContext _context;
   std::unique_ptr<RNSkia::Display> _display;
-  sk_sp<GrDirectContext> directContext;
+  std::unique_ptr<RNSkia::Context> _ctx;
+  std::unique_ptr<RNSkia::Surface> _surface;
+  sk_sp<GrDirectContext> _directContext;
 
   OpenGLContext() {
     _display = std::make_unique<RNSkia::Display>();
-    RNSkia::SkiaOpenGLHelper::createSkiaDirectContextIfNecessary(&_context);
+    auto config = _display->chooseConfig();
+    _ctx = _display->makeContext(config, nullptr);
+    _surface = _display->makePixelBufferSurface(config, 1, 1);
+    _ctx->makeCurrent(*_surface);
+    auto backendInterface = GrGLMakeNativeInterface();
+    _directContext = GrDirectContexts::MakeGL(backendInterface);
+
+    if (_directContext == nullptr) {
+      throw std::runtime_error("GrDirectContexts::MakeGL failed");
+    }
   }
 };
---
+++ packages/skia/android/cpp/rnskia-android/OpenGLWindowContext.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "RNSkLog.h"
+
+#include <fbjni/fbjni.h>
+#include <jni.h>
+
+#include <android/native_window_jni.h>
+#include <android/surface_texture.h>
+#include <android/surface_texture_jni.h>
+#include <condition_variable>
+#include <memory>
+#include <thread>
+#include <unordered_map>
+
+#include "WindowContext.h"
+#include "opengl/Display.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+
+#include "include/core/SkCanvas.h"
+#include "include/core/SkColorSpace.h"
+#include "include/core/SkSurface.h"
+#include "include/gpu/ganesh/GrBackendSurface.h"
+#include "include/gpu/ganesh/GrDirectContext.h"
+#include "include/gpu/ganesh/SkSurfaceGanesh.h"
+#include "include/gpu/ganesh/gl/GrGLInterface.h"
+
+#pragma clang diagnostic pop
+
+namespace RNSkia {
+
+class OpenGLWindowContext : public WindowContext {
+public:
+  OpenGLWindowContext(Display *display, Context* context, GrDirectContext* directContext, ANativeWindow *window,
+                     int width, int height)
+      : _directContext(directContext), _context(context), _window(window), _width(width), _height(height) {
+        // TODO: reuse config?
+        auto config = display->chooseConfig();
+        _surface = display->makeWindowSurface(config, window);
+      }
+
+
+  ~OpenGLWindowContext() { ANativeWindow_release(_window); }
+
+  sk_sp<SkSurface> getSurface() override;
+
+  void present() override {
+    _context->makeCurrent(*_surface);
+  
+    // Flush and submit the direct context
+    _directContext->flushAndSubmit();
+
+    // Swap buffers
+    _surface->Present();
+  }
+
+  void resize(int width, int height) override {
+    _skSurface = nullptr;
+    _width = width;
+    _height = height;
+  }
+
+  int getWidth() override { return _width; };
+
+  int getHeight() override { return _height; };
+
+private:
+  ANativeWindow *_window;
+  sk_sp<SkSurface> _skSurface = nullptr;
+  std::unique_ptr<Surface> _surface;
+  Context* _context;
+  GrDirectContext* _directContext;
+  int _width = 0;
+  int _height = 0;
+};
+
+} // namespace RNSkia
