e46b1ec346c2c0d45f8b65a2ce435fcc67760da5
William Candillon <wcandillon@gmail.com>
2024-11-20 15:38:29 +0100

    Make view flexible

--- packages/skia/android/src/main/java/com/shopify/reactnative/skia/SkiaBaseView.java
+++ packages/skia/android/src/main/java/com/shopify/reactnative/skia/SkiaBaseView.java
@@ -1,26 +1,20 @@
 package com.shopify.reactnative.skia;
 
 import android.content.Context;
-import android.graphics.SurfaceTexture;
-import android.util.Log;
 import android.view.Surface;
-import android.view.TextureView;
-
-import androidx.annotation.NonNull;
+import android.view.View;
 
 import com.facebook.react.views.view.ReactViewGroup;
 
-public abstract class SkiaBaseView extends ReactViewGroup implements TextureView.SurfaceTextureListener {
-    private final TextureView mTexture;
+public abstract class SkiaBaseView extends ReactViewGroup implements SkiaViewAPI {
+    private final View mView;
 
-    private String tag = "SkiaView";
+    private final String tag = "SkiaView";
 
     public SkiaBaseView(Context context) {
         super(context);
-        mTexture = new TextureView(context);
-        mTexture.setSurfaceTextureListener(this);
-        mTexture.setOpaque(false);
-        addView(mTexture);
+        mView = new SkiaTextureView(context, this);
+        addView(mView);
     }
 
     void dropInstance() {
@@ -29,39 +23,25 @@ public abstract class SkiaBaseView extends ReactViewGroup implements TextureView
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        Log.i(tag, "onLayout " + this.getMeasuredWidth() + "/" + this.getMeasuredHeight());
         super.onLayout(changed, left, top, right, bottom);
-        mTexture.layout(0, 0, this.getMeasuredWidth(), this.getMeasuredHeight());
+        mView.layout(0, 0, this.getMeasuredWidth(), this.getMeasuredHeight());
     }
 
     @Override
-    public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
-        Log.i(tag, "onSurfaceTextureAvailable " + width + "/" + height);
-        surfaceAvailable(new Surface(surface), width, height);
+    public void onSurfaceCreated(Surface surface, int width, int height) {
+        surfaceAvailable(surface, width, height);
     }
 
     @Override
-    public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
-        Log.i(tag, "onSurfaceTextureSizeChanged " + width + "/" + height);
+    public void onSurfaceChanged(int width, int height) {
         surfaceSizeChanged(width, height);
     }
 
     @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        Log.i(tag, "onSurfaceTextureDestroyed");
-        // https://developer.android.com/reference/android/view/TextureView.SurfaceTextureListener#onSurfaceTextureDestroyed(android.graphics.SurfaceTexture)
+    public void onSurfaceDestroyed() {
         surfaceDestroyed();
-        return false;
     }
 
-    private long _prevTimestamp = 0;
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-        long timestamp = surface.getTimestamp();
-        long frameDuration = (timestamp - _prevTimestamp)/1000000;
-        Log.i(tag, "onSurfaceTextureUpdated "+frameDuration+"ms");
-        _prevTimestamp = timestamp;
-    }
 
     protected abstract void surfaceAvailable(Object surface, int width, int height);
 
