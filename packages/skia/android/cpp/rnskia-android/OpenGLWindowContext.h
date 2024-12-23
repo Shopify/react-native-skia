dff71531ee08f6faa23e6e556417bd5e4e6eb060
William Candillon <wcandillon@gmail.com>
2024-12-23 17:16:38 +0100

    :green_heart:

--- packages/skia/android/cpp/rnskia-android/OpenGLWindowContext.h
+++ packages/skia/android/cpp/rnskia-android/OpenGLWindowContext.h
@@ -58,9 +58,9 @@ public:
 
   void resize(int width, int height) override {
     if (_skSurface != nullptr) {
-       _glContext->makeCurrent(_glSurface.get());
-       _skSurface->getCanvas()->clear(SK_ColorTRANSPARENT);
-       present();
+      // Let's make sure there is no pending work
+      _glContext->makeCurrent(_glSurface.get());
+      _glSurface->present();
       _skSurface = nullptr;
     }
   }
