f981eba742e0e6333fb8357b3a52e06c6075a246
William Candillon <wcandillon@gmail.com>
2024-12-30 14:42:36 +0100

    :green_heart:

--- packages/skia/src/external/reanimated/renderHelpers.ts
+++ packages/skia/src/external/reanimated/renderHelpers.ts
@@ -9,8 +9,13 @@ import Rea from "./ReanimatedProxy";
 export let HAS_REANIMATED = false;
 export let HAS_REANIMATED_3 = false;
 try {
+  // This logic is convoluted but necessary
+  // In most systems, `require("react-native-reanimated")` throws an error, all is well.
+  // In webpack, in some configuration it will return an empty object.
+  // So it will not throw an error and we need to check the version to know if it's there.
   const reanimatedVersion =
     require("react-native-reanimated/package.json").version;
+  require("react-native-reanimated");
   HAS_REANIMATED = !!reanimatedVersion;
   if (
     reanimatedVersion &&
