79ffd663cbb11ee3c2585f8fe0775e51678a38d2
William Candillon <wcandillon@gmail.com>
2025-01-03 12:35:36 +0100

    :wrench:

--- apps/paper/src/Examples/Matrix/Matrix.tsx
+++ apps/paper/src/Examples/Matrix/Matrix.tsx
@@ -44,7 +44,6 @@ export const Matrix = () => {
     <Canvas style={{ flex: 1 }} opaque>
       <Fill color="black" />
       <Group>
-        <BlurMask blur={8} style="solid" />
         {cols.map((_i, i) =>
           rows.map((_j, j) => (
             <Symbol
