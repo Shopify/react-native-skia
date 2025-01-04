2409cbb82b14b6b32086e7a251143c5e11b71794
William Candillon <wcandillon@gmail.com>
2025-01-04 17:22:55 +0100

    :wrench:

--- packages/skia/src/sksg/nodes/Node.ts
+++ packages/skia/src/sksg/nodes/Node.ts
@@ -28,6 +28,9 @@ export const sortNodeChildren = (parent: Node) => {
   "worklet";
   const maskFilters: Node[] = [];
   const colorFilters: Node[] = [];
+  const shaders: Node[] = [];
+  const imageFilters: Node[] = [];
+  const pathEffects: Node[] = [];
   const drawings: Node[] = [];
   const declarations: Node[] = [];
   parent.children.forEach((node) => {
@@ -42,11 +45,54 @@ export const sortNodeChildren = (parent: Node) => {
       colorFilters.push(node);
     } else if (node.type === NodeType.BlurMaskFilter) {
       maskFilters.push(node);
+    } else if (
+      // Path Effects
+      node.type === NodeType.DiscretePathEffect ||
+      node.type === NodeType.DashPathEffect ||
+      node.type === NodeType.Path1DPathEffect ||
+      node.type === NodeType.Path2DPathEffect ||
+      node.type === NodeType.CornerPathEffect ||
+      node.type === NodeType.SumPathEffect ||
+      node.type === NodeType.Line2DPathEffect
+    ) {
+      pathEffects.push(node);
+    } else if (
+      // Image Filters
+      node.type === NodeType.OffsetImageFilter ||
+      node.type === NodeType.DisplacementMapImageFilter ||
+      node.type === NodeType.BlurImageFilter ||
+      node.type === NodeType.DropShadowImageFilter ||
+      node.type === NodeType.MorphologyImageFilter ||
+      node.type === NodeType.BlendImageFilter ||
+      node.type === NodeType.RuntimeShaderImageFilter
+    ) {
+      imageFilters.push(node);
+    } else if (
+      // Shaders
+      node.type === NodeType.Shader ||
+      node.type === NodeType.ImageShader ||
+      node.type === NodeType.ColorShader ||
+      node.type === NodeType.Turbulence ||
+      node.type === NodeType.FractalNoise ||
+      node.type === NodeType.LinearGradient ||
+      node.type === NodeType.RadialGradient ||
+      node.type === NodeType.SweepGradient ||
+      node.type === NodeType.TwoPointConicalGradient
+    ) {
+      shaders.push(node);
     } else if (node.isDeclaration) {
       declarations.push(node);
     } else {
       drawings.push(node);
     }
   });
-  return { colorFilters, drawings, declarations, maskFilters };
+  return {
+    colorFilters,
+    drawings,
+    declarations,
+    maskFilters,
+    shaders,
+    pathEffects,
+    imageFilters,
+  };
 };
