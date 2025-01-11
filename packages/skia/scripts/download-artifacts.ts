921687bc7fbf4e4d84f3a79a43f9e1b79e0f65c6
William Candillon <wcandillon@gmail.com>
2025-01-11 23:07:51 +0100

    fixes ##2745

---
+++ packages/skia/scripts/download-artifacts.ts
@@ -0,0 +1,56 @@
+/* eslint-disable max-len */
+import fs from "fs";
+import path from "path";
+
+import { $ } from "./utils";
+
+const repo = "shopify/react-native-skia";
+const workflow = "build-skia.yml";
+const names = [
+  "skia-android-arm",
+  "skia-android-arm-64",
+  "skia-android-arm-x64",
+  "skia-android-arm-x86",
+  "skia-apple-xcframeworks",
+];
+//const branch = "main";
+
+const artifactsDir = path.resolve(
+  __dirname,
+  "../../../packages/skia/artifacts"
+);
+
+// Function to clear directory contents
+const clearDirectory = (directory: string) => {
+  if (fs.existsSync(directory)) {
+    fs.readdirSync(directory).forEach((file) => {
+      const curPath = path.join(directory, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        clearDirectory(curPath);
+        fs.rmdirSync(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    console.log(`❌ ${directory}`);
+  } else {
+    console.log(`Directory ${directory} does not exist, creating it...`);
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
+
+const result = $(
+  `gh run list --repo "${repo}" --workflow "${workflow}" --status success --limit 1 --json databaseId --jq '.[0].databaseId'`
+);
+const id = result.toString("utf8").trim();
+
+console.log("Clearing existing artifacts...");
+clearDirectory(artifactsDir);
+
+console.log(`Downloading artifacts to ${artifactsDir}`);
+names.forEach((artifactName) => {
+  $(
+    `gh run download "${id}" --repo "${repo}" --name "${artifactName}" --dir ${artifactsDir}/${artifactName}`
+  );
+});
+console.log("Done");
