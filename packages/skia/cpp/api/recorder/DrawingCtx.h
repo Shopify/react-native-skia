78ee5a61a7663e95b7673a3a8eccccd11a2f3de1
William Candillon <wcandillon@gmail.com>
2025-01-24 14:08:09 +0100

    :wrench:

--- packages/skia/cpp/api/recorder/DrawingCtx.h
+++ packages/skia/cpp/api/recorder/DrawingCtx.h
@@ -1,20 +1,100 @@
 #pragma once
-
-#include <include/core/SkCanvas.h>
-
-namespace RNSkia {
+#include <memory>
+#include <vector>
 
 class DrawingCtx {
 public:
-  SkCanvas *canvas;
-
-  DrawingCtx(SkCanvas *aCanvas) : canvas(aCanvas) {};
-  ~DrawingCtx() = default;
-
-  SkPaint &getPaint() {
+  DrawingCtx(SkCanvas *canvas) : canvas(canvas) {
     SkPaint paint;
-    return paint;
+    paints.push_back(paint);
+    nextPaintIndex++;
   }
-};
 
-} // namespace RNSkia
+  void savePaint() {
+    paints.push_back(SkPaint(getPaint()));
+    nextPaintIndex++;
+  }
+
+  //  void saveBackdropFilter() {
+  //    SkImageFilter *imageFilter = nullptr;
+  //    if (!imageFilters.empty()) {
+  //      imageFilter = imageFilters.back();
+  //      imageFilters.pop_back();
+  //    } else if (!colorFilters.empty()) {
+  //      auto cf = colorFilters.back();
+  //      colorFilters.pop_back();
+  //      imageFilter = skia->ImageFilter_MakeColorFilter(cf, nullptr);
+  //    }
+  //    canvas->saveLayer(nullptr, nullptr, imageFilter);
+  //    canvas->restore();
+  //  }
+
+  SkPaint &getPaint() { return paints.back(); }
+  const SkPaint &getPaint() const { return paints.back(); }
+
+  SkPaint &&restorePaint() {
+    if (paints.empty()) {
+      throw std::runtime_error("No paint available");
+    }
+    auto &&paint = std::move(paints.back());
+    paints.pop_back();
+    return std::move(paint);
+  }
+
+  //  void materializePaint() {
+  //    if (!colorFilters.empty()) {
+  //      SkColorFilter *result = nullptr;
+  //      for (auto it = colorFilters.rbegin(); it != colorFilters.rend(); ++it)
+  //      {
+  //        if (!result)
+  //          result = *it;
+  //        else
+  //          result = skia->ColorFilter_MakeCompose(*it, result);
+  //      }
+  //      getPaint()->setColorFilter(result);
+  //    }
+  //
+  //    if (!shaders.empty()) {
+  //      getPaint()->setShader(shaders.back());
+  //    }
+  //
+  //    if (!imageFilters.empty()) {
+  //      SkImageFilter *result = nullptr;
+  //      for (auto it = imageFilters.rbegin(); it != imageFilters.rend(); ++it)
+  //      {
+  //        if (!result)
+  //          result = *it;
+  //        else
+  //          result = skia->ImageFilter_MakeCompose(*it, result);
+  //      }
+  //      getPaint()->setImageFilter(result);
+  //    }
+  //
+  //    if (!pathEffects.empty()) {
+  //      SkPathEffect *result = nullptr;
+  //      for (auto it = pathEffects.rbegin(); it != pathEffects.rend(); ++it) {
+  //        if (!result)
+  //          result = *it;
+  //        else
+  //          result = skia->PathEffect_MakeCompose(*it, result);
+  //      }
+  //      getPaint()->setPathEffect(result);
+  //    }
+  //
+  //    colorFilters.clear();
+  //    shaders.clear();
+  //    imageFilters.clear();
+  //    pathEffects.clear();
+  //  }
+
+  SkCanvas *canvas;
+  std::vector<SkPaint> paints;
+  std::vector<sk_sp<SkColorFilter>> colorFilters;
+  std::vector<sk_sp<SkShader>> shaders;
+  std::vector<sk_sp<SkImageFilter>> imageFilters;
+  std::vector<sk_sp<SkPathEffect>> pathEffects;
+  // std::vector<SkPaint *> paintDeclarations;
+
+private:
+  size_t nextPaintIndex = 0;
+};
