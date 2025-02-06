be30130b0a49ce8c389c333bb5f2af892f100d35
Nathan Ahn <nathancahn@gmail.com>
2025-02-06 17:23:02 -0500

    Animated square

--- apps/expo-router-app/components/AnimatedSquareCanvas.tsx
+++ apps/expo-router-app/components/AnimatedSquareCanvas.tsx
@@ -1,9 +1,27 @@
 import { Rect } from "@shopify/react-native-skia/src";
+import { useEffect } from "react";
+import {
+	useSharedValue,
+	withRepeat,
+	withSequence,
+	withTiming,
+} from "react-native-reanimated";
 
 const AnimatedSquareCanvas = () => {
+	const x = useSharedValue(0);
+	useEffect(() => {
+		x.value = withRepeat(
+			withSequence(
+				withTiming(100, { duration: 1000 }),
+				withTiming(0, { duration: 1000 }),
+			),
+			-1,
+		);
+	}, [x]);
+
 	return (
 		<Canvas style={{ width: "100%", height: "100%" }}>
-			<Rect x={0} y={0} width={100} height={100} color="blue" />
+			<Rect x={x} y={0} width={100} height={100} color="blue" />
 		</Canvas>
 	);
 };
