7dc66d9253014682cfca32da0676e8b27e2409a3
William Candillon <wcandillon@gmail.com>
2025-02-25 08:46:19 +0100

    error handling in encodeImageData

--- packages/skia/cpp/api/JsiSkImage.h
+++ packages/skia/cpp/api/JsiSkImage.h
@@ -103,7 +103,7 @@ public:
         std::make_shared<JsiSkShader>(getContext(), std::move(shader)));
   }
 
-  sk_sp<SkData> encodeImageData(const jsi::Value *arguments, size_t count) {
+  sk_sp<SkData> encodeImageData(const jsi::Value *arguments, size_t count, bool rasterIfNeeded) {
     // Get optional parameters
     auto format =
         count >= 1 ? static_cast<SkEncodedImageFormat>(arguments[0].asNumber())
@@ -116,12 +116,14 @@ public:
 #if defined(SK_GRAPHITE)
     image = DawnContext::getInstance().MakeRasterImage(image);
 #else
-    if (image->isTextureBacked()) {
+    if (image->isTextureBacked() && rasterIfNeeded) {
       auto grContext = getContext()->getDirectContext();
       image = image->makeRasterImage(grContext);
       if (!image) {
-        return nullptr;
+        throw std::runtime_error("Failed to create raster image from texture in encodeImageData");
       }
+    } else if (image->isTextureBacked() && rasterIfNeeded) {
+      throw std::runtime_error("Cannot encode texture-backed image directly");
     }
 #endif
     sk_sp<SkData> data;
@@ -144,12 +146,15 @@ public:
       SkPngEncoder::Options options;
       data = SkPngEncoder::Encode(nullptr, image.get(), options);
     }
-
+    if (data == nullptr) {
+      throw std::runtime_error("Failed to encode image data or read pixels from the image");
+    }
     return data;
   }
 
   JSI_HOST_FUNCTION(encodeToBytes) {
-    auto data = encodeImageData(arguments, count);
+    bool rasterIfNeeded = count > 0 && arguments[0].isBool() ? arguments[0].asBool() : true;
+    auto data = encodeImageData(arguments, count, rasterIfNeeded);
     if (!data) {
       return jsi::Value::null();
     }
@@ -172,6 +177,7 @@ public:
   }
 
   JSI_HOST_FUNCTION(encodeToBase64) {
+    bool rasterIfNeeded = count > 0 && arguments[0].isBool() ? arguments[0].asBool() : true;
     auto data = encodeImageData(arguments, count);
     if (!data) {
       return jsi::Value::null();
