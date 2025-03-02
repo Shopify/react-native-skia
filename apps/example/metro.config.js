9c0380e1f6fd08e52e2822455ca8502c80f2dac4
Saad Najmi <sanajmi@microsoft.com>
2025-03-02 09:24:43 -0800

    fix "reanimated is not installed" by enabling inline requires

--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -4,7 +4,7 @@ module.exports = makeMetroConfig({
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
