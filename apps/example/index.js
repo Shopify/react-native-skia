8dfff63468d97dea312b7638f1a4854b1361fbfe
Saad Najmi <sanajmi@microsoft.com>
2025-03-02 09:07:16 -0800

    move paper test app to example

--- apps/example/index.js
+++ apps/example/index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
+import { AppRegistry } from "react-native";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from "./src/App";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
