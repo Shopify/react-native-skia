284eefdd5c5fbc1242112ad08288f2825f170162
Agustin Ferrari <agustin.ferrari@qubika.com>
2025-03-05 12:59:55 -0300

    Adds breaking code for example

--- external-apps/expo-router-app/components/AnimatedSquareCanvas.tsx
+++ external-apps/expo-router-app/components/AnimatedSquareCanvas.tsx
@@ -1,28 +1,33 @@
-import { Canvas, Rect } from "@shopify/react-native-skia/src";
-import { useEffect } from "react";
-import {
-	useSharedValue,
-	withRepeat,
-	withSequence,
-	withTiming,
-} from "react-native-reanimated";
+import { Canvas, RoundedRect } from "@shopify/react-native-skia/src";
+import { useCallback, useEffect, useState } from "react";
+import { runOnJS, useAnimatedReaction, useSharedValue, withRepeat, withSequence, withTiming } from "react-native-reanimated";
 
 const AnimatedSquareCanvas = () => {
-	const x = useSharedValue(0);
-	useEffect(() => {
-		x.value = withRepeat(
-			withSequence(
-				withTiming(100, { duration: 1000 }),
-				withTiming(0, { duration: 1000 }),
-			),
-			-1,
-		);
-	}, [x]);
+    const isActive = useSharedValue(false);
 
-	return (
-		<Canvas style={{ width: "100%", height: "100%" }}>
-			<Rect x={x} y={0} width={100} height={100} color="blue" />
-		</Canvas>
-	);
+    const [, setFirstRenderFinished] = useState(false);
+
+    const setIsActive = useCallback(() => {
+        runOnJS(setFirstRenderFinished)(true);
+    }, []);
+
+    useAnimatedReaction(
+        () => isActive.value,
+        () => {
+            runOnJS(setIsActive)();
+        },
+        [isActive, setIsActive]
+    );
+
+    const x = useSharedValue(0);
+    useEffect(() => {
+        x.value = withRepeat(withSequence(withTiming(100, { duration: 1000 }), withTiming(0, { duration: 1000 })), -1);
+    }, [x]);
+
+    return (
+        <Canvas style={{ width: "100%", height: "100%" }}>
+            <RoundedRect x={x} y={0} width={100} height={100} color="blue" r={8} />
+        </Canvas>
+    );
 };
 export default AnimatedSquareCanvas;
