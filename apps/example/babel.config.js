ca4aab7b3fc907e9dc96c4417ca2736107643cb1
Saad Najmi <sanajmi@microsoft.com>
2025-03-13 05:30:09 -0500

    align typescript version and get eslint to run again

--- apps/example/babel.config.js
+++ apps/example/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
   plugins: [
-    'react-native-reanimated/plugin',
-    'transform-inline-environment-variables',
-  ]
+    "react-native-reanimated/plugin",
+    "transform-inline-environment-variables",
+  ],
 };
