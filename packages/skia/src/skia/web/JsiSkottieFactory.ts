26f18a1569c33558fa18c172a172419a8750dba6
William Candillon <wcandillon@gmail.com>
2025-05-15 10:51:00 +0200

    :wrench:

--- packages/skia/src/skia/web/JsiSkottieFactory.ts
+++ packages/skia/src/skia/web/JsiSkottieFactory.ts
@@ -1,6 +1,6 @@
 import type { CanvasKit } from "canvaskit-wasm";
 
-import type { SkottieFactory, SkManagedSkottieAnimation } from "../types";
+import type { SkottieFactory } from "../types";
 import type { SkSkottieAnimation } from "../types/Skottie";
 
 import { Host } from "./Host";
@@ -12,19 +12,10 @@ export class JsiSkottieFactory extends Host implements SkottieFactory {
   }
 
   Make(json: string): SkSkottieAnimation {
-    const animation = this.CanvasKit.MakeAnimation(json);
+    const animation = this.CanvasKit.MakeManagedAnimation(json);
     if (!animation) {
       throw new Error("Failed to create SkottieAnimation");
     }
     return new JsiSkottieAnimation(this.CanvasKit, animation);
-  }
-
-  MakeManagedAnimation(
-    _json: string,
-    _assets?: Record<string, ArrayBuffer>,
-    _filterPrefix?: string
-  ): //soundMap?: SoundMap
-  SkManagedSkottieAnimation {
-    throw new Error("Not implemented yet");
   }
 }
