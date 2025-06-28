d5a4a702f38121d752aa07ffa5067861aad67073
William Candillon <wcandillon@gmail.com>
2025-06-28 16:17:28 +0200

    :wrench:

--- packages/skia/src/renderer/__tests__/e2e/Camera.spec.tsx
+++ packages/skia/src/renderer/__tests__/e2e/Camera.spec.tsx
@@ -411,4 +411,42 @@ describe("Camera", () => {
     );
     checkImage(image, "snapshots/matrix4/camera-top-left-center.png");
   });
+  it("test perspective (1)", async () => {
+    const { Skia } = importSkia();
+    const { width, height } = surface;
+    const pad = 0;
+    const rct = {
+      x: pad,
+      y: pad,
+      width: width - pad * 2,
+      height: height - pad * 2,
+    };
+    const path = Skia.Path.Make();
+    const path3 = new Path3();
+    path3.addHRect(rct, 0);
+
+    const camAngle = Math.PI / 4;
+
+    const cam = {
+      eye: [-2, 0, 0.5], // Camera centered
+      coa: [-1, 0, 0], // Look at top-left corner of rectangle
+      up: [0, 1, 0],
+      near: 0.02,
+      far: 4,
+      angle: camAngle,
+    };
+
+    const mat = CanvasKit.M44.setupCamera(
+      CanvasKit.LTRBRect(0, 0, width, height),
+      Math.min(width, height) / 2,
+      cam
+    );
+    path3.project(path, mat as unknown as Matrix4);
+    const image = await surface.draw(
+      <Group>
+        <Path path={path} color="cyan" opacity={0.5} />
+      </Group>
+    );
+    checkImage(image, "snapshots/matrix4/test-perspective.png");
+  });
 });
