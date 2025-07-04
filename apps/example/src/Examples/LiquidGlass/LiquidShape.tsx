44db6042f7564d7b435eebed46ed9da31410032a
William Candillon <wcandillon@gmail.com>
2025-07-04 09:44:11 +0200

    :wrench:

--- apps/example/src/Examples/LiquidGlass/LiquidShape.tsx
+++ apps/example/src/Examples/LiquidGlass/LiquidShape.tsx
@@ -1,5 +1,21 @@
-import { Canvas, Fill, Group, rect } from "@shopify/react-native-skia";
+import {
+  BackdropFilter,
+  Canvas,
+  Fill,
+  Group,
+  rect,
+  RuntimeShader,
+  Skia,
+} from "@shopify/react-native-skia";
 import React, { useState } from "react";
+
+const source = Skia.RuntimeEffect.Make(`
+uniform shader image;
+
+half4 main(float2 xy) {
+  return image.eval(xy) + vec4(0.5, 0.1, 0.9, 0.0);
+}
+`)!;
 
 export const LiquidShape = () => {
   const [{ width, height }, setSize] = useState({ width: 0, height: 0 });
@@ -16,6 +32,7 @@ export const LiquidShape = () => {
       <Group clip={rect(0, height / 2, width, height / 2)}>
         <Fill color="black" />
       </Group>
+      <BackdropFilter filter={<RuntimeShader source={source} />} />
     </Canvas>
   );
 };
