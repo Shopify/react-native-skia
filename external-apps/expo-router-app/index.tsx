112027ed49d517933c588ac19ed761714773faec
William Candillon <wcandillon@gmail.com>
2025-08-10 12:23:40 +0200

    :wrench:

--- external-apps/expo-router-app/index.tsx
+++
@@ -1,5 +0,0 @@
-import "@expo/metro-runtime";
-import { App } from "expo-router/build/qualified-entry";
-import { renderRootComponent } from "expo-router/build/renderRootComponent";
-
-renderRootComponent(App);
