382bef2983947b2c9104cc0da858920199cc6bdf
William Candillon <wcandillon@gmail.com>
2025-08-10 10:04:38 +0200

    :wrench:

--- apps/remotion/src/Playground/Playground.tsx
+++ apps/remotion/src/Playground/Playground.tsx
@@ -1,49 +1,15 @@
 import React from "react";
-import { Fill } from "@shopify/react-native-skia";
 
 import { Background, Canvas } from "../components";
 import {
   makeAnimation,
-  stagger,
-  timing,
+  wait,
 } from "../components/animations/Animations";
 
-const duration = 20;
 
-const playground = {
-  expoProgress: 0,
-  skiaProgress: 0,
-  remotionProgress: 0,
-  snackProgress: 0,
-  webProgress: 0,
-  uiProgress: 0,
-  reconcilerProgress: 0,
-  picturesProgress: 0,
-};
-
-const state = makeAnimation(function* ({
-  expoProgress,
-  skiaProgress,
-  remotionProgress,
-  snackProgress,
-  webProgress,
-  uiProgress,
-  reconcilerProgress,
-  picturesProgress,
-}) {
-  yield* stagger(
-    8,
-    timing(skiaProgress, { duration }),
-    timing(expoProgress, { duration }),
-    timing(snackProgress, { duration }),
-    timing(webProgress, { duration }),
-    timing(uiProgress, { duration }),
-    timing(reconcilerProgress, { duration }),
-    timing(picturesProgress, { duration }),
-    timing(remotionProgress, { duration })
-  );
-},
-playground);
+const state = makeAnimation(function* ({}) {
+  wait(30);
+}, {});
 
 export const durationInFrames = state.duration;
 
@@ -54,17 +20,8 @@ export const Playground = () => {
         typefaces={{ RubikMedium: require("./assets/Rubik-Medium.otf") }}
         images={{}}
       >
-        <Fill color="black" />
-        <Tiles />
+       <Background />
       </Canvas>
-    </>
-  );
-};
-
-const Tiles = () => {
-  return (
-    <>
-      <Background />
     </>
   );
 };
