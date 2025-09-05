3209d1c941a16016ad15bc67385a7ef66f44e257
William Candillon <wcandillon@gmail.com>
2025-09-05 10:15:28 +0200

    chore(🐙): Update timeouts in test client

--- apps/example/src/Tests/Tests.tsx
+++ apps/example/src/Tests/Tests.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-eval */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
@@ -9,7 +10,7 @@ import {
 } from "@shopify/react-native-skia";
 import type { RefObject } from "react";
 import React, { useEffect, useRef, useState } from "react";
-import { PixelRatio, Text, View } from "react-native";
+import { PixelRatio, Platform, Text, View } from "react-native";
 
 import type { SerializedNode } from "./deserialize";
 import { parseNode, parseProps } from "./deserialize";
@@ -20,7 +21,7 @@ export const CI = process.env.CI === "true";
 const s = 3;
 const scale = s / PixelRatio.get();
 const size = 256 * scale;
-const timeToDraw = CI ? 1500 : 500;
+const timeToDraw = CI ? (Platform.OS === "ios" ? 1500 : 3000) : 500;
 
 interface TestsProps {
   assets: { [key: string]: any };
@@ -32,7 +33,7 @@ export const Tests = ({ assets }: TestsProps) => {
   const [client, hostname] = useClient();
   const [drawing, setDrawing] = useState<any>(null);
   const [screen, setScreen] = useState<any>(null);
-  
+
   useEffect(() => {
     if (client !== null) {
       // Define the message handler as a separate function
@@ -66,16 +67,16 @@ export const Tests = ({ assets }: TestsProps) => {
       };
 
       // Use addEventListener instead of onmessage
-      client.addEventListener('message', handleMessage);
+      client.addEventListener("message", handleMessage);
 
       // Clean up: remove the specific event listener
       return () => {
-        client.removeEventListener('message', handleMessage);
+        client.removeEventListener("message", handleMessage);
       };
     }
     return;
   }, [assets, client]);
-  
+
   useEffect(() => {
     if (drawing && client) {
       const it = setTimeout(() => {
@@ -104,7 +105,7 @@ export const Tests = ({ assets }: TestsProps) => {
     }
     return;
   }, [client, drawing, ref]);
-  
+
   useEffect(() => {
     if (screen && client) {
       const it = setTimeout(async () => {
@@ -120,7 +121,7 @@ export const Tests = ({ assets }: TestsProps) => {
     }
     return;
   }, [client, screen]);
-  
+
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Text style={{ color: "black" }}>
