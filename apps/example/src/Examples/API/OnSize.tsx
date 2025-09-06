bcffe54a85167bd42aac4856dafae0813395d904
William Candillon <wcandillon@gmail.com>
2025-09-06 17:39:50 +0200

    Add example

---
+++ apps/example/src/Examples/API/OnSize.tsx
@@ -0,0 +1,37 @@
+import { Canvas, Rect, rect } from "@shopify/react-native-skia";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { useDerivedValue, useSharedValue } from "react-native-reanimated";
+
+export const OnSize = () => {
+  const size = useSharedValue({ width: 0, height: 0 });
+  const redRect = useDerivedValue(() => {
+    console.log(size.value);
+    return rect(0, 0, size.value.width, size.value.height);
+  });
+
+  return (
+    <View
+      style={{
+        alignSelf: "flex-start",
+        backgroundColor: "cyan",
+      }}
+    >
+      <Canvas onSize={size} style={StyleSheet.absoluteFill}>
+        <Rect rect={redRect} color="red" />
+      </Canvas>
+      <Text>
+        Skia version:{" "}
+        {require("@shopify/react-native-skia/package.json").version}
+      </Text>
+      <TextInput
+        placeholder={`
+Hello
+World!
+ `}
+        multiline
+      />
+    </View>
+  );
+};
