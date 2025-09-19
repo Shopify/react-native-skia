0ffe9daebfd3e0186725ae7599d9e1977d43e915
William Candillon <wcandillon@gmail.com>
2025-09-19 07:20:50 +0200

    :wrench:

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -8,7 +8,6 @@ import { HeaderBackButton } from "@react-navigation/elements";
 import { FiberProvider } from "its-fine";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { enableScreens } from "react-native-screens";
-import { Skia } from "@shopify/react-native-skia";
 
 import {
   ReanimatedExample,
