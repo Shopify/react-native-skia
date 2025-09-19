f519eb3a8e8d3f5b94354d10f906b36fe11a772c
William Candillon <wcandillon@gmail.com>
2025-09-19 06:08:28 +0200

    :wrench:

--- packages/skia/src/renderer/__tests__/e2e/Paths.spec.tsx
+++ packages/skia/src/renderer/__tests__/e2e/Paths.spec.tsx
@@ -289,7 +289,7 @@ describe("Paths", () => {
   });
   it("should be possible to call dispose on a path", async () => {
     await surface.eval((Skia) => {
-      const path = Skia.Path.Make();
+      using path = Skia.Path.Make();
       path.moveTo(20, 20).lineTo(20, 40).lineTo(40, 20);
       path.dispose();
       return path;
