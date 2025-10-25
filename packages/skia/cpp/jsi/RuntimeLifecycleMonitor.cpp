6f3e0f9898ade87c24f31de07c4648b721914b5b
William Candillon <wcandillon@gmail.com>
2025-10-25 09:13:41 +0200

    fix(💣): fix race condition in RuntimeAwareCache (#3478)

--- packages/skia/cpp/jsi/RuntimeLifecycleMonitor.cpp
+++ packages/skia/cpp/jsi/RuntimeLifecycleMonitor.cpp
@@ -1,5 +1,6 @@
 #include "RuntimeLifecycleMonitor.h"
 
+#include <mutex>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
@@ -9,25 +10,43 @@ namespace RNJsi {
 static std::unordered_map<jsi::Runtime *,
                           std::unordered_set<RuntimeLifecycleListener *>>
     listeners;
+static std::mutex listenersMutex;
 
 struct RuntimeLifecycleMonitorObject : public jsi::HostObject {
   jsi::Runtime *_rt;
   explicit RuntimeLifecycleMonitorObject(jsi::Runtime *rt) : _rt(rt) {}
   ~RuntimeLifecycleMonitorObject() {
-    auto listenersSet = listeners.find(_rt);
-    if (listenersSet != listeners.end()) {
-      for (auto listener : listenersSet->second) {
-        listener->onRuntimeDestroyed(_rt);
+    std::unordered_set<RuntimeLifecycleListener *> listenersCopy;
+    {
+      std::lock_guard<std::mutex> lock(listenersMutex);
+      auto listenersSet = listeners.find(_rt);
+      if (listenersSet != listeners.end()) {
+        listenersCopy = listenersSet->second;
+        listeners.erase(listenersSet);
       }
-      listeners.erase(listenersSet);
+    }
+    for (auto listener : listenersCopy) {
+      listener->onRuntimeDestroyed(_rt);
     }
   }
 };
 
 void RuntimeLifecycleMonitor::addListener(jsi::Runtime &rt,
                                           RuntimeLifecycleListener *listener) {
-  auto listenersSet = listeners.find(&rt);
-  if (listenersSet == listeners.end()) {
+  bool shouldInstallMonitor = false;
+  {
+    std::lock_guard<std::mutex> lock(listenersMutex);
+    auto listenersSet = listeners.find(&rt);
+    if (listenersSet == listeners.end()) {
+      std::unordered_set<RuntimeLifecycleListener *> newSet;
+      newSet.insert(listener);
+      listeners.emplace(&rt, std::move(newSet));
+      shouldInstallMonitor = true;
+    } else {
+      listenersSet->second.insert(listener);
+    }
+  }
+  if (shouldInstallMonitor) {
     // We install a global host object in the provided runtime, this way we can
     // use that host object destructor to get notified when the runtime is being
     // terminated. We use a unique name for the object as it gets saved with the
@@ -36,16 +55,12 @@ void RuntimeLifecycleMonitor::addListener(jsi::Runtime &rt,
         rt, "__rnskia_rt_lifecycle_monitor",
         jsi::Object::createFromHostObject(
             rt, std::make_shared<RuntimeLifecycleMonitorObject>(&rt)));
-    std::unordered_set<RuntimeLifecycleListener *> newSet;
-    newSet.insert(listener);
-    listeners.emplace(&rt, std::move(newSet));
-  } else {
-    listenersSet->second.insert(listener);
   }
 }
 
 void RuntimeLifecycleMonitor::removeListener(
     jsi::Runtime &rt, RuntimeLifecycleListener *listener) {
+  std::lock_guard<std::mutex> lock(listenersMutex);
   auto listenersSet = listeners.find(&rt);
   if (listenersSet == listeners.end()) {
     // nothing to do here
