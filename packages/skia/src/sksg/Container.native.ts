f3b890c6615fcec25e2f27074ed6d335e793efa6
William Candillon <wcandillon@gmail.com>
2025-11-13 13:44:16 +0100

    :green_heart:

--- packages/skia/src/sksg/Container.native.ts
+++ packages/skia/src/sksg/Container.native.ts
@@ -44,14 +44,15 @@ class NativeReanimatedContainer extends Container {
       return;
     }
     const recorder = new ReanimatedRecorder(this.Skia);
-    const { nativeId, picture } = this;
+    const { nativeId, picture, Skia } = this;
     visit(recorder, this.root);
     const sharedValues = recorder.getSharedValues();
     const sharedRecorder = recorder.getRecorder();
     // Send first frame to be drawn
     Rea.executeOnUIRuntimeSync(() => {
       "worklet";
-      nativeDrawOnscreen(nativeId, sharedRecorder, picture);
+      const firstPicture = Skia.Picture.MakePicture(null)!;
+      nativeDrawOnscreen(nativeId, sharedRecorder, firstPicture);
     })();
     // Animate
     if (sharedValues.length > 0) {
