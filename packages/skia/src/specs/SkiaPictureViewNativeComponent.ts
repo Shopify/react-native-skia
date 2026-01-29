19f45e3be3d87e3591cafc2702d287a7da22e85b
William Candillon <wcandillon@gmail.com>
2026-01-29 20:07:25 +0100

    :green_heart:

--- packages/skia/src/specs/SkiaPictureViewNativeComponent.ts
+++ packages/skia/src/specs/SkiaPictureViewNativeComponent.ts
@@ -2,6 +2,7 @@ import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNati
 import type { ViewProps } from "react-native";
 import type { WithDefault } from "react-native/Libraries/Types/CodegenTypes";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - pointerEvents needs to be redeclared for codegen to generate native bindings
 export interface NativeProps extends ViewProps {
   debug?: boolean;
