35ec4ebaf1482f1999ca0bac0d2fbf952fade7f8
William Candillon <wcandillon@gmail.com>
2026-01-29 20:04:00 +0100

    :wrench:

--- packages/skia/src/specs/SkiaPictureViewNativeComponent.ts
+++ packages/skia/src/specs/SkiaPictureViewNativeComponent.ts
@@ -2,7 +2,8 @@ import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNati
 import type { ViewProps } from "react-native";
 import type { WithDefault } from "react-native/Libraries/Types/CodegenTypes";
 
-export interface NativeProps extends Omit<ViewProps, "pointerEvents"> {
+// @ts-ignore - pointerEvents needs to be redeclared for codegen to generate native bindings
+export interface NativeProps extends ViewProps {
   debug?: boolean;
   opaque?: boolean;
   colorSpace?: string;
