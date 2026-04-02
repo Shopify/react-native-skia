06830320d24b869f1cd6872c314c5f04b929a6ae
William Candillon <wcandillon@gmail.com>
2026-04-02 13:38:45 +0200

    Open chore(🐙): fix import warnings (#3798)

--- packages/skia/src/specs/WebGPUViewNativeComponent.ts
+++ packages/skia/src/specs/WebGPUViewNativeComponent.ts
@@ -1,6 +1,6 @@
-import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
-import type { Int32 } from "react-native/Libraries/Types/CodegenTypes";
+import { codegenNativeComponent } from "react-native";
 import type { ViewProps } from "react-native";
+import type { Int32 } from "react-native/Libraries/Types/CodegenTypes";
 
 export interface NativeProps extends ViewProps {
   contextId: Int32;
